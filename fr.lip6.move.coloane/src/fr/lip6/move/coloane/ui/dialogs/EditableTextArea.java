bf356931364debacaacd425f38d4597acf568e57
aortiz <aortiz@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2007-01-22 15:22:46 +0000

    Ajout des boites de dialogue. Il manque l'interactive. La doc arrive bientot (j'vais reviser mon rattrapage de demain :p).

---
+++ fr.lip6.move.coloane/src/fr/lip6/move/coloane/ui/dialogs/EditableTextArea.java
@@ -0,0 +1,38 @@
+package fr.lip6.move.coloane.ui.dialogs;
+
+import java.util.ArrayList;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.SWT;
+
+public class EditableTextArea extends TextArea {
+
+	public EditableTextArea(Composite parent,
+			int multiLine, String defaultValue) {
+		super(parent, TextArea.INPUT_AUTHORIZED, multiLine, defaultValue);
+		
+		if (multiLine == TextArea.SINGLE_LINE)
+			textWidget = new Text(parent, SWT.SINGLE);
+		else
+			textWidget = new Text(parent, SWT.MULTI | SWT.WRAP | SWT.V_SCROLL);
+		
+		textWidget.setSize(250, 500);
+		((Text)textWidget).setText(defaultValue);
+	}
+	
+	public ArrayList<String> getText() {
+		ArrayList<String> result = new ArrayList<String>();
+		
+		String[] tokens = ((Text)textWidget).getText().split("\n");
+		
+		for (String token : tokens)
+			result.add(token);
+		
+		return result;
+	}
+
+	public void addChoice(String choice) {
+		throw new UnsupportedOperationException();
+	}
+}
