cb7893088797406d6b931df86002d64102b9060a
aortiz <aortiz@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2007-01-25 23:20:29 +0000

    Ajout de la gestion des evenements et de l'initialisation. Il faut que je voie avec Jean-Baptiste ce dont il a besoin comme methodes de mise a jour et de quelle maniere il veut pouvoir les faire.

--- fr.lip6.move.coloane/src/fr/lip6/move/coloane/ui/panels/PNSCView.java
+++ fr.lip6.move.coloane/src/fr/lip6/move/coloane/ui/panels/PNSCView.java
@@ -4,38 +4,76 @@ import java.util.Vector;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.List;
 import org.eclipse.ui.part.ViewPart;
 
 public class PNSCView extends ViewPart {
-	List resultsList;
-	Vector<PNSCResult> results;
+	protected List resultsList;
+	protected Vector<PNSCResult> results;
+	protected StyledText text;
 	
 	public PNSCView() {
 		super();
-		
+		results = new Vector<PNSCResult>();
 	}
+
 	
-	protected void BuildresulstList(Composite parent) {
-		
+	public void add(PNSCResult result) {
+		results.add(result);
 	}
 	
 	@Override
 	public void createPartControl(Composite parent) {
+		/* This view will be divided in two parts :
+		 * - to the left, we will have a list off errors
+		 * - to the right, we will have the text correponding to the
+		 *   selected error
+		 */ 
 		parent.getShell().setLayout(new GridLayout(2, true));
 		resultsList = new List(parent, SWT.SINGLE | SWT.BORDER);
-		resultsList.add("Premier élément");
-		resultsList.add("Deuxieme élément");
 		
-		StyledText text =
+		text =
 			new StyledText(parent, SWT.READ_ONLY | SWT.BORDER);
-		text.setText("Wazaaaaaaaaaaa");
+		text.setJustify(true);
+		text.setAlignment(SWT.CENTER);
+		
+		setSelectionListener();
+		setList();
 	}
-
+	
 	@Override
 	public void setFocus() {
 		// TODO Auto-generated method stub
+	}
+	
+	protected void setList() {
+		for (int i = 0; i < results.size(); i++)
+			resultsList.add(results.get(i).getResultName());
+		
+		
+		if (results.size() > 0) {
+			text.setText(results.get(0).getResultDescription());
+		} else
+			text.setText("Your network is perfect :D");
+	}
+	
+	protected void setSelectionListener() {
+		resultsList.addSelectionListener(new SelectionListener() {
+
+			public void widgetDefaultSelected(SelectionEvent e) {
+				text.setText(results.get(resultsList.
+						getSelectionIndex()).getResultDescription());
+				
+			}
+
+			public void widgetSelected(SelectionEvent e) {
+				text.setText(results.get(resultsList.
+						getSelectionIndex()).getResultDescription());
+			}
+		});
 	}
 }
