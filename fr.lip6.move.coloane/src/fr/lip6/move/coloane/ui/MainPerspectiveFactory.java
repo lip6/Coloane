4d694be3e9b8d386df42ef8154796d53c34cd8e0
aortiz <aortiz@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2007-01-25 23:16:14 +0000

    Ajout de la vue pour les resultats du Petri Net Syntax Checker. La vue s'ajoute quand la perspective Coloane est lancee. Ce n'est pas le bon comportement mais, en laissant la vue non-affichee par defaut, il y avait un probleme au demarrage d'Eclipse que je n'ai pour l'instant pas vu comment resoudre. Je vais me pencher dessus.

--- fr.lip6.move.coloane/src/fr/lip6/move/coloane/ui/MainPerspectiveFactory.java
+++ fr.lip6.move.coloane/src/fr/lip6/move/coloane/ui/MainPerspectiveFactory.java
@@ -20,6 +20,8 @@ public class MainPerspectiveFactory implements IPerspectiveFactory {
 	/** ID for history view */
 	private static final String STATE_VIEW = "fr.lip6.move.coloane.views.StateView";
 	
+	private static final String PNSC_VIEW = "fr.lip6.move.coloane.views.PNSCView";
+	
 	/** ID for wizard */
 	//private static final String NEW_WIZARD = "CPN-AMIoE.NewModelWizard";
 			
@@ -53,6 +55,8 @@ public class MainPerspectiveFactory implements IPerspectiveFactory {
 		//  Add "show views". They will be present in "show view" menu
 		layout.addShowViewShortcut(HISTORY_VIEW);
 		layout.addShowViewShortcut(STATE_VIEW);
+		layout.addShowViewShortcut(PNSC_VIEW);
+		
 		layout.addShowViewShortcut(IPageLayout.ID_PROP_SHEET);
 		layout.addShowViewShortcut(IPageLayout.ID_RES_NAV);
 		layout.addShowViewShortcut(IPageLayout.ID_OUTLINE);
@@ -82,6 +86,7 @@ public class MainPerspectiveFactory implements IPerspectiveFactory {
 		
 		bottom.addView(HISTORY_VIEW);
 		bottom.addView(STATE_VIEW);
+		bottom.addView(PNSC_VIEW);
 		bottom.addView(IPageLayout.ID_PROP_SHEET);
 	}
 
