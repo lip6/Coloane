928ea978a97fdec2c967c9fa1cda3ec476198bb9
aortiz <aortiz@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2007-01-25 23:17:14 +0000

    Ajout des quelques lignes qui rendent cette classe utilisable.

--- fr.lip6.move.coloane/src/fr/lip6/move/coloane/ui/panels/PNSCResult.java
+++ fr.lip6.move.coloane/src/fr/lip6/move/coloane/ui/panels/PNSCResult.java
@@ -1,5 +1,20 @@
 package fr.lip6.move.coloane.ui.panels;
 
 public class PNSCResult {
+	protected String resultName;
+	protected String resultDescription;
+	
+	public PNSCResult(String resultName,
+			String resultDescription) {
+		this.resultName = resultName;
+		this.resultDescription = resultDescription;
+	}
 
+	public String getResultDescription() {
+		return resultDescription;
+	}
+
+	public String getResultName() {
+		return resultName;
+	}
 }
