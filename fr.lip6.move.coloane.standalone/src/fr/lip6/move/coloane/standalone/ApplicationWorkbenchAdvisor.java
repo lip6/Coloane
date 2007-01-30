26241fdbbac65acd864f1e3068482304c073822f
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2007-01-30 21:19:53 +0000

    Pour eviter un warning incompréhensible...

--- fr.lip6.move.coloane.standalone/src/fr/lip6/move/coloane/standalone/ApplicationWorkbenchAdvisor.java
+++ fr.lip6.move.coloane.standalone/src/fr/lip6/move/coloane/standalone/ApplicationWorkbenchAdvisor.java
@@ -5,6 +5,7 @@ import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 import org.eclipse.ui.internal.ide.IDEWorkbenchAdvisor;
 
+@SuppressWarnings("restriction")
 public class ApplicationWorkbenchAdvisor extends IDEWorkbenchAdvisor {
 
 	private static final String PERSPECTIVE_ID = "fr.lip6.move.coloane.perspective";
