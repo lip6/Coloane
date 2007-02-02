5ece8cc6795917058bb8ba0b4a46a36b7cbf4d81
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2007-02-02 09:36:18 +0000

    Ajout de la prise en compte des resultats de services Ajout d'un test pour l'affichage des menus

--- fr.lip6.move.coloane/src/fr/lip6/move/coloane/ui/UserInterface.java
+++ fr.lip6.move.coloane/src/fr/lip6/move/coloane/ui/UserInterface.java
@@ -3,11 +3,15 @@ package fr.lip6.move.coloane.ui;
 
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 
+import fr.lip6.move.coloane.communications.objects.Result;
 import fr.lip6.move.coloane.interfaces.*;
 import fr.lip6.move.coloane.menus.RootMenu;
 import fr.lip6.move.coloane.motor.session.Session;
+import fr.lip6.move.coloane.results.ActionsList;
+import fr.lip6.move.coloane.results.ResultsList;
 import fr.lip6.move.coloane.ui.dialogs.Dialog;
 import fr.lip6.move.coloane.ui.dialogs.DialogFactory;
 import fr.lip6.move.coloane.ui.dialogs.IDialog;
@@ -31,13 +35,18 @@ public class UserInterface implements IUiCom, IUiMotor {
     /** Le module de moteur */
     private IMotorUi motor = null; 
     
-    
+    /** La gestion des resultats */
+    private ActionsList serviceResultList = null;
+        
     
     /**
      * Constructeur de la classe
      */
     public UserInterface() {
         fenetreTravail = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+        
+        // Le gestionnaire de resultats de services
+        serviceResultList = new ActionsList();
     }
 
 
@@ -82,20 +91,33 @@ public class UserInterface implements IUiCom, IUiMotor {
 	 */
 	public void drawMenu (RootMenu menu) {
 		Session currentSession = motor.getSessionManager().getCurrentSession();
-		currentSession.setSessionMenu(menu);
+		currentSession.setServicesMenu(menu);
 		GraphicalMenu gmenu = new GraphicalMenu(menu,fenetreTravail,this);
 		gmenu.build();		
 	}
 	
+	
 	/**
 	 * Demande la mise a jour du menu
 	 * @param menuAPI arborescence menu
-	 * */
+	 */
 	public void updateMenu() {
 		Session currentSession = motor.getSessionManager().getCurrentSession();
-		GraphicalMenu gmenu = new GraphicalMenu(currentSession.getSessionMenu(),fenetreTravail,this);
-		gmenu.update();
+		if (currentSession != null) {
+			GraphicalMenu gmenu = new GraphicalMenu(currentSession.getServicesMenu(),fenetreTravail,this);
+			gmenu.update();
+		} 
 	}
+	
+	
+	/**
+	 * Demande la suppression d'un menu designee par son nom
+	 * @param menuName Le nom du menu a supprimer
+	 */
+	public void removeMenu(String menuName) {
+		MenuManipulation.remove(menuName);
+	}
+
 	
 	/**
 	 * Desactivation du rootMenu
@@ -105,6 +127,63 @@ public class UserInterface implements IUiCom, IUiMotor {
 		MenuManipulation.setEnabled(rootName,rootName,status);
 	}
 	
+	
+	/**
+	 * Affichage des resultats d'un appel de service
+	 * @param serviceName Le nom du service qui produit ses resultats
+	 * @param result L'objet contenant les resultats pour ce service
+	 */
+	public void setResults(String serviceName, Result result) {
+		if (serviceResultList != null) {
+			String labelService;
+			ResultsList r = null;
+			
+			if (serviceName.equals("Petri net syntax checker")) {
+				
+				labelService = "Syntax-Checker Results";
+				r = new ResultsList(labelService);
+				
+				String description = null;
+				
+				System.out.println(" ! > "+result.getDescription());
+				if (result.getDescription().equals("List of unnamed places.")) {
+					description = "This place is unnamed";
+				} else if (result.getDescription().equals("List of unnamed transitions.")) {
+					System.out.println("Ajout de la description "+description);
+					description = "This transition is unnamed";
+				}
+				
+				// Parcours de mes resultats
+				for (String object : result.getList()) {
+					System.out.println("Ajout d'un objet :"+object);
+					r.add(new fr.lip6.move.coloane.results.Result(object,description));
+				}
+			}
+			
+			System.out.println("Transmission des resultats");
+			serviceResultList.setResultsList(r);
+		}
+	}
+	
+	
+	/**
+	 * Affichage des resultats dans la vue resultats
+	 */
+	public void printResults() {
+		Display.getDefault().asyncExec(new Runnable() {
+			public void run() {
+				try {
+					serviceResultList.addResultsList();
+					fenetreTravail.getActivePage().showView(MainPerspectiveFactory.RESULTS_VIEW);
+					serviceResultList.display(MainPerspectiveFactory.RESULTS_VIEW, fenetreTravail);
+				} catch (PartInitException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	
 	/**
 	 * Demande d'un service
 	 * @param rootMenuName Le nom du menu racine
@@ -112,8 +191,11 @@ public class UserInterface implements IUiCom, IUiMotor {
 	 * @param serviceName LE nom du service demande
 	 */
 	public void askForService(String rootMenuName, String parentName, String serviceName) {
+		serviceResultList.removeAll();
+		
 		this.com.askForService(rootMenuName, parentName, serviceName);
 	}
+	
 	
 	/** Affichage d'une boite de dialogue
 	 * @param Dialog L'objet contenant toutes les informations sur la boite de dialogue a afficher
@@ -146,6 +228,7 @@ public class UserInterface implements IUiCom, IUiMotor {
 	public void setCom(IComUi com) {
 		this.com = com;
 	}
+	
 	
 	/**
 	 * On attache le module du moteur a l' l'interface utilisateur
