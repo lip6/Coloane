94aa7fa0a8b98954225a8b87988f65bd44571985
aortiz <aortiz@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2007-02-04 16:44:16 +0000

    The project is now created.

--- fr.lip6.move.coloane/src/fr/lip6/move/coloane/ui/wizards/NewProjectWizard.java
+++ fr.lip6.move.coloane/src/fr/lip6/move/coloane/ui/wizards/NewProjectWizard.java
@@ -1,9 +1,20 @@
 package fr.lip6.move.coloane.ui.wizards;
 
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.wizards.newresource.BasicNewProjectResourceWizard;
+import org.eclipse.ui.wizards.newresource.BasicNewResourceWizard;
+
+import fr.lip6.move.coloane.main.Coloane;
 
 public class NewProjectWizard extends Wizard implements INewWizard {
 
@@ -21,11 +32,38 @@ public class NewProjectWizard extends Wizard implements INewWizard {
 	
 	@Override
 	public boolean performFinish() {
-		return projectCreationPage.finish();
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		IProject newProject = projectCreationPage.getProjectHandle();
+		IProjectDescription description = workspace.newProjectDescription(newProject.getName());
+
+		String[] natureIds = { "Model project" };
+		description.setNatureIds(natureIds);
+		IPath oldPath = Platform.getLocation();
+		IPath newPath = projectCreationPage.getLocationPath();
+		
+		if (!oldPath.equals(newPath)) {
+			oldPath = newPath;
+			description.setLocation(newPath);
+		}
+
+		try {
+			if (!newProject.exists())
+				newProject.create(description, null);
+			
+			if (!newProject.isOpen())
+				newProject.open(null);
+		} catch (CoreException e) {
+			e.printStackTrace();
+		}
+		// Select and reveal the project in the workbench window
+		BasicNewProjectResourceWizard.updatePerspective(null);
+		BasicNewResourceWizard.selectAndReveal(newProject,Coloane.getDefault().getWorkbench().getActiveWorkbenchWindow());
+		return true;
 	}
 
 	public void init(IWorkbench workbench, IStructuredSelection selection) {
-		projectCreationPage = new ProjectCreationPage("Page name", selection);
+		projectCreationPage =
+			new ProjectCreationPage("Page name", selection);
 	}
 
 }
