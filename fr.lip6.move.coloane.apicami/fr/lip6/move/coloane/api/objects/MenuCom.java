4e2cbb27cf2ecc81590b91f50d4c05aeb9daaaf3
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2007-02-08 14:45:13 +0000

    Trois nouveau objets heritant des objets abstraits des interfaces

---
+++ fr.lip6.move.coloane.apicami/fr/lip6/move/coloane/api/objects/MenuCom.java
@@ -0,0 +1,9 @@
+package fr.lip6.move.coloane.api.objects;
+
+public class MenuCom extends fr.lip6.move.coloane.interfaces.objects.MenuCom {
+
+	public MenuCom(String serviceName, String fatherName, boolean enabled) {
+		super(serviceName, fatherName, enabled);
+	}
+
+}
---
+++ fr.lip6.move.coloane.apicami/fr/lip6/move/coloane/api/objects/RootMenuCom.java
@@ -0,0 +1,8 @@
+package fr.lip6.move.coloane.api.objects;
+
+public class RootMenuCom extends fr.lip6.move.coloane.interfaces.objects.RootMenuCom {
+
+	public RootMenuCom(String rootMenu) {
+		super(rootMenu);
+	}
+}
