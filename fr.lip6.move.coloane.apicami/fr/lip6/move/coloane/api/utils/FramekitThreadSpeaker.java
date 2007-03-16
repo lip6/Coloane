fd68d916b86e3d4d5e3134f94ca9158c02f58074
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2007-03-16 10:30:16 +0000

    Mise a jour de l'architecture des packages pour API. Actuellement api.java est a la racine de fr.lip6.move.coloane.api Maven interdit l'utilisation d'un .java sans package independant. Donc : creation d'un package main pour contenir api. Nouvelle architecture : fr.lip6.move.coloane.api.main.api.java
    Plus mise a jour des fichiers impactés par ce changement (refactoring)

--- fr.lip6.move.coloane.apicami/fr/lip6/move/coloane/api/utils/FramekitThreadSpeaker.java
+++ fr.lip6.move.coloane.apicami/fr/lip6/move/coloane/api/utils/FramekitThreadSpeaker.java
@@ -1,7 +1,7 @@
 package fr.lip6.move.coloane.api.utils;
 
-import fr.lip6.move.coloane.api.Api;
 import fr.lip6.move.coloane.api.exceptions.CommunicationCloseException;
+import fr.lip6.move.coloane.api.main.Api;
 import fr.lip6.move.coloane.interfaces.model.IModel;
 
 /**
