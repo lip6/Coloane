b69972b3a506e5b9818999d83df10d84d31432d5
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2007-04-11 15:09:28 +0000

    Nouvelle exception utile lors de la construction du modele

---
+++ fr.lip6.move.coloane/branches/model-consistency/src/fr/lip6/move/coloane/exceptions/BuildException.java
@@ -0,0 +1,36 @@
+package fr.lip6.move.coloane.exceptions;
+
+/**
+ * Class exception 
+ * Cette exception est utilisée par DialogTool
+ * @author Albert DALODE
+ */
+
+public class BuildException extends Exception {
+	/**
+	 * Utilisé pour la serialisation
+	 */
+	private static final long serialVersionUID = 3722590923091130674L;
+	
+	
+	/**
+	 * Message d'information sur l'exception
+	 */
+	private String msg;
+	
+	/**
+	 * Constructeur de la classe
+	 * @param s message associatée à l'exception
+	 */
+	public  BuildException ( String s ) {
+		super(s);
+		msg = s;
+	}
+	
+	/**
+	 * @return string message d'erreur
+	 */
+	public String getMessage() {
+		return msg;
+	}
+}
