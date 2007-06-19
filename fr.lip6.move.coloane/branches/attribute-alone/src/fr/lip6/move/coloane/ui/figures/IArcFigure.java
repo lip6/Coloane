7c3bed796080a96ddf20c9533043581274731679
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2007-06-19 23:54:37 +0000

    Suppression de la gestion des attributs dans la figure de l'arc. Tout est maintenant gere directement depuis l'editpart de l'attribut

--- fr.lip6.move.coloane/branches/attribute-alone/src/fr/lip6/move/coloane/ui/figures/IArcFigure.java
+++ fr.lip6.move.coloane/branches/attribute-alone/src/fr/lip6/move/coloane/ui/figures/IArcFigure.java
@@ -4,11 +4,5 @@ import org.eclipse.draw2d.Connection;
 import org.eclipse.draw2d.IFigure;
 
 public interface IArcFigure extends IFigure, Connection {
-    /**
-     * Indique la valeur associee a l'arc
-     * @param text La valeur de l'arc
-     */
-	public void setLabelValue(String text);
 	
-	public void setLabelLabel(String text);
 }
