0fbb26e180a5d06e3bb0118d1b5168996b4eae99
emasliah <emasliah@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2007-06-26 11:54:39 +0000

    Deuxieme test avec differents packages

---
+++ sandbox/emasliah/Internationalisation/langue/deuxieme/Deuxieme.java
@@ -0,0 +1,17 @@
+package langue.deuxieme;
+
+import langue.Premier;
+
+/** */
+public class Deuxieme {
+
+	public Deuxieme() {
+	}
+
+	public void parler() {
+		System.out.println(Premier.bundle.getString("Deuxieme1"));
+		System.out.println(Premier.bundle.getString("Deuxieme2"));
+	}
+
+} // Deuxieme
+
