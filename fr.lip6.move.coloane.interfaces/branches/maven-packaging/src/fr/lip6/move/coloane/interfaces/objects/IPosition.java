cfd36a54647976e1c0d4a0b10e7694f601696a9c
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2007-07-16 07:56:42 +0000

    Nouvelle branche de travail pour eric afin de tester le deploiement automatique des interfaces

---
+++ fr.lip6.move.coloane.interfaces/branches/maven-packaging/src/fr/lip6/move/coloane/interfaces/objects/IPosition.java
@@ -0,0 +1,11 @@
+package fr.lip6.move.coloane.interfaces.objects;
+
+public interface IPosition {
+
+	
+	public int getXPosition();
+	
+	public int getYPosition();
+	
+	public void setPosition(int x, int y);
+}
