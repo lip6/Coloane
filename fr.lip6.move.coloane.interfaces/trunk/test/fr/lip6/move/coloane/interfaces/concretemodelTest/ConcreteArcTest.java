37c391f9750e1e5455b6cbc9087e23a8c6a24b73
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2007-08-08 09:55:07 +0000

    Tests for Arcs and Attributes are now OK and use new version of interfaces

--- fr.lip6.move.coloane.interfaces/trunk/test/fr/lip6/move/coloane/interfaces/concretemodelTest/ConcreteArcTest.java
+++ fr.lip6.move.coloane.interfaces/trunk/test/fr/lip6/move/coloane/interfaces/concretemodelTest/ConcreteArcTest.java
@@ -3,223 +3,256 @@ package fr.lip6.move.coloane.interfaces.concretemodelTest;
 import fr.lip6.move.coloane.interfaces.concretemodel.ConcreteArc;
 import fr.lip6.move.coloane.interfaces.concretemodel.ConcreteAttribute;
 import fr.lip6.move.coloane.interfaces.concretemodel.ConcreteNode;
-import fr.lip6.move.coloane.interfaces.exceptions.SyntaxErrorException;
-import fr.lip6.move.coloane.interfaces.model.Arc;
+import fr.lip6.move.coloane.interfaces.exceptions.ModelException;
 import fr.lip6.move.coloane.interfaces.model.IAttribute;
 import fr.lip6.move.coloane.interfaces.model.INode;
-import fr.lip6.move.coloane.interfaces.objects.IPosition;
-import fr.lip6.move.coloane.interfaces.objects.Position;
-
 import java.util.Vector;
 
 import junit.framework.TestCase;
 
+/**
+ * Test des arcs du modele generique
+ */
+
 public class ConcreteArcTest extends TestCase {
 
+	private ConcreteArc caShort;
+	private ConcreteArc caLong;
 	private ConcreteArc ca;
 
-	private ConcreteArc caId;
+	static final int IDARC = 6;
+	static final int IDARC2 = 10;
+	static final String TYPEARC = "Arc concret";
 
 	public ConcreteArcTest(String name) {
 		super(name);
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
 	protected final void setUp() throws Exception {
 		super.setUp();
-		ca = new ConcreteArc("Arc concret");
-		caId = new ConcreteArc("Arc concret", 1);
+		caShort = new ConcreteArc(TYPEARC);
+		caLong = new ConcreteArc(TYPEARC, IDARC);
+		ca = caShort;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
 	protected final void tearDown() throws Exception {
 		super.tearDown();
-		ca = null;
-		caId = null;
+		caShort = null;
+		caLong = null;
 	}
 
-	public final void testTranslate() {
-		//assertTrue(true == true);
-	}
-
+	/**
+	 * Test method for {@link fr.lip6.move.coloane.interfaces.model.Arc#Arc(String, int)}
+	 */
 	public final void testConcreteArcStringInt() {
-		ConcreteArc cb = new ConcreteArc("Arc concret", 5);
-		assertTrue(cb.getId() == 5);
-		assertTrue(cb.getArcType().equals("Arc concret"));
+		assertTrue(caLong.getId() == IDARC);
+		assertTrue(caLong.getArcType().equals(TYPEARC));
 	}
 
+	/**
+	 * Test method for {@link fr.lip6.move.coloane.interfaces.model.Arc#Arc(String)}
+	 */
 	public final void testConcreteArcString() {
-		ConcreteArc cb = new ConcreteArc("Arc concret");
-		assertTrue(cb.getArcType().equals("Arc concret"));
-		assertTrue(cb.getId() == 0);
+		assertTrue(caShort.getArcType().equals(TYPEARC));
+		assertTrue(caShort.getId() == 0);
 	}
 
-	public final void testArcStringInt() {
-		Arc a = new ConcreteArc("Arc abstrait", 3);
-		assertTrue(a.getArcType().equals("Arc abstrait"));
-		assertTrue(a.getId() == 3);
-	}
-
-	public final void testArcString() {
-		Arc a = new ConcreteArc("Arc abstrait");
-		assertTrue(a.getArcType().equals("Arc abstrait"));
-		assertTrue(a.getId() == 0);
-	}
-
+	/**
+	 * Test method for {@link fr.lip6.move.coloane.interfaces.model.Arc#getArcType()
+	 */
 	public final void testGetArcType() {
-		assertTrue(ca.getArcType().equals("Arc concret"));
-		assertTrue(caId.getArcType().equals("Arc concret"));
+		assertTrue(caLong.getArcType().equals(TYPEARC));
+		assertTrue(caShort.getArcType().equals(TYPEARC));
 	}
 
+	/**
+	 * Test method for {@link fr.lip6.move.coloane.interfaces.model.Arc#getId()}
+	 */
 	public final void testGetId() {
-		assertTrue(caId.getId() == 1);
-		assertTrue(ca.getId() == 0);
+		assertTrue(caLong.getId() == IDARC);
+		assertTrue(caShort.getId() == 0);
 	}
 
+	/**
+	 * Test method for {@link fr.lip6.move.coloane.interfaces.model.Arc#setId(int)}
+	 */
 	public final void testSetId() {
-		ca.setId(3);
-		caId.setId(5);
-		assertTrue(ca.getId() == 3);
-		assertTrue(caId.getId() == 5);
+		caShort.setId(IDARC);
+		caLong.setId(IDARC2);
+		assertTrue(caShort.getId() == IDARC);
+		assertTrue(caLong.getId() == IDARC2);
 	}
 
+
+	static final String TYPEBEGIN = "Begin";
+	static final String TYPEEND = "End";
+
+	/**
+	 * Test method for {@link fr.lip6.move.coloane.interfaces.model.Arc#setStartingNode(INode)}
+	 */
 	public final void testSetStartingNode() {
-		INode node = new ConcreteNode("Debut");
+		INode node = new ConcreteNode(TYPEBEGIN);
 		ca.setStartingNode(node);
 		assertTrue(ca.getStartingNode().equals(node));
 	}
 
+	/**
+	 * Test method for {@link fr.lip6.move.coloane.interfaces.model.Arc#setEndingNode(INode)}
+	 */
 	public final void testSetEndingNode() {
-		INode node = new ConcreteNode("Fin");
+		INode node = new ConcreteNode(TYPEEND);
 		ca.setEndingNode(node);
 		assertTrue(ca.getEndingNode().equals(node));
 	}
 
+	/**
+	 * Test method for {@link fr.lip6.move.coloane.interfaces.model.Arc#getStartingNode()}
+	 */
 	public final void testGetStartingNode() {
 		assertTrue(ca.getStartingNode() == null);
-		INode node = new ConcreteNode("debut");
+		INode node = new ConcreteNode(TYPEBEGIN);
 		ca.setStartingNode(node);
 		assertTrue(ca.getStartingNode().equals(node));
 	}
 
+	/**
+	 * Test method for {@link fr.lip6.move.coloane.interfaces.model.Arc#getEndingNode()}
+	 */
 	public final void testGetEndingNode() {
 		assertTrue(ca.getEndingNode() == null);
-		INode node = new ConcreteNode("fin");
+		INode node = new ConcreteNode(TYPEEND);
 		ca.setEndingNode(node);
 		assertTrue(ca.getEndingNode().equals(node));
 	}
 
+	static final String ATTNAME = "Attribute";
+	static final String ATTVALUE = "Hello";
+	static final String ATTVALUE2 = "Bye";
+	static final String ATTVALUE3 = "See you !";
+	static final int ATTREFID = 0;
+
+	/**
+	 * Test method for {@link fr.lip6.move.coloane.interfaces.model.Arc#addAttribute(IAttribute)}
+	 */
 	public final void testAddAttribute() {
-		IAttribute attribute = new ConcreteAttribute("Attribute", "0", 1);
+		IAttribute attribute = new ConcreteAttribute(ATTNAME, ATTVALUE, ATTREFID);
 		ca.addAttribute(attribute);
 		assertTrue(ca.getListOfAttrSize() == 1);
 		assertTrue(ca.getNthAttr(0).equals(attribute));
+		assertTrue(ca.getId() == attribute.getRefId());
 	}
 
+	/**
+	 * Test method for {@link fr.lip6.move.coloane.interfaces.model.Arc#removeAttribute(IAttribute)}
+	 */
 	public final void testRemoveAttributeIAttribute() {
-		IAttribute attribute = new ConcreteAttribute("Attribute", "0", 1);
-		IAttribute attr = new ConcreteAttribute("Attribute", "0", 2);
-		ca.addAttribute(attr);
+		IAttribute attribute = new ConcreteAttribute(ATTNAME, ATTVALUE, ATTREFID);
+		IAttribute attribute2 = new ConcreteAttribute(ATTNAME, ATTVALUE2, ATTREFID);
+		ca.addAttribute(attribute2);
 		ca.addAttribute(attribute);
 		assertTrue(ca.getListOfAttrSize() == 2);
-		ca.removeAttribute(attribute);
-		assertTrue(ca.getListOfAttrSize() == 1);
-		ca.removeAttribute(attr);
-		assertTrue(ca.getListOfAttrSize() == 0);
-	}
-
-	public final void testRemoveAttributeInt() {
-		IAttribute attribute = new ConcreteAttribute("Attribute", "0", 1);
-		IAttribute attr = new ConcreteAttribute("Attribute", "0", 2);
-		ca.addAttribute(attr);
-		ca.addAttribute(attribute);
-		ca.removeAttribute(0);
-		assertTrue(ca.getListOfAttrSize() == 1);
-	}
-
-	public final void testGetListOfAttrSize() {
-		IAttribute at1 = new ConcreteAttribute("Attribute", "0", 1);
-		IAttribute at2 = new ConcreteAttribute("Attribute", "0", 2);
-		IAttribute at3 = new ConcreteAttribute("Attribut", "0", 3);
-		ca.addAttribute(at1);
-		ca.addAttribute(at2);
-		ca.addAttribute(at3);
-		assertTrue(ca.getListOfAttrSize() == 3);
-	}
-
-	public final void testGetListOfAttr() {
-		Vector<IAttribute> via;
-		IAttribute at1 = new ConcreteAttribute("Attribute", "0", 1);
-		IAttribute at2 = new ConcreteAttribute("Attribute", "0", 2);
-		IAttribute at3 = new ConcreteAttribute("Attribut", "0", 3);
-		ca.addAttribute(at1);
-		ca.addAttribute(at2);
-		ca.addAttribute(at3);
-		via = ca.getListOfAttr();
-		assertTrue(via.elementAt(0).equals(at1));
-		assertFalse(via.elementAt(0).equals(at2));
-		assertTrue(via.elementAt(1).equals(at2));
-		assertFalse(via.elementAt(0).equals(at3));
-		assertTrue(via.elementAt(2).equals(at3));
-	}
-
-	public final void testGetNthAttr() {
-		IAttribute at1 = new ConcreteAttribute("Attribute", "0", 1);
-		IAttribute at2 = new ConcreteAttribute("Attribute", "0", 2);
-		IAttribute at3 = new ConcreteAttribute("Attribut", "0", 3);
-		ca.addAttribute(at1);
-		ca.addAttribute(at2);
-		ca.addAttribute(at3);
-
-		assertTrue(ca.getNthAttr(2).equals(at3));
-	}
-
-	public final void testGetListOfPI() {
-		Vector<IPosition> vip;
-		IPosition ip0 = new Position(0, 0);
-		IPosition ip1 = new Position(1, 1);
-		IPosition ip2 = new Position(2, 2);
-
 		try {
-			ca.addPI(0, 0);
-			ca.addPI(1, 1);
-			ca.addPI(2, 2);
-
-			vip = ca.getListOfPI();
-			assertTrue(vip.elementAt(0).getXPosition() == ip0.getXPosition());
-			assertTrue(vip.elementAt(0).getYPosition() == ip0.getYPosition());
-
-			assertTrue(vip.elementAt(1).getXPosition() == ip1.getXPosition());
-			assertTrue(vip.elementAt(1).getYPosition() == ip1.getYPosition());
-
-			assertTrue(vip.elementAt(2).getXPosition() == ip2.getXPosition());
-			assertTrue(vip.elementAt(2).getYPosition() == ip2.getYPosition());
-
-		} catch (SyntaxErrorException e) {
-			System.err.println("Erreur");
+			ca.removeAttribute(attribute);
+			assertTrue(ca.getListOfAttrSize() == 1);
+			ca.removeAttribute(attribute2);
+			assertTrue(ca.getListOfAttrSize() == 0);
+		} catch (ModelException e) {
+			fail(e.toString());
 		}
 	}
 
+	/**
+	 * Test method for {@link fr.lip6.move.coloane.interfaces.model.Arc#removeAttribute(int)}
+	 */
+	public final void testRemoveAttributeInt() {
+	IAttribute attribute = new ConcreteAttribute(ATTNAME, ATTVALUE, ATTREFID);
+		IAttribute attribute2 = new ConcreteAttribute(ATTNAME, ATTVALUE2, ATTREFID);
+		ca.addAttribute(attribute2);
+		ca.addAttribute(attribute);
+		try {
+			ca.removeAttribute(0);
+		} catch (ModelException e) {
+			fail(e.toString());
+		}
+
+		assertTrue(ca.getListOfAttrSize() == 1);
+	}
+
+	/**
+	 * Test method for {@link fr.lip6.move.coloane.interfaces.model.Arc#getListOfAttr()}
+	 */
+	public final void testGetListOfAttr() {
+		Vector<IAttribute> via;
+		IAttribute at1 = new ConcreteAttribute(ATTNAME, ATTVALUE, ATTREFID);
+		IAttribute at2 = new ConcreteAttribute(ATTNAME, ATTVALUE2, ATTREFID);
+		IAttribute at3 = new ConcreteAttribute(ATTNAME, ATTVALUE3, ATTREFID);
+
+		ca.addAttribute(at1);
+		ca.addAttribute(at2);
+		ca.addAttribute(at3);
+
+		via = ca.getListOfAttr();
+		assertTrue(via.elementAt(0).equals(at1));
+		assertTrue(via.elementAt(1).equals(at2));
+		assertTrue(via.elementAt(2).equals(at3));
+
+		// Pour la recuperation d'un attribut en particulier
+		assertTrue(ca.getNthAttr(2).equals(at3));
+	}
+
+	/**
+	 * Test method for {@link fr.lip6.move.coloane.interfaces.model.Arc#addPI(int, int)}
+	 */
 	public final void testAddPIIntInt() {
 		try {
 			ca.addPI(0, 0);
 			ca.addPI(1, 1);
 			assertTrue(ca.getListOfPI().size() == 2);
-		} catch (SyntaxErrorException e) {
-			System.out.println("Erreur dans testAddPIIntInt");
+		} catch (ModelException e) {
+			fail(e.toString());
 		}
 
+		// Ajout d'un PI qui existe deja... Doit lever une exception
+		try {
+			ca.addPI(1, 1);
+			fail("Erreur : Il ne doit pas etre possible d'ajouter un PI deja existant");
+		} catch (Exception e) {
+			assertTrue(e instanceof ModelException);
+		}
 	}
 
+	/**
+	 * Test method for {@link fr.lip6.move.coloane.interfaces.model.Arc#addPI(int, int, int)}
+	 */
 	public final void testAddPIIntIntInt() {
 		try {
 			ca.addPI(0, 0, 0);
 			ca.addPI(1, 1, 1);
 			assertTrue(ca.getListOfPI().size() == 2);
-		} catch (SyntaxErrorException e) {
-			System.out.println("Erreur dans testAddPIIntInt");
+		} catch (ModelException e) {
+			fail(e.toString());
 		}
 
+		// Ajout d'un PI qui existe deja... Doit lever une exception
+		try {
+			ca.addPI(1, 1, 0);
+			fail("Erreur : Il ne doit pas etre possible d'ajouter un PI deja existant");
+		} catch (Exception e) {
+			assertTrue(e instanceof ModelException);
+		}
 	}
 
+	/**
+	 * Test method for {@link fr.lip6.move.coloane.interfaces.model.Arc#removePI(int, int)}
+	 */
 	public final void testRemovePIIntInt() {
 		try {
 			ca.addPI(0, 0);
@@ -227,13 +260,26 @@ public class ConcreteArcTest extends TestCase {
 			assertTrue(ca.getListOfPI().size() == 2);
 			ca.removePI(0, 0);
 			assertTrue(ca.getListOfPI().size() == 1);
+
+			// On essaye d'ajouter un 0,0 ... Ca doti passer
+			try {
+				ca.addPI(0, 0);
+				assertTrue(ca.getListOfPI().size() == 2);
+			} catch (ModelException e) {
+				fail(e.toString());
+			}
+
+			ca.removePI(0, 0);
 			ca.removePI(1, 1);
 			assertTrue(ca.getListOfPI().size() == 0);
-		} catch (SyntaxErrorException e) {
-			System.out.println("Erreur dans testRemovePIIntInt");
+		} catch (ModelException e) {
+			fail(e.toString());
 		}
 	}
 
+	/**
+	 * Test method for {@link fr.lip6.move.coloane.interfaces.model.Arc#removePI(int)}
+	 */
 	public final void testRemovePIInt() {
 		try {
 			ca.addPI(0, 1);
@@ -241,41 +287,38 @@ public class ConcreteArcTest extends TestCase {
 			assertTrue(ca.getListOfPI().size() == 2);
 			ca.removePI(0);
 			assertTrue(ca.getListOfPI().size() == 1);
+
+			// On essaye d'ajouter un 0,1 ... Ca doti passer
+			try {
+				ca.addPI(0, 1);
+				assertTrue(ca.getListOfPI().size() == 2);
+			} catch (ModelException e) {
+				fail(e.toString());
+			}
+
+			ca.removePI(0);
 			ca.removePI(0);
 			assertTrue(ca.getListOfPI().size() == 0);
-		} catch (SyntaxErrorException e) {
-			System.out.println("Erreur dans testRemovePIIntInt");
+		} catch (ModelException e) {
+			fail(e.toString());
 		}
 
 	}
 
+	/**
+	 * Test method for {@link fr.lip6.move.coloane.interfaces.model.Arc#modifyPI(int, int, int)}
+	 */
 	public final void testModifyPI() {
 		try {
-			IPosition pi = new Position(0, 0);
 			ca.addPI(0, 0, 0);
 			ca.addPI(1, 1, 1);
-			ca.addPI(3, 5, 2);
-			assertTrue(ca.getNthPI(0).getXPosition() == pi.getXPosition());
-			assertTrue(ca.getNthPI(0).getYPosition() == pi.getYPosition());
+			ca.addPI(0, 1, 2);
+
 			ca.modifyPI(0, 2, 2);
-			assertTrue(ca.getNthPI(0).getXPosition() != pi.getXPosition());
-			assertTrue(ca.getNthPI(0).getYPosition() != pi.getYPosition());
-		} catch (SyntaxErrorException e) {
-			System.out.println("Erreur dans testModifyPI");
-		}
-	}
-
-	public final void testGetNthPI() {
-		try {
-			IPosition pi = new Position(0, 0);
-			ca.addPI(0, 0, 0);
-			ca.addPI(1, 1, 1);
-			ca.addPI(3, 5, 2);
-			assertTrue(ca.getNthPI(0).getXPosition() == pi.getXPosition());
-			assertTrue(ca.getNthPI(0).getYPosition() == pi.getYPosition());
-
-		} catch (SyntaxErrorException e) {
-			System.err.println("Erreur");
+			assertTrue(ca.getNthPI(2).getXPosition() == 0);
+			assertTrue(ca.getNthPI(0).getYPosition() == 2);
+		} catch (ModelException e) {
+			fail(e.toString());
 		}
 	}
 }
--- fr.lip6.move.coloane.interfaces/trunk/test/fr/lip6/move/coloane/interfaces/concretemodelTest/ConcreteAttributeTest.java
+++ fr.lip6.move.coloane.interfaces/trunk/test/fr/lip6/move/coloane/interfaces/concretemodelTest/ConcreteAttributeTest.java
@@ -1,20 +1,20 @@
-/**
- *
- */
 package fr.lip6.move.coloane.interfaces.concretemodelTest;
 
-
 import fr.lip6.move.coloane.interfaces.concretemodel.ConcreteAttribute;
-import fr.lip6.move.coloane.interfaces.model.Attribute;
 import junit.framework.TestCase;
 
 /**
- * @author cdcharles
- *
+ * Test des attributs du modele generique
  */
+
 public class ConcreteAttributeTest extends TestCase {
 
 	private ConcreteAttribute ca;
+
+	static final String ATTNAME = "Attribute";
+	static final String ATTVALUE = "hello";
+	static final int ATTREFID = 0;
+
 	/**
 	 * @param name
 	 */
@@ -25,15 +25,15 @@ public class ConcreteAttributeTest extends TestCase {
 	/* (non-Javadoc)
 	 * @see junit.framework.TestCase#setUp()
 	 */
-	protected void setUp() throws Exception {
+	protected final void setUp() throws Exception {
 		super.setUp();
-		ca = new ConcreteAttribute("Attribut", "Concret", 0);
+		ca = new ConcreteAttribute(ATTNAME, ATTVALUE, ATTREFID);
 	}
 
 	/* (non-Javadoc)
 	 * @see junit.framework.TestCase#tearDown()
 	 */
-	protected void tearDown() throws Exception {
+	protected final void tearDown() throws Exception {
 		super.tearDown();
 		ca = null;
 	}
@@ -48,91 +48,39 @@ public class ConcreteAttributeTest extends TestCase {
 	/**
 	 * Test method for {@link fr.lip6.move.coloane.interfaces.concretemodel.ConcreteAttribute#ConcreteAttribute(java.lang.String, java.lang.String, int)}.
 	 */
-	public void testConcreteAttribute() {
-		assertTrue(ca.getName().equals("Attribut"));
-		assertTrue(ca.getValue().equals("Concret"));
+	public final void testConcreteAttribute() {
+		assertTrue(ca.getName().equals(ATTNAME));
+		assertTrue(ca.getValue().equals(ATTVALUE));
 		assertTrue(ca.getRefId() == 0);
-	}
-
-	/**
-	 * Test method for {@link fr.lip6.move.coloane.interfaces.model.Attribute#Attribute(java.lang.String, java.lang.String, int)}.
-	 */
-	public void testAttribute() {
-		Attribute a = new ConcreteAttribute("Attribut", "abstrait", 1);
-		assertTrue(a.getName().equals("Attribut"));
-		assertTrue(a.getValue().equals("abstrait"));
-		assertTrue(a.getRefId() == 1);
-	}
-
-
-	/**
-	 * Test method for {@link fr.lip6.move.coloane.interfaces.model.Attribute#getName()}.
-	 */
-	public void testGetName() {
-		ConcreteAttribute cb = new ConcreteAttribute("Attribut", "concret", 0);
-		assertTrue(cb.getName().equals("Attribut"));
 	}
 
 	/**
 	 * Test method for {@link fr.lip6.move.coloane.interfaces.model.Attribute#setPosition(int, int)}.
 	 */
-	public void testSetPosition() {
+	public final void testSetPosition() {
 		assertTrue(ca.getXPosition() == 0);
 		assertTrue(ca.getYPosition() == 0);
 		ca.setPosition(1, 1);
 		assertTrue(ca.getXPosition() == 1);
 		assertTrue(ca.getYPosition() == 1);
-	}
-
-	/**
-	 * Test method for {@link fr.lip6.move.coloane.interfaces.model.Attribute#getXPosition()}.
-	 */
-	public void testGetXPosition() {
-		assertTrue(ca.getXPosition() == 0);
-		ca.setPosition(1, 1);
-		assertTrue(ca.getXPosition() == 1);
-	}
-
-	/**
-	 * Test method for {@link fr.lip6.move.coloane.interfaces.model.Attribute#getYPosition()}.
-	 */
-	public void testGetYPosition() {
-		assertTrue(ca.getYPosition() == 0);
-		ca.setPosition(1, 1);
-		assertTrue(ca.getYPosition() == 1);
-	}
-
-	/**
-	 * Test method for {@link fr.lip6.move.coloane.interfaces.model.Attribute#getRefId()}.
-	 */
-	public void testGetRefId() {
-		assertTrue(ca.getRefId() == 0);
-
 	}
 
 	/**
 	 * Test method for {@link fr.lip6.move.coloane.interfaces.model.Attribute#setRefId(int)}.
 	 */
-	public void testSetRefId() {
+	public final void testSetRefId() {
 		assertTrue(ca.getRefId() == 0);
 		ca.setRefId(1);
 		assertTrue(ca.getRefId() == 1);
 	}
 
+	static final String ATTVALUE2 = "Bye";
 	/**
 	 * Test method for {@link fr.lip6.move.coloane.interfaces.model.Attribute#setValue(java.lang.String)}.
 	 */
-	public void testSetValue() {
-		assertTrue(ca.getValue().equals("Concret"));
-		ca.setValue("c");
-		assertTrue(ca.getValue().equals("c"));
+	public final void testSetValue() {
+		assertTrue(ca.getValue().equals(ATTVALUE));
+		ca.setValue(ATTVALUE2);
+		assertTrue(ca.getValue().equals(ATTVALUE2));
 	}
-
-	/**
-	 * Test method for {@link fr.lip6.move.coloane.interfaces.model.Attribute#getValue()}.
-	 */
-	public void testGetValue() {
-		assertTrue(ca.getValue().equals("Concret"));
-	}
-
 }
