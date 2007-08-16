65ab7e12a5dc467981f4cdbfa8974e065fbb7ba8
emasliah <emasliah@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2007-08-16 12:11:24 +0000

    Creation d'un nouveau dossier pour une nouvelle architecture de coloane sur disque

---
+++ sandbox/emasliah/coloane/trunk/core/src/fr/lip6/move/coloane/ui/figures/IArcFigure.java
@@ -0,0 +1,8 @@
+package fr.lip6.move.coloane.ui.figures;
+
+import org.eclipse.draw2d.Connection;
+import org.eclipse.draw2d.IFigure;
+
+public interface IArcFigure extends IFigure, Connection {
+
+}
---
+++ sandbox/emasliah/coloane/trunk/interfaces/test/fr/lip6/move/coloane/interfaces/concretemodel/ConcreteModel.java
@@ -0,0 +1,30 @@
+package fr.lip6.move.coloane.interfaces.concretemodel;
+
+import fr.lip6.move.coloane.interfaces.exceptions.SyntaxErrorException;
+import fr.lip6.move.coloane.interfaces.model.Model;
+
+import java.util.Vector;
+
+/**
+ * Model concret pour permettre les tests unitaires
+ */
+public class ConcreteModel extends Model {
+
+	/**
+	 * Constructeur
+	 */
+	public ConcreteModel() {
+
+	}
+
+	// Pas besoin de traduction pour cet objet
+	@Override
+	public final String[] translate() {
+		return null;
+	}
+
+	// Pas besoin de construction de modele
+	public final void buildModel(Vector<String> commands) throws SyntaxErrorException {
+		return;
+	}
+}
