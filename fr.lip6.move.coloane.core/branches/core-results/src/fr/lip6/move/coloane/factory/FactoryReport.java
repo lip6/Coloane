1f0fb57e50d6f2eae4eb142a58aa504cbc191632
cdcharles <cdcharles@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2007-08-16 11:47:30 +0000

    Tous les services passent par la Factory

--- fr.lip6.move.coloane.core/branches/core-results/src/fr/lip6/move/coloane/factory/FactoryReport.java
+++ fr.lip6.move.coloane.core/branches/core-results/src/fr/lip6/move/coloane/factory/FactoryReport.java
@@ -9,20 +9,27 @@ import fr.lip6.move.coloane.interfaces.objects.IResultsCom;
 
 /**
  * @author cdcharles
- *
+ * 
  */
 public class FactoryReport {
 
 	private IResultsCom resultCom;
-	
-	FactoryReport(IResultsCom resultCom){
+
+	public FactoryReport(IResultsCom resultCom) {
 		this.resultCom = resultCom;
 	}
-	
-	public IReport createReport(){
-		if(resultCom.getCmdRQ().equals("Petri Net Syntax Checker")){
-			 return new SyntaxCheckerReport(resultCom);			
+
+	public IReport createReport() {
+		if (resultCom.getCmdRQ().equals("Petri net syntax checker")
+				|| resultCom.getCmdRQ().equals("Compute structural bounds")
+				|| resultCom.getCmdRQ().equals("Is the net structuraly safe?")
+				|| resultCom.getCmdRQ().equals("Is the net structurally bounded?")
+				|| resultCom.getCmdRQ().equals("P-invariants")
+				|| resultCom.getCmdRQ().equals("P-positive invariants")
+				|| resultCom.getCmdRQ().equals("T-positive invariants")){
+			return new GenericServicesReport(resultCom);
 		}
 		return null;
+		 //return new DefaultReport();
 	}
 }
