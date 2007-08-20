15da7ab44b89d71903cb7a722d6273e994ce6f50
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2007-08-20 15:31:05 +0000

    Constants have been moved from IDialog to this public interface

--- fr.lip6.move.coloane.interfaces/trunk/src/fr/lip6/move/coloane/interfaces/objects/IDialogCom.java
+++ fr.lip6.move.coloane.interfaces/trunk/src/fr/lip6/move/coloane/interfaces/objects/IDialogCom.java
@@ -7,6 +7,36 @@ package fr.lip6.move.coloane.interfaces.objects;
  */
 public interface IDialogCom {
 
+	/** Dialogue standart */
+	int DLG_STANDARD = 1;
+	/** Dialogue warning */
+	int DLG_WARNING = 2;
+	/** Dialogue erreur */
+	int DLG_ERROR = 3;
+	/** Dialogue interractif */
+	int DLG_INTERACTIVE = 4;
+
+	/** Aucun bouton */
+	int DLG_NO_BUTTON = 1;
+	/** Un bouton OK */
+	int DLG_OK = 2;
+	/** Un bouton OK et un bouton Cancel */
+	int DLG_OK_CANCEL = 3;
+
+	/** Saisie autorisee */
+	int INPUT_AUTHORIZED = 1;
+	/** Saisie interdite */
+	int INPUT_FORBIDDEN = 2;
+	/** Saisie autorisee et echappement possible */
+	int INPUT_AND_ABORT_AUTHORIZED = 5;
+
+	/** Affichage mono-ligne */
+	int SINGLE_LINE = 1;
+	/** Affichage multi-ligne avec selection simple */
+	int MULTI_LINE_WITH_SINGLE_SELECTION = 2;
+	/** Affichage multi-ligne avec selection multiple */
+	int MULTI_LINE_WITH_MULTI_SELECTION = 5;
+
 	/**
 	 * Retourne le type des boutons affiches dans la boite dialogue
 	 * @return un indicateur
