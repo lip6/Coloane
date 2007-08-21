c91f426b9d1a8410a1475539716cf80efdfc0521
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2007-08-21 16:03:36 +0000

    Detailled exception : Now the menu exception could show the name and the parent's name of the bad menu

--- trunk/core/src/fr/lip6/move/coloane/exceptions/MenuNotFoundException.java
+++ trunk/core/src/fr/lip6/move/coloane/exceptions/MenuNotFoundException.java
@@ -2,9 +2,29 @@ package fr.lip6.move.coloane.exceptions;
 
 public class MenuNotFoundException extends Exception {
 
-	/**
-	 *
-	 */
 	private static final long serialVersionUID = 1L;
+
+	private String message;
+
+	/**
+	 * Constructeur simple
+	 */
+	public MenuNotFoundException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * Constructeur avec indication
+	 * @param name Nom du menu a construire
+	 * @param parent Nom du parent du menu
+	 */
+	public MenuNotFoundException(String name, String parent) {
+		this.message = parent + "->" + name;
+	}
+
+	@Override
+	public final String toString() {
+		return this.message;
+	}
 
 }
