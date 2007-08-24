5571158a9b7d10689c8fa1990ab89659fd8fd16f
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2007-08-24 23:52:58 +0000

    New figure for inhibitor arc. Fix for #150

--- trunk/core/src/fr/lip6/move/coloane/ui/figures/ArcFigure.java
+++ trunk/core/src/fr/lip6/move/coloane/ui/figures/ArcFigure.java
@@ -1,22 +1,50 @@
 package fr.lip6.move.coloane.ui.figures;
 
+import fr.lip6.move.coloane.ui.model.IArcGraphicInfo;
 import fr.lip6.move.coloane.ui.model.IArcImpl;
 
 import org.eclipse.draw2d.BendpointConnectionRouter;
+import org.eclipse.draw2d.ColorConstants;
 import org.eclipse.draw2d.PolygonDecoration;
 import org.eclipse.draw2d.PolylineConnection;
+import org.eclipse.draw2d.geometry.PointList;
 
 public class ArcFigure extends PolylineConnection implements IArcFigure {
 
-	private static final int SCALE1 = 4;
-	private static final int SCALE2 = 2;
+	private static final double SCALE1 = 0.8;
+	private static final double SCALE2 = 0.8;
 
 	public ArcFigure(IArcImpl arc) {
+		PolygonDecoration decoration = null;
 
-		// La decoration de l'arc
-		PolygonDecoration dec = new PolygonDecoration();
-		dec.setScale(SCALE1, SCALE2);
-		this.setTargetDecoration(dec);
+		// Choix de la decoration de l'arc
+		if (arc.getGraphicInfo().getFigureStyle() == IArcGraphicInfo.FIG_ARC_INHIBITOR) {
+			decoration = new PolygonDecoration();
+
+			PointList decorationPointList = new PointList();
+			decorationPointList.addPoint(3, 3);
+			decorationPointList.addPoint(4, 1);
+			decorationPointList.addPoint(4, -1);
+			decorationPointList.addPoint(3, -3);
+			decorationPointList.addPoint(1, -4);
+			decorationPointList.addPoint(-1, -4);
+			decorationPointList.addPoint(-3, -3);
+			decorationPointList.addPoint(-4, -1);
+			decorationPointList.addPoint(-4, 1);
+			decorationPointList.addPoint(-3, 3);
+			decorationPointList.addPoint(-1, 4);
+			decorationPointList.addPoint(1, 4);
+
+			decoration.setTemplate(decorationPointList);
+			decoration.setFill(true);
+			decoration.setBackgroundColor(ColorConstants.white);
+			decoration.setScale(SCALE1, SCALE2);
+		} else {
+			decoration = new PolygonDecoration();
+			decoration.setTemplate(PolygonDecoration.TRIANGLE_TIP);
+			decoration.setScale(3, 3);
+		}
+		this.setTargetDecoration(decoration);
 
 		// La liste des points d'inflexion de l'arc
 		this.setConnectionRouter(new BendpointConnectionRouter());
