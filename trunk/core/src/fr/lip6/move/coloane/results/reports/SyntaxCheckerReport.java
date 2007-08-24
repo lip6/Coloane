b8c4efc611db0fdc2cbb83cce0aa95dd5857b626
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2007-08-24 12:05:15 +0000

    The report now take into account special errors and the correct ending of the tool

--- trunk/core/src/fr/lip6/move/coloane/results/reports/SyntaxCheckerReport.java
+++ trunk/core/src/fr/lip6/move/coloane/results/reports/SyntaxCheckerReport.java
@@ -36,6 +36,11 @@ public class SyntaxCheckerReport implements IReport {
 	private void buildReport() {
 		String description = "";
 
+		// Si il n'y a pas de resultat : Aucun probleme !
+		if (this.resultCom.getSubResults().size() == 0) {
+			this.resultList.add(new Result("Your model is correct !", ""));
+		}
+
 		// Parcours de tous les DE-FE
 		for (SubResultsCom sr : this.resultCom.getSubResults()) {
 			String label = sr.getCmdRT().get(0);
@@ -44,6 +49,8 @@ public class SyntaxCheckerReport implements IReport {
 				description = "This place is unnamed";
 			} else if (label.equalsIgnoreCase("List of unnamed transitions.")) {
 				description = "This transition is unnamed";
+			} else {
+				for (String desc : sr.getCmdRT()) {	description += desc; }
 			}
 
 			// Parcours de tous les objets mis en valeur
