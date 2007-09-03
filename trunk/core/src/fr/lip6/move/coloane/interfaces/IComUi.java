0ce49101e2c7b47708ea2c09019e0be42b3cc64d
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2007-09-03 17:02:35 +0000

    A lot of work of documentation to do here...
    No great work. I'm not proud of it ! :(

--- trunk/core/src/fr/lip6/move/coloane/interfaces/IComUi.java
+++ trunk/core/src/fr/lip6/move/coloane/interfaces/IComUi.java
@@ -1,11 +1,22 @@
 package fr.lip6.move.coloane.interfaces;
 
+/**
+ * Toutes les methodes qui doivent etre implementees par la COM pour L'UI
+ */
 public interface IComUi {
 
-	/** Demande d'un service a la plateforme FK */
-	void askForService(String rootMenuName, String parentName, String serviceName);
+	/**
+	 * Demande d'un service a la plateforme
+	 * @param rootMenuName Le menu racine
+	 * @param refernceName Le menu de reference (qui n'est pas forcement le parent)
+	 * @param serviceName Le service a executer
+	 */
+	void askForService(String rootMenuName, String refernceName, String serviceName);
 
-	/** Transmet les reponses obtenues via une boite de dialogue */
-	void getDialogAnswers(IDialogResult results);
+	/**
+	 * Transmission des resultats d'une boite de dialogue a la plateforme
+	 * @param results Les resultats sous la forme d'un {@link IDialogResult}
+	 */
+	void sendDialogAnswers(IDialogResult results);
 
 }
--- trunk/core/src/fr/lip6/move/coloane/interfaces/IUiCom.java
+++ trunk/core/src/fr/lip6/move/coloane/interfaces/IUiCom.java
@@ -27,4 +27,11 @@ public interface IUiCom {
 	void setResults(String serviceName, IResultsCom result);
 
 	void printResults();
+
+	/**
+	 * Permet la modification des menus de Coloane permettant l'authentification
+	 * @param authentication Est-ce que l'authentification est OK ?
+	 * @param session Est-ce que une session est ouverte ?
+	 */
+	void platformState(boolean authentication, int session);
 }
