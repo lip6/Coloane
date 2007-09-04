aae795fd14ccafd051b964f1c270d39cad86c26f
linard <linard@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2007-09-04 15:38:45 +0000

    Add generic Visitor base class using reflection

---
+++ branches/apicami-antlr/src/fr/lip6/move/coloane/api/cami/Visitor.java
@@ -0,0 +1,17 @@
+package fr.lip6.move.coloane.api.cami;
+
+public abstract class Visitor<Base, Result> {
+
+	public abstract Result defaultVisit(Base o);
+	
+	public Result visit(Base o) {
+		try {
+			Class<? extends Visitor> self = this.getClass();
+			return (Result) self.getMethod("visitExact", o.getClass()).invoke(this, o);
+		} catch (Exception e) {
+			System.err.println("No method visit found for type " + o.getClass().getName() + " of visitor " + this.getClass().getName());
+			return this.defaultVisit(o);
+		}
+	}
+		
+}
