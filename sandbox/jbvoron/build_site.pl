b5e840d37c8e07569452dc7cfdb645106fb8ac61
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2007-09-06 12:14:03 +0000

    buil_site script needs less parameters and uses the last file generates by maven

--- sandbox/jbvoron/build_site.pl
+++ sandbox/jbvoron/build_site.pl
@@ -8,14 +8,13 @@ diagnostics::enable();
 # Author : Jean-Baptiste Voron (LIP6/MoVe)
 # Description : Rewrite site.xml to produce night-builds products
 #
-# Use : perl build_site.pl <SITE FILE> <FEATURE VERSION> <FEATURE PATH>
+# Use : perl build_site.pl <SITE FILE> <FEATURE DIR>
 
 use XML::Twig;
 
 # Fetch paramaters
 my $sitefile = shift;
-my $versionfeature = shift;
-my $pathfeature = shift;
+my $dirfeature = shift;
 
 # Initialize parser
 my $xml = XML::Twig->new(); 
@@ -24,8 +23,16 @@ $xml->parsefile($sitefile);
 # Find the root and the 'feature' element
 my $root = $xml->root;
 my $feature = $root->first_child('feature');
-$feature->set_att(url => $pathfeature); 
-$feature->set_att(version => $versionfeature); 
+my $nameid = $feature->att('id');
+
+open (DESC, "<last_$nameid") or die "FAILURE !!! (last_$nameid)\n";
+my $lastversion = <DESC>;
+chomp $lastversion;
+print "Last version : $lastversion\n" if $debug;
+close(DESC);
+
+$feature->set_att(url => $dirfeature.$nameid."_".$lastversion.".jar"); 
+$feature->set_att(version => $lastversion); 
 
 # Openning feature.xml for writing
 $xml->print_to_file("resources/".$sitefile,pretty_print => 'indented');
