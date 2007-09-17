7574f638091c1f82703bf006431e519d89baafbb
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2007-09-17 15:19:40 +0000

    debug trace

--- tools/continuum/updateManifest.sh
+++ tools/continuum/updateManifest.sh
@@ -16,5 +16,6 @@ echo "Updating Manifest File..."
 # # Modifie la version du projet
 echo "Writing a new Manifest file !"
 perl -i -pe 's/^Bundle-Version: ([^\s]*)/Bundle-Version: $1.'$number'/' META-INF/MANIFEST.MF
+cat META-INF/MANIFEST.MF
 
 echo "Updating Manifest File complete..."
