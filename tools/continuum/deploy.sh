7ef4752c33a20bfe12261b1fc286fd650e80f1d0
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2007-09-17 12:42:07 +0000

    New command to write the build number inside the manifest file.
    Otherwise eclipse do not find plugins at runtime

--- tools/continuum/deploy.sh
+++ tools/continuum/deploy.sh
@@ -1,34 +1,42 @@
 #! /bin/sh
 if [ $# -ne 3 ]; then
-	echo "Il faut 3 arguments : le nom du jar, le buildNumber et le dossier des plugins"
+	echo "FAILURE: This script needs 3 arguments : Jar's name, Build number and Plugin's directory"
 	exit 1
 fi
 
 jar=$1
 rev=$2
-nightUpdatesPlugins=$3
+directory=$3
 
 if [ ! -f target/$jar ]; then 
-	echo "Le jar n'existe pas"
+	echo "FAILURE: Jar file does not exist !"
 	exit 0 
 fi
 
 if [ ! -f META-INF/MANIFEST.MF ]; then
-	echo "Le MANIFEST n'existe pas"
+	echo "FAILURE: Manifest file does not exist !"
 	exit 0
 fi
 
+# Recupere le nom du projet
+echo "Fetching information..."
 bundleSymbolicName=`grep ^Bundle-SymbolicName: META-INF/MANIFEST.MF | awk -F ' ' '{ print $2}' | tr -d "\r"`
-
 if [ `echo $bundleSymbolicName | grep ";"` ];then
 	bundleSymbolicName=`echo $bundleSymbolicName | awk -F ';' '{print $1}'`
 fi
 
+# Recupere la version du projet
 bundleVersion=`grep ^Bundle-Version: META-INF/MANIFEST.MF | awk -F ' ' '{ print $2}' | tr -d "\r"`
 
+# Modifie la version du projet
+echo "Writing a new Manifest file !"
+sed "/^Bundle-Version:/ s/:.*/: $bundleVersion.r$rev/" META-INF/MANIFEST.MF > META-INF/MANIFEST.MF
+
+# Affichage indicatif
 newjar=`echo $bundleSymbolicName\_$bundleVersion.r$rev.jar`
 
-echo "scp target/$jar coloane.rsr.lip6.fr:$nightUpdatesPlugins/$newjar"
+# Copie...
+#echo "scp target/$jar coloane.rsr.lip6.fr:$nightUpdatesPlugins/$newjar"
 `scp target/$jar coloane.rsr.lip6.fr:$nightUpdatesPlugins/$newjar`
 
 echo $bundleVersion.r$rev > last_$bundleSymbolicName
