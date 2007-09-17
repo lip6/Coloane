5367a1f49ed3268a28b41d4c414987055f392a04
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2007-09-17 14:46:01 +0000

    Use perl instead sed

--- tools/continuum/updateManifest.sh
+++ tools/continuum/updateManifest.sh
@@ -4,7 +4,7 @@ if [ $# -ne 1 ]; then
 	exit 1
 fi
 
-number=$3
+number=$1
 
 if [ ! -f META-INF/MANIFEST.MF ]; then
 	echo "FAILURE: Manifest file does not exist !"
@@ -13,12 +13,8 @@ fi
 
 echo "Updating Manifest File..."
 
-# Recupere la version du projet
-bundleVersion=`grep ^Bundle-Version: META-INF/MANIFEST.MF | awk -F ' ' '{ print $2}' | tr -d "\r"`
-echo "Actual Bundle Version : $bundleVersion"
-
 # # Modifie la version du projet
 echo "Writing a new Manifest file !"
-sed "/^Bundle-Version:/ s/:.*/: $bundleVersion.r$rev/" META-INF/MANIFEST.MF > META-INF/MANIFEST.MF
+perl -i -pe 's/^Bundle-Version: ([^\s]*)/Bundle-Version: $1.'$number'/' META-INF/MANIFEST.MF
 
 echo "Updating Manifest File complete..."
