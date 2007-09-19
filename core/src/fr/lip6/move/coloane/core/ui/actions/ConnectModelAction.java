e8fd572b67be189610ce4530bf89d6158f775399
jbvoron <Jean-Baptiste.Voron@lip6.fr>
2007-09-19 20:51:12 +0000

    Actions are now very simple. All complex stuff has been moved to the motor. Action have just to call the appropriate method.
    AuthenticationDialog has been adapted to new way to handle process (progressbar). Now even if information are false, the dialog returns. All information are saved into a dedicated structure : AuthenticationInformation

--- core/src/fr/lip6/move/coloane/core/ui/actions/ConnectModelAction.java
+++ core/src/fr/lip6/move/coloane/core/ui/actions/ConnectModelAction.java
@@ -1,60 +1,34 @@
 package fr.lip6.move.coloane.core.ui.actions;
 
-import fr.lip6.move.coloane.core.exceptions.ColoaneException;
 import fr.lip6.move.coloane.core.main.Coloane;
-import fr.lip6.move.coloane.core.motor.Motor;
-import fr.lip6.move.coloane.core.motor.session.Session;
-import fr.lip6.move.coloane.core.ui.MainPerspectiveFactory;
-import fr.lip6.move.coloane.core.ui.panels.HistoryView;
 
 import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
-import org.eclipse.ui.PartInitException;
 
+/**
+ * Demande de connexion de la session courante<br>
+ * Cette action est provoquee par un clic de l'utilisateur sur :
+ * <ul>
+ * 	<li>Le bouton dans la barre d'outils</li>
+ * 	<li>L'item dans le menu de la plateforme</li>
+ * </ul>
+ */
 public class ConnectModelAction implements IWorkbenchWindowActionDelegate {
-	private IWorkbenchWindow window;
 
 	/*
 	 * (non-Javadoc)
 	 * @see org.eclipse.ui.IWorkbenchWindowActionDelegate#init(org.eclipse.ui.IWorkbenchWindow)
 	 */
-	public final void init(IWorkbenchWindow w) {
-		this.window = w;
-	}
+	public final void init(IWorkbenchWindow w) { }
 
 	/*
 	 * (non-Javadoc)
 	 * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
 	 */
 	public final void run(IAction action) {
-
-		// La vue HISTORY doit etre initialisee
-		try {
-			window.getActivePage().showView(MainPerspectiveFactory.HISTORY_VIEW);
-		} catch (PartInitException e) {
-			MessageDialog.openError(window.getShell(), Messages.ConnectModelAction_0, Messages.ConnectModelAction_1);
-		}
-
-		HistoryView.getInstance().addLine(Messages.ConnectModelAction_2);
-		Session current = Motor.getInstance().getSessionManager().getCurrentSession();
-		if (current != null) {
-			// Le modele existe... On peut essayer de le connecter
-			HistoryView.getInstance().addText(Messages.ConnectModelAction_4);
-			Coloane.getLogger().fine("Session courante : " + current.getName()); //$NON-NLS-1$
-			try {
-				if (Motor.getInstance().openSession()) {
-					HistoryView.getInstance().addLine(Messages.ConnectModelAction_6);
-				} else {
-					HistoryView.getInstance().addLine(Messages.ConnectModelAction_9);
-				}
-			} catch (ColoaneException e) {
-				Coloane.getLogger().warning("Impossible de connecter le modele : " + e.getMessage()); //$NON-NLS-1$
-				HistoryView.getInstance().addLine(Messages.ConnectModelAction_9);
-			}
-		}
+		Coloane.getDefault().getMotor().openSession();
 	}
 
 	/*
--- core/src/fr/lip6/move/coloane/core/ui/actions/DisconnectModelAction.java
+++ core/src/fr/lip6/move/coloane/core/ui/actions/DisconnectModelAction.java
@@ -1,17 +1,20 @@
 package fr.lip6.move.coloane.core.ui.actions;
 
 import fr.lip6.move.coloane.core.main.Coloane;
-import fr.lip6.move.coloane.core.motor.Motor;
-import fr.lip6.move.coloane.core.motor.session.Session;
-import fr.lip6.move.coloane.core.ui.panels.HistoryView;
-
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 
+/**
+ * Demande de deconnexion de la session courante<br>
+ * Cette action est provoquee par un clic de l'utilisateur sur :
+ * <ul>
+ * 	<li>Le bouton dans la barre d'outils</li>
+ * 	<li>L'item dans le menu de la plateforme</li>
+ * </ul>
+ */
 public class DisconnectModelAction implements IWorkbenchWindowActionDelegate {
-
 	/*
 	 * (non-Javadoc)
 	 * @see org.eclipse.ui.IWorkbenchWindowActionDelegate#init(org.eclipse.ui.IWorkbenchWindow)
@@ -23,24 +26,18 @@ public class DisconnectModelAction implements IWorkbenchWindowActionDelegate {
 	 * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
 	 */
 	public final void run(IAction action) {
-		HistoryView.getInstance().addLine(Messages.DisconnectModelAction_2);
-		Session current = Motor.getInstance().getSessionManager().getCurrentSession();
-		if (current != null) {
-			// Le modele existe... On peut essayer de le connecter
-			HistoryView.getInstance().addText(Messages.DisconnectModelAction_4);
-			Coloane.getLogger().fine("Session courante : " + current.getName()); //$NON-NLS-1$
-			if (Motor.getInstance().closeSession()) {
-				HistoryView.getInstance().addLine(Messages.DisconnectModelAction_6);
-			} else {
-				HistoryView.getInstance().addLine(Messages.DisconnectModelAction_9);
-			}
-		}
+		Coloane.getDefault().getMotor().closeSession();
 	}
 
-	public void selectionChanged(IAction action, ISelection selection) {
-	}
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.IActionDelegate#selectionChanged(org.eclipse.jface.action.IAction, org.eclipse.jface.viewers.ISelection)
+	 */
+	public void selectionChanged(IAction action, ISelection selection) { }
 
-	public final void dispose() {
-		return;
-	}
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.IWorkbenchWindowActionDelegate#dispose()
+	 */
+	public final void dispose() { return; }
 }
