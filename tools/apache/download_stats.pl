361cfd88997c140c6ca7d260772fbeafe3994d08
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2007-10-26 15:53:21 +0000

    Add support for update (release) statistics

--- tools/apache/download_stats.pl
+++ tools/apache/download_stats.pl
@@ -28,7 +28,9 @@ sub pad_day {
 
 # Counter and Details
 my $counter = 0;
+my $counter_up = 0;
 my %details;
+my %details_up;
 
 # Some properties
 my $filedesc = "/coloane/logs/access.log";
@@ -59,7 +61,7 @@ while (<DESC>) {
 	my $ligne = $_;
 	chomp $ligne;
 	
-	# Pattern calculation
+	# Pattern calculation for night-updates
 	my $pattern = '^(\d+\.\d+\.\d+\.\d+) - - \['.$pattern_date.'.+\] \"GET \/night-updates\/features\/fr\.lip6\.move\.coloane\.feature.*200 ';
 	
 	if ($ligne =~ /$pattern/) {
@@ -68,12 +70,25 @@ while (<DESC>) {
 		if (exists $details{$ip}) { $details{$ip}++ } else { $details{$ip}=1; }
 		$counter++;
 	}
+
+	# Pattern calculation for updates
+	my $pattern_up = '^(\d+\.\d+\.\d+\.\d+) - - \['.$pattern_date.'.+\] \"GET \/updates\/features\/fr\.lip6\.move\.coloane\.feature.*200 ';
+	
+	if ($ligne =~ /$pattern_up/) {
+		my $ip = $1;
+		if ($ip =~ /132\.227\.76\.6/) { next; }
+		if (exists $details_up{$ip}) { $details_up{$ip}++ } else { $details_up{$ip}=1; }
+		$counter_up++;
+	}
+
 }
 
 # Output 
-print "@@ ".$pattern_date." : ".$counter." \n";
+print "@@ ".$pattern_date." : ".$counter." ".$counter_up."\n";
 for my $ip (keys %details) {
 	print "%% $ip -> ".$details{$ip}." \n";
 }
-
+for my $ip (keys %details_up) {
+	print "++ $ip -> ".$details_up{$ip}." \n";
+}
 close (DESC);
--- tools/apache/scan_stats.pl
+++ tools/apache/scan_stats.pl
@@ -28,7 +28,9 @@ sub pad_day {
 
 # Counter and Details
 my $counter = 0;
+my $counter_up = 0;
 my %details;
+my %details_up;
 
 # Some properties
 my $filedesc = "/coloane/logs/access.log";
@@ -68,12 +70,26 @@ while (<DESC>) {
 		if (exists $details{$ip}) { $details{$ip}++ } else { $details{$ip}=1; }
 		$counter++;
 	}
+	
+	# Pattern calculation for updates
+	my $pattern_up = '^(\d+\.\d+\.\d+\.\d+) - - \['.$pattern_date.'.+\] \"GET \/updates\/site\.xml.*200 ';
+	
+	if ($ligne =~ /$pattern_up/) {
+		my $ip = $1;
+		if ($ip =~ /132\.227\.76\.6/) { next; }
+		if (exists $details_up{$ip}) { $details_up{$ip}++ } else { $details_up{$ip}=1; }
+		$counter_up++;
+	}
+	
 }
 
 # Output 
-print "@@ ".$pattern_date." : ".$counter." \n";
+print "@@ ".$pattern_date." : ".$counter." ".$counter_up." \n";
 for my $ip (keys %details) {
 	print "%% $ip -> ".$details{$ip}." \n";
+}
+for my $ip (keys %details_up) {
+	print "%% $ip -> ".$details_up{$ip}." \n";
 }
 
 close (DESC);
