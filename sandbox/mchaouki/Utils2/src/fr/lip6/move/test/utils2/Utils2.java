5c1a0aec660966b9742374c970a1b16f4d95e953
mchaouki <mchaouki@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-01-17 14:26:37 +0000

    Creation d'une classe Utils2.java qui implemente une multiplication et une division.

---
+++ sandbox/mchaouki/Utils2/src/fr/lip6/move/test/utils2/Utils2.java
@@ -0,0 +1,11 @@
+package fr.lip6.move.test.utils2;
+
+public class Utils2 {
+	public double multiplication(double x, double y){
+		return x*y;
+	}
+	
+	public double division(double x, double y){
+		return x/y;
+	}
+}
