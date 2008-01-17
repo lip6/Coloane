aedf0e40b562195b08e850c18f8bc710d7763aa5
mchaouki <mchaouki@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-01-17 14:50:56 +0000

    Creation d'une classe ConvertFramework.java qui implemente les functions convertToXXX(prix), taxer(prix) et detaxer(prix).

---
+++ sandbox/mchaouki/ConvertFramework/src/fr/lip6/move/test/convertFramework/ConvertFramework.java
@@ -0,0 +1,38 @@
+package fr.lip6.move.test.convertFramework;
+
+import fr.lip6.move.test.utils1.Utils1;
+import fr.lip6.move.test.utils2.Utils2;
+
+public class ConvertFramework {
+	public double convertToDollar(double prix){
+		Utils2 utils2 = new Utils2();
+		return utils2.multiplication(prix, 1.5);
+	}
+	
+	public double convertToFranc(double prix){
+		Utils2 utils2 = new Utils2();
+		return utils2.multiplication(prix, 6.55);	}
+	
+	public double convertToLivre(double prix){
+		Utils2 utils2 = new Utils2();
+		return utils2.multiplication(prix, 1.33);	}
+	
+	public double convertToDirham(double prix){
+		Utils2 utils2 = new Utils2();
+		return utils2.multiplication(prix, 10);	}
+	
+	public double convertToYen(double prix){
+		Utils2 utils2 = new Utils2();
+		return utils2.multiplication(prix, 100);	}
+	
+	public double taxer(double prix){
+		Utils1 utils1 = new Utils1();
+		return utils1.addition(prix, 50);
+	}
+	
+	public double detaxer(double prix){
+		Utils1 utils1 = new Utils1();
+		return utils1.soustraction(prix, 50);	
+	}
+
+}
