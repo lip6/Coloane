7bd25c678a61a43905327692a6d3b227b2317b55
mchaouki <mchaouki@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-01-17 22:42:34 +0000

    Creation d'un point d'extension utilisant les 4 librairies : utils1.jar, utils2.jar, convertFramework.jar, convert.jar

---
+++ sandbox/mchaouki/Extension_Yen/src/org/monir/test/extension/yen/ConvertiseurImpl.java
@@ -0,0 +1,36 @@
+package org.monir.test.extension.yen;
+
+import org.monir.test.convert.Convertiseur;
+
+import fr.lip6.move.test.convert.tools.Tools;
+
+public class ConvertiseurImpl implements Convertiseur {
+	private double taux = 100;
+	public ConvertiseurImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public double convert(double prix) {
+		// TODO Auto-generated method stub
+		Tools tools = new Tools();
+		
+		double prixConvertie = tools.convertTo(prix, 4);
+		
+		System.out.println("Voicie le Prix convertie"+prixConvertie);
+		return prixConvertie;
+	}
+
+	@Override
+	public double getTaux() {
+		// TODO Auto-generated method stub
+		return taux;
+	}
+
+	@Override
+	public void setTaux(double taux) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
