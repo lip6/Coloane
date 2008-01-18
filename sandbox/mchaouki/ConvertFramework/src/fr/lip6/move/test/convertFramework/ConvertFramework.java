9e89453b0706ed2b30a77595f0a3445226f1abaf
mchaouki <mchaouki@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-01-18 12:07:11 +0000

    Correction indentation

--- sandbox/mchaouki/ConvertFramework/src/fr/lip6/move/test/convertFramework/ConvertFramework.java
+++ sandbox/mchaouki/ConvertFramework/src/fr/lip6/move/test/convertFramework/ConvertFramework.java
@@ -5,37 +5,27 @@ import fr.lip6.move.test.utils2.Utils2;
 
 public class ConvertFramework {
 	public double convertToDollar(double prix){
-		System.out.println("ToDollar");
 		Utils2 utils2 = new Utils2();
-		System.out.println("EndTo");
 		return utils2.multiplication(prix, 1.5);
 	}
 	
 	public double convertToFranc(double prix){
-		System.out.println("ToFranc");
 		Utils2 utils2 = new Utils2();
-		System.out.println("EndTo");
 		return utils2.multiplication(prix, 6.55);
 	}
 	
 	public double convertToLivre(double prix){
-		System.out.println("ToLivre");
 		Utils2 utils2 = new Utils2();
-		System.out.println("EndTo");
 		return utils2.multiplication(prix, 1.33);
 	}
 	
 	public double convertToDirham(double prix){
-		System.out.println("ToDirham");
 		Utils2 utils2 = new Utils2();
-		System.out.println("EndTo");
 		return utils2.multiplication(prix, 10);
 	}
 	
 	public double convertToYen(double prix){
-		System.out.println("ToYen");
 		Utils2 utils2 = new Utils2();
-		System.out.println("EndTo");
 		return utils2.multiplication(prix, 100);	
 	}
 	
