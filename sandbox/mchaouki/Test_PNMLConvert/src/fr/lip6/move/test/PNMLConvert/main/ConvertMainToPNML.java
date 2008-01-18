6e23b2e613c51b20a2efa1d256265b569ebe9579
mchaouki <mchaouki@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-01-18 17:04:18 +0000

    Creation d'un test.

---
+++ sandbox/mchaouki/Test_PNMLConvert/src/fr/lip6/move/test/PNMLConvert/main/ConvertMainToPNML.java
@@ -0,0 +1,13 @@
+package fr.lip6.move.test.PNMLConvert.main;
+
+import fr.lip6.move.test.PNMLConvert.convert.ConvertToPNML;
+
+public class ConvertMainToPNML {
+	public static void main(String[] args){
+		
+	ConvertToPNML convector = new ConvertToPNML();
+	
+	convector.convertToPNML(args);
+	
+	}
+}
