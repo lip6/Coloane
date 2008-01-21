4de93c8451dd80fbf1df3e697c6664a68b97d00b
bbarbouche <bbarbouche@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-01-21 14:34:50 +0000

    Mini-projet final,tout les problemes ont été reglé. Ceci est le projet pere:projet_maven3

---
+++ sandbox/bbarbouche/projet_maven3/src/main/java/fr/lip6/calcul/Operation.java
@@ -0,0 +1,29 @@
+package fr.lip6.calcul;
+
+public class Operation {
+
+	public int addition(int a,int b)
+	{
+			return a+b;
+	}
+	
+	public int soustraction(int a,int b)
+	{
+		return a-b;
+		
+	}
+	
+	public int division(int a,int b)
+	{
+		return a/b;
+	}
+	
+	public int multiplication(int a,int b)
+	{
+		return a*b;
+	}
+	
+	
+	
+	
+}
