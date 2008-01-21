d4ccd5ec241d2bc5fc9ad7d7f0eee8e68402a713
mchaouki <mchaouki@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-01-21 10:58:52 +0000

    Ajout de l'affichage des argumants.

--- sandbox/mchaouki/Test_PNMLConvert/src/fr/lip6/move/test/PNMLConvert/main/ConvertMainToPNML.java
+++ sandbox/mchaouki/Test_PNMLConvert/src/fr/lip6/move/test/PNMLConvert/main/ConvertMainToPNML.java
@@ -6,7 +6,9 @@ public class ConvertMainToPNML {
 	public static void main(String[] args){
 		
 	ConvertToPNML convector = new ConvertToPNML();
-	
+	for (int i=0; i< args.length;i++){
+		System.out.println("arguments "+i+": "+args[i]);
+	}
 	convector.convertToPNML(args);
 	
 	}
