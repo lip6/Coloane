232224fba98ac6d34f15c3daae019f169a63bcf2
mchaouki <mchaouki@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-01-22 18:21:54 +0000

    Creation d'un extension qui export le model courant au forat CAMI.

---
+++ branches/coloane-extensions/exportToExtensions/exportToCAMI/src/fr/lip6/move/coloane/extension/exportToCAMI/ExportToImpl.java
@@ -0,0 +1,28 @@
+package fr.lip6.move.coloane.extension.exportToCAMI;
+
+import fr.lip6.move.coloane.core.interfaces.IExportTo;
+import fr.lip6.move.coloane.core.motor.formalism.FormalismManager;
+import fr.lip6.move.coloane.core.ui.model.IModelImpl;
+
+public class ExportToImpl implements IExportTo {
+
+	public ExportToImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void export(IModelImpl modelAdapter, String fileName) throws Exception {
+		// TODO Auto-generated method stub
+		
+		/**
+		 * Creation d'un FormalismManager ???
+		 * 
+		 */
+		FormalismManager fm = new FormalismManager();
+		
+		/**
+		 * Appel la methode exportModel qui export le model courant au format CAMI
+		 */
+		fm.exportModel(modelAdapter, fileName);
+	}
+
+}
