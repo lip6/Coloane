33fec75f44a15272546a9a77efb3861033dd0dfe
mchaouki <mchaouki@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-01-22 18:40:32 +0000

    Ajout de l'utilisation de ma methode 'export'. Fin.

--- branches/coloane-extensions/core/src/fr/lip6/move/coloane/core/ui/actions/ExportTo.java
+++ branches/coloane-extensions/core/src/fr/lip6/move/coloane/core/ui/actions/ExportTo.java
@@ -1,10 +1,16 @@
 package fr.lip6.move.coloane.core.ui.actions;
 
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 
+import fr.lip6.move.coloane.core.extensions.ExportToExtension;
+import fr.lip6.move.coloane.core.interfaces.IExportTo;
+import fr.lip6.move.coloane.core.main.Coloane;
+import fr.lip6.move.coloane.core.ui.Editor;
 import fr.lip6.move.coloane.core.ui.dialogs.ExportToDilaog;
 
 public class ExportTo implements IWorkbenchWindowActionDelegate {
@@ -23,8 +29,42 @@ public class ExportTo implements IWorkbenchWindowActionDelegate {
 
 	public void run(IAction action) {
 		// TODO Auto-generated method stub
+		
+		// Verification si on est dans un Editor ???
+		if (!(window.getActivePage().getActiveEditor() instanceof Editor)) {
+			Coloane.showErrorMsg(Messages.ImportExportCAMI_0);
+			return;
+		}
+
+		// Recupere le modele courant
+		Editor editor = (Editor) window.getActivePage().getActiveEditor();
+
+		// Si le modele n'a pas ete sauvegarde... On ne peut pas exporter.
+		// On demande la sauvegarde a l'utilisateur
+		if (editor.isDirty()) {
+			Coloane.showWarningMsg(Messages.ImportExportCAMI_1);
+			return;
+		}
+		
+		int choix;
+		
 		ExportToDilaog exportDialog = new ExportToDilaog(window.getShell());
-		exportDialog.open();
+		choix = exportDialog.open();
+		
+		if (choix == Dialog.OK){
+			System.out.println("Creation d'un instance pour l'exportation au format:"+exportDialog.getFormat());
+			IExportTo exportateur = ExportToExtension.createConvertInstance(exportDialog.getFormat());
+			MessageDialog.openInformation(window.getShell(), "Export To...", " Votre fichier a bien etait exporter au format: "+exportDialog.getFormat());
+			
+			try {
+				exportateur.export(editor.getModel(), exportDialog.getOutputFile());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				MessageDialog.openError(window.getShell(), "Export To...", e.getMessage());
+			}
+			
+		}
 
 	}
 
