ff3e8fcb3c72dab63c31110c63635f0abdf32c2c
mchaouki <mchaouki@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-01-22 18:05:45 +0000

    Ajout throws Exception dans la signature de la methode export.

--- branches/coloane-extensions/core/src/fr/lip6/move/coloane/core/interfaces/IExportTo.java
+++ branches/coloane-extensions/core/src/fr/lip6/move/coloane/core/interfaces/IExportTo.java
@@ -3,5 +3,5 @@ package fr.lip6.move.coloane.core.interfaces;
 import fr.lip6.move.coloane.core.ui.model.IModelImpl;
 
 public interface IExportTo {
-	public void export(IModelImpl modeleCourant,String outputFile);
+	public void export(IModelImpl modeleCourant,String outputFile) throws Exception;
 }
