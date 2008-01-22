e29ec5771d85e2bdb7d6c1e0365874d952808b3b
mchaouki <mchaouki@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-01-22 13:22:33 +0000

    creation d'une extension EportToCami vide.

---
+++ sandbox/mchaouki/Extension_ExportToCami/src/fr/lip6/move/test/extension/exportToCami/ConversionImpl.java
@@ -0,0 +1,16 @@
+package fr.lip6.move.test.extension.exportToCami;
+
+import fr.lip6.move.test.cami.interfaceConvert.Conversion;
+
+public class ConversionImpl implements Conversion {
+
+	public ConversionImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void convert(String inputFile, String outputFile) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
