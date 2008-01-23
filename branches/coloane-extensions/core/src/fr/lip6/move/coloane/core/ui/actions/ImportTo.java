4c93c163b49a57f239786a535e0ab761022d3f9a
mchaouki <mchaouki@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-01-23 10:22:00 +0000

    Creation d'un items 'Import To...' dans le menu 'File'

---
+++ branches/coloane-extensions/core/src/fr/lip6/move/coloane/core/ui/actions/ImportTo.java
@@ -0,0 +1,35 @@
+package fr.lip6.move.coloane.core.ui.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+
+public class ImportTo implements IWorkbenchWindowActionDelegate {
+	/** 
+	 * Fenetre de travail
+	 */
+	private IWorkbenchWindow window;
+	
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void init(IWorkbenchWindow window) {
+		// TODO Auto-generated method stub
+		this.window = window;
+
+	}
+
+	public void run(IAction action) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void selectionChanged(IAction action, ISelection selection) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
