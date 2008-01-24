01ff770ae28266a3611f03e485ff67d03043b70e
mchaouki <mchaouki@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-01-24 11:14:28 +0000

    Modification de 'ImportTo' en 'ImportFrom'

--- branches/coloane-extensions/extensions/importToCAMI/src/fr/lip6/move/coloane/extension/importToCAMI/ImportToImpl.java
+++ branches/coloane-extensions/extensions/importToCAMI/src/fr/lip6/move/coloane/extension/importToCAMI/ImportToImpl.java
@@ -2,11 +2,11 @@ package fr.lip6.move.coloane.extension.importToCAMI;
 
 
 import fr.lip6.move.coloane.core.exceptions.ColoaneException;
-import fr.lip6.move.coloane.core.interfaces.IImportTo;
+import fr.lip6.move.coloane.core.interfaces.IImportFrom;
 import fr.lip6.move.coloane.core.motor.formalism.FormalismManager;
 import fr.lip6.move.coloane.core.ui.model.IModelImpl;
 
-public class ImportToImpl implements IImportTo {
+public class ImportToImpl implements IImportFrom {
 
 	public ImportToImpl() {
 		// TODO Auto-generated constructor stub
