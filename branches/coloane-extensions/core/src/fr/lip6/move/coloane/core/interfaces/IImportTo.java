532142908d3b0a642592386e17cea262cf7a20da
mchaouki <mchaouki@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-01-24 09:54:53 +0000

    Ajout de commentaires.

--- branches/coloane-extensions/core/src/fr/lip6/move/coloane/core/interfaces/IImportTo.java
+++ branches/coloane-extensions/core/src/fr/lip6/move/coloane/core/interfaces/IImportTo.java
@@ -3,5 +3,11 @@ package fr.lip6.move.coloane.core.interfaces;
 import fr.lip6.move.coloane.core.ui.model.IModelImpl;
 
 public interface IImportTo {
+	/**
+	 * Permet de récupérer un modele correspendant à la représention interne de Coloane
+	 * @param filePath chemin du fichier à importer
+	 * @return un modele correspendant à la représention interne de Coloane
+	 * @throws Exception toutes les exceptions susceptiblent d'étre engendrées et à afficher
+	 */
 	public IModelImpl importTo(String filePath) throws Exception;
 }
