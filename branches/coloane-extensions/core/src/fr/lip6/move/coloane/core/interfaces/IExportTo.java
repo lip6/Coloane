47c7e4b31618ff8ff6c26295d22b0e5ad543052d
mchaouki <mchaouki@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-01-24 09:27:58 +0000

    Ajouts de commentaires.

--- branches/coloane-extensions/core/src/fr/lip6/move/coloane/core/interfaces/IExportTo.java
+++ branches/coloane-extensions/core/src/fr/lip6/move/coloane/core/interfaces/IExportTo.java
@@ -3,5 +3,11 @@ package fr.lip6.move.coloane.core.interfaces;
 import fr.lip6.move.coloane.core.ui.model.IModelImpl;
 
 public interface IExportTo {
-	public void export(IModelImpl modeleCourant,String outputFile) throws Exception;
+	/**
+	 * Export le modele courant, dans un format précis, dans le fichier passé en paramétre.
+	 * @param modeleCourant modele courant, à exporter
+	 * @param filePath chemin du fichier dans lequel exporter le modele courant
+	 * @throws Exception toutes les exceptions susceptiblent d'étre engendrées et à afficher
+	 */
+	public void export(IModelImpl modeleCourant,String filePath) throws Exception;
 }
