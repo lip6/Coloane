cc4fa0dd98de27b435bc695e3895200dda951f9d
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-01-29 18:07:25 +0000

    Changes in comment lines
    The class importtoextension have been renamed importfromextension

--- branches/coloane-extensions/core/src/fr/lip6/move/coloane/core/extensions/ExportToExtension.java
+++ branches/coloane-extensions/core/src/fr/lip6/move/coloane/core/extensions/ExportToExtension.java
@@ -10,17 +10,17 @@ public class ExportToExtension {
 	/**
 	 * Attributs du point d'extension 'exports'
 	 */
-	private static final String EXTENSIN_POINT_ID = "fr.lip6.move.coloane.core.exports";
+	private static final String EXTENSION_POINT_ID = "fr.lip6.move.coloane.core.exports";
 	private static final String NAME_EXTENSION = "name";
 	private static final String CLASS_EXTENSION = "class";
 	
 	/**
-	 * Consulte le registre des extensions pour trouver les contributions au point d'extension 'EXTENSIN_POINT_ID'
-	 * et récupère la valeur de l'attribut 'NAME_EXTENSION' de chaque extension.
-	 * @return le tableau des noms des convertiseurs présents
+	 * Consulte le registre des extensions pour trouver les contributions au point d'extension 'EXTENSION_POINT_ID'
+	 * et recupere la valeur de l'attribut 'NAME_EXTENSION' de chaque extension.
+	 * @return le tableau des noms des convertiseurs presents
 	 */
 	public static String[] getAllNameExtensionConvert(){		
-		IConfigurationElement[] contributions = Platform.getExtensionRegistry().getConfigurationElementsFor(EXTENSIN_POINT_ID);
+		IConfigurationElement[] contributions = Platform.getExtensionRegistry().getConfigurationElementsFor(EXTENSION_POINT_ID);
 		String[] nomsConvertiseurs = new String[contributions.length];
 		for (int i = 0; i < contributions.length; i++) {
 			nomsConvertiseurs[i] = contributions[i].getAttribute(NAME_EXTENSION);
@@ -29,13 +29,13 @@ public class ExportToExtension {
 	}
 	
 	/**
-	 * Créer une instance de convertiseur
-	 * @param nomConvertiseur nom du convertiseur à instancier
+	 * Creer une instance de convertiseur
+	 * @param nomConvertiseur nom du convertiseur a instancier
 	 * @return un convertiseur
 	 * @throws CoreException Exception lors de la creation de une instance
 	 */
 	public static IExportTo createConvertInstance(String nomConvertiseur) throws CoreException{
-		IConfigurationElement[] contributions = Platform.getExtensionRegistry().getConfigurationElementsFor(EXTENSIN_POINT_ID);
+		IConfigurationElement[] contributions = Platform.getExtensionRegistry().getConfigurationElementsFor(EXTENSION_POINT_ID);
 		IConfigurationElement extensionConvertiseur = null;
 		for (int i = 0; i < contributions.length; i++) {
 			if(contributions[i].getAttribute(NAME_EXTENSION).equals(nomConvertiseur)) {
--- branches/coloane-extensions/core/src/fr/lip6/move/coloane/core/extensions/ImportFromExtension.java
+++ branches/coloane-extensions/core/src/fr/lip6/move/coloane/core/extensions/ImportFromExtension.java
@@ -6,21 +6,21 @@ import org.eclipse.core.runtime.Platform;
 
 import fr.lip6.move.coloane.core.interfaces.IImportFrom;
 
-public class ImportToExtension {
+public class ImportFromExtension {
 	/**
 	 * Attributs du point d'extension 
 	 */
-	private static final String EXTENSIN_POINT_ID = "fr.lip6.move.coloane.core.imports";
+	private static final String EXTENSION_POINT_ID = "fr.lip6.move.coloane.core.imports";
 	private static final String NAME_EXTENSION = "name";
 	private static final String CLASS_EXTENSION = "class";
 	
 	/**
-	 * Consulte le registre des extensions pour trouver les contributions au point d'extension 'EXTENSIN_POINT_ID'
-	 * et récupère la valeur de l'attribut 'NAME_EXTENSION' de chaque extension.
-	 * @return le tableau des noms des convertiseurs présents
+	 * Consulte le registre des extensions pour trouver les contributions au point d'extension 'EXTENSION_POINT_ID'
+	 * et recupere la valeur de l'attribut 'NAME_EXTENSION' de chaque extension.
+	 * @return le tableau des noms des convertiseurs presents
 	 */
 	public static String[] getAllNameExtensionConvert(){		
-		IConfigurationElement[] contributions = Platform.getExtensionRegistry().getConfigurationElementsFor(EXTENSIN_POINT_ID);
+		IConfigurationElement[] contributions = Platform.getExtensionRegistry().getConfigurationElementsFor(EXTENSION_POINT_ID);
 		String[] nomsConvertiseurs = new String[contributions.length];
 		for (int i = 0; i < contributions.length; i++) {
 			nomsConvertiseurs[i] = contributions[i].getAttribute(NAME_EXTENSION);
@@ -30,12 +30,12 @@ public class ImportToExtension {
 	
 	/**
 	 * Créer une instance de convertiseur.
-	 * @param nomConvertiseur nom du convertiseur à instancier
+	 * @param nomConvertiseur nom du convertiseur a instancier
 	 * @return un convertiseur
 	 * @throws CoreException Exception lors de la creation de une instance
 	 */
 	public static IImportFrom createConvertInstance(String nomConvertiseur) throws CoreException{
-		IConfigurationElement[] contributions = Platform.getExtensionRegistry().getConfigurationElementsFor(EXTENSIN_POINT_ID);
+		IConfigurationElement[] contributions = Platform.getExtensionRegistry().getConfigurationElementsFor(EXTENSION_POINT_ID);
 		IConfigurationElement extensionConvertiseur = null;
 		for (int i = 0; i < contributions.length; i++) {
 			if(contributions[i].getAttribute(NAME_EXTENSION).equals(nomConvertiseur)) {
