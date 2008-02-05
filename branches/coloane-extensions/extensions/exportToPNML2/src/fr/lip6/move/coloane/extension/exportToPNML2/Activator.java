902f2ee13d3605944a0947f13243f2ca0960f732
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-02-05 15:49:27 +0000

    Change project's name to map with others

--- branches/coloane-extensions/extensions/exportToPNML2/src/fr/lip6/move/coloane/extension/exportToPNML2/Activator.java
+++ branches/coloane-extensions/extensions/exportToPNML2/src/fr/lip6/move/coloane/extension/exportToPNML2/Activator.java
@@ -9,7 +9,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "fr.lip6.move.coloane.extenstion.exportToPNML2";
+	public static final String PLUGIN_ID = "fr.lip6.move.coloane.extension.exportToPNML2";
 
 	// The shared instance
 	private static Activator plugin;
