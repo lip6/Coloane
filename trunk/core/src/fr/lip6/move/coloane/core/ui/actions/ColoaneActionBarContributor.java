80e78f74ecde5edb4c283d0f98642aefe5ce5873
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-04-01 14:39:14 +0000

    Add alignment support (not fonctionnal yet)

--- trunk/core/src/fr/lip6/move/coloane/core/ui/actions/ColoaneActionBarContributor.java
+++ trunk/core/src/fr/lip6/move/coloane/core/ui/actions/ColoaneActionBarContributor.java
@@ -1,6 +1,8 @@
 package fr.lip6.move.coloane.core.ui.actions;
 
+import org.eclipse.draw2d.PositionConstants;
 import org.eclipse.gef.ui.actions.ActionBarContributor;
+import org.eclipse.gef.ui.actions.AlignmentRetargetAction;
 import org.eclipse.gef.ui.actions.DeleteRetargetAction;
 import org.eclipse.gef.ui.actions.GEFActionConstants;
 import org.eclipse.gef.ui.actions.RedoRetargetAction;
@@ -8,9 +10,12 @@ import org.eclipse.gef.ui.actions.UndoRetargetAction;
 import org.eclipse.gef.ui.actions.ZoomComboContributionItem;
 import org.eclipse.gef.ui.actions.ZoomInRetargetAction;
 import org.eclipse.gef.ui.actions.ZoomOutRetargetAction;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.jface.action.Separator;
 import org.eclipse.ui.actions.ActionFactory;
+import org.eclipse.ui.actions.RetargetAction;
 
 public class ColoaneActionBarContributor extends ActionBarContributor {
 
@@ -25,6 +30,15 @@ public class ColoaneActionBarContributor extends ActionBarContributor {
 		addRetargetAction(new DeleteRetargetAction());
 		addRetargetAction(new ZoomInRetargetAction());
 		addRetargetAction(new ZoomOutRetargetAction());
+		
+		addRetargetAction(new RetargetAction(GEFActionConstants.TOGGLE_GRID_VISIBILITY, "Grid View", IAction.AS_CHECK_BOX));
+		
+		addRetargetAction(new AlignmentRetargetAction(PositionConstants.LEFT));
+		addRetargetAction(new AlignmentRetargetAction(PositionConstants.CENTER));
+		addRetargetAction(new AlignmentRetargetAction(PositionConstants.RIGHT));
+		addRetargetAction(new AlignmentRetargetAction(PositionConstants.TOP));
+		addRetargetAction(new AlignmentRetargetAction(PositionConstants.MIDDLE));
+		addRetargetAction(new AlignmentRetargetAction(PositionConstants.BOTTOM));
 	}
 
 
@@ -39,6 +53,15 @@ public class ColoaneActionBarContributor extends ActionBarContributor {
 		addGlobalActionKey(ActionFactory.PASTE.getId());
 		addGlobalActionKey(ActionFactory.DELETE.getId());
 	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.part.EditorActionBarContributor#contributeToMenu(org.eclipse.jface.action.IMenuManager)
+	 */
+	@Override 
+	public void contributeToMenu(IMenuManager menuManager) { 
+		super.contributeToMenu(menuManager); 
+	} 
 
 	/*
 	 * (non-Javadoc)
@@ -48,10 +71,23 @@ public class ColoaneActionBarContributor extends ActionBarContributor {
 		toolBarManager.add(getAction(ActionFactory.UNDO.getId()));
 		toolBarManager.add(getAction(ActionFactory.REDO.getId()));
 		toolBarManager.add(getAction(ActionFactory.DELETE.getId()));
+		
 		toolBarManager.add(new Separator());
 		toolBarManager.add(getAction(GEFActionConstants.ZOOM_IN));
 		toolBarManager.add(getAction(GEFActionConstants.ZOOM_OUT));
 		toolBarManager.add(new ZoomComboContributionItem(getPage()));
+		
+		toolBarManager.add(new Separator());
+		toolBarManager.add(getAction(GEFActionConstants.ALIGN_TOP));
+		toolBarManager.add(getAction(GEFActionConstants.ALIGN_MIDDLE));
+		toolBarManager.add(getAction(GEFActionConstants.ALIGN_BOTTOM));
+		
+		toolBarManager.add(new Separator());
+		toolBarManager.add(getAction(GEFActionConstants.ALIGN_LEFT));
+		toolBarManager.add(getAction(GEFActionConstants.ALIGN_CENTER));
+		toolBarManager.add(getAction(GEFActionConstants.ALIGN_RIGHT));
+		
+		toolBarManager.add(new Separator());
+		toolBarManager.add(getAction(GEFActionConstants.TOGGLE_GRID_VISIBILITY));
 	}
-
 }
