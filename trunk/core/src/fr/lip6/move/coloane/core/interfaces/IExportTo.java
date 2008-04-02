80472c78c1de584631004da87a02a37424cd502c
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-04-02 19:18:31 +0000

    Typos in Javadoc

--- trunk/core/src/fr/lip6/move/coloane/core/interfaces/IExportTo.java
+++ trunk/core/src/fr/lip6/move/coloane/core/interfaces/IExportTo.java
@@ -5,10 +5,10 @@ import fr.lip6.move.coloane.core.ui.model.IModelImpl;
 
 public interface IExportTo {
 	/**
-	 * Export le modele courant, dans un format précis, dans le fichier passé en paramétre.
-	 * @param modeleCourant modele courant, à exporter
+	 * Export le modele courant, dans un format precis, dans le fichier passe en parametre.
+	 * @param modeleCourant modele courant a exporter
 	 * @param filePath chemin du fichier dans lequel exporter le modele courant
-	 * @throws ColoaneException toutes les exceptions susceptiblent d'étre engendrées et à afficher
+	 * @throws ColoaneException toutes les exceptions susceptiblent d'etre engendrees et a afficher
 	 */
 	public void export(IModelImpl modeleCourant,String filePath) throws ColoaneException;
 }
--- trunk/core/src/fr/lip6/move/coloane/core/interfaces/IImportFrom.java
+++ trunk/core/src/fr/lip6/move/coloane/core/interfaces/IImportFrom.java
@@ -5,10 +5,10 @@ import fr.lip6.move.coloane.core.ui.model.IModelImpl;
 
 public interface IImportFrom {
 	/**
-	 * Permet de récupérer un modele correspendant à la représention interne de Coloane
-	 * @param filePath chemin du fichier à importer
-	 * @return un modele correspendant à la représention interne de Coloane
-	 * @throws ColoaneException toutes les exceptions susceptiblent d'étre engendrées et à afficher
+	 * Permet de recuperer un modele correspendant a la represention interne de Coloane
+	 * @param filePath chemin du fichier a importer
+	 * @return un modele correspendant a la represention interne de Coloane
+	 * @throws ColoaneException toutes les exceptions susceptiblent d'etre engendrees et a afficher
 	 */
 	public IModelImpl importFrom(String filePath) throws ColoaneException;
 }
