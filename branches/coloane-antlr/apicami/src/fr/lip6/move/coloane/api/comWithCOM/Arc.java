f074d1a5c1c5487ab31d707c5fc7f95e65fd6b88
kbouarab <kbouarab@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-04-12 17:57:59 +0000

    complément sur nos interfaces

---
+++ branches/coloane-antlr/apicami/src/fr/lip6/move/coloane/api/comWithCOM/Arc.java
@@ -0,0 +1,5 @@
+package fr.lip6.move.coloane.api.comWithCOM;
+
+public class Arc {
+
+}
--- branches/coloane-antlr/apicami/src/fr/lip6/move/coloane/api/interfaces/IDomainTable.java
+++ branches/coloane-antlr/apicami/src/fr/lip6/move/coloane/api/interfaces/IDomainTable.java
@@ -1,5 +1,18 @@
 package fr.lip6.move.coloane.api.interfaces;
 
+import java.util.ArrayList;
+
+
+/**
+ * cette interface représente une table de domaines.
+ * @author KAHOO & UU
+ *
+ */
 public interface IDomainTable {
 
+	/**
+	 * Donne les objets contenus dans ma table des domaines.
+	 * @return le tableau de ces objets.
+	 */
+	ArrayList<IObjectDomainTable>  getObjectsDomainTable();
 }
