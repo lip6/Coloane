3e77b283cae2f4cd5d1f58b2f8e9f884bf20aceb
kbouarab <kbouarab@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-04-15 20:07:19 +0000

    openconnection -definition des interfaces fournies à coloane .

--- branches/coloane-antlr/apicami/src/fr/lip6/move/coloane/api/camiObject/Arc.java
+++ branches/coloane-antlr/apicami/src/fr/lip6/move/coloane/api/camiObject/Arc.java
@@ -5,31 +5,47 @@ import java.util.ArrayList;
 import fr.lip6.move.coloane.api.interfaces.IArc;
 import fr.lip6.move.coloane.api.interfaces.INode;
 
+/**
+ *cette classe implemente linterface IArc.
+ * @author kahoo & UU
+ *
+ */
 public class Arc implements IArc{
+	 /** le type de l'arc*/
+	private String arcType;
+
+	 /** les attributs*/
+	private ArrayList<String> attribute;
+
+	 /** l'inode de la fin*/
+	private INode endingNode;
+
+	 /** identifiant*/
+	private int id ;
+
+	 /** l'inode du debut*/
+	private INode startingNode;
+
+
 
 	public String getArcType() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.arcType;
 	}
 
-	public ArrayList<String> getAttribute() {
-		// TODO Auto-generated method stub
-		return null;
+	public ArrayList<String> getAttribute(){
+		return this.attribute;
 	}
 
 	public INode getEndingNode() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.endingNode;
 	}
 
 	public int getId() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.id;
 	}
 
 	public INode getStartingNode() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.startingNode;
 	}
 
 }
