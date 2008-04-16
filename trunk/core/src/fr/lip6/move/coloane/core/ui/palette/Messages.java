32979d1f095f5fcdd500011b11bd3216cb9109cd
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-04-16 15:30:26 +0000

    some keys are missing in the palette...

--- trunk/core/src/fr/lip6/move/coloane/core/ui/palette/Messages.java
+++ trunk/core/src/fr/lip6/move/coloane/core/ui/palette/Messages.java
@@ -3,15 +3,11 @@ package fr.lip6.move.coloane.core.ui.palette;
 import org.eclipse.osgi.util.NLS;
 
 public final class Messages extends NLS {
-	private static final String BUNDLE_NAME = "fr.lip6.move.coloane.core.ui.messages"; //$NON-NLS-1$
-	public static String Editor_1;
-	public static String Editor_3;
-	public static String ExportWizard_2;
-	public static String ModifyWorkspace_0;
+	private static final String BUNDLE_NAME = "fr.lip6.move.coloane.core.ui.palette.messages"; //$NON-NLS-1$
 	public static String PaletteFactory_4;
 	public static String PaletteFactory_5;
 	public static String PaletteFactory_6;
-	public static String UserInterface_1;
+
 	static {
 		// initialize resource bundle
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
