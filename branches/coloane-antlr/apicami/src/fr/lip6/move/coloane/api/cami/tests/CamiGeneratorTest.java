d217e4be26a882167e3f629a9ceb34c9874f4a8d
ybelattaf <ybelattaf@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-04-28 16:19:10 +0000

    tests Cami generator sur l'ouverture de la session

--- branches/coloane-antlr/apicami/src/fr/lip6/move/coloane/api/cami/tests/CamiGeneratorTest.java
+++ branches/coloane-antlr/apicami/src/fr/lip6/move/coloane/api/cami/tests/CamiGeneratorTest.java
@@ -1,6 +1,7 @@
 package fr.lip6.move.coloane.api.cami.tests;
 
 import junit.framework.TestCase;
+import fr.lip6.move.coloane.api.cami.CamiGenerator;
 
 public class CamiGeneratorTest extends TestCase {
 	protected void setUp() throws Exception {
@@ -10,4 +11,26 @@ public class CamiGeneratorTest extends TestCase {
 	protected void tearDown() throws Exception {
 		super.tearDown();
 	}
+
+	/**
+	 * tester la generation des commandes de
+	 * l'ouverture de la session
+	 */
+	public void testOpenSessionCmd(){
+
+		byte[] s = CamiGenerator.generateCmdOS("model_1.petri", "123456", "AMI-NET");
+		System.out.println(new String(s , 4, s.length-4));
+
+		s= CamiGenerator.generateCmdDI();
+        System.out.println(new String(s , 4, s.length-4));
+
+		s= CamiGenerator.generateCmdCI("interlocutor", 1);
+        System.out.println(new String(s , 4, s.length-4));
+
+		s= CamiGenerator.generateCmdFI();
+        System.out.println(new String(s, 4, s.length-4));
+
+	}
+
+
 }
