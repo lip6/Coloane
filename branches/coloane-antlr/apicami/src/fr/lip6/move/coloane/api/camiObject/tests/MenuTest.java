1030e07d5671b9bf662a12fe7df832f5df98cb24
kbouarab <kbouarab@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-04-30 11:12:53 +0000

    opensession _classe test de MENU

--- branches/coloane-antlr/apicami/src/fr/lip6/move/coloane/api/camiObject/tests/MenuTest.java
+++ branches/coloane-antlr/apicami/src/fr/lip6/move/coloane/api/camiObject/tests/MenuTest.java
@@ -1,6 +1,11 @@
 package fr.lip6.move.coloane.api.camiObject.tests;
 
+import java.util.ArrayList;
+
+import fr.lip6.move.coloane.api.camiObject.Menu;
+import fr.lip6.move.coloane.api.interfaces.IMenu;
 import junit.framework.TestCase;
+import java.util.*;
 
 public class MenuTest extends TestCase {
 
@@ -12,4 +17,32 @@ public class MenuTest extends TestCase {
 		super.tearDown();
 	}
 
+	public void testMenu() {
+		boolean activate = false;
+		ArrayList<IMenu> children = new ArrayList<IMenu> ();
+		boolean dialogAllowed = true;
+		String name = "AMI-NET";
+		String outputFormalism ="fd";
+		IMenu parent = new Menu();
+		int questionBehavior = 1;
+		int questionType = 5;
+		boolean stopAuthorized = false;
+		boolean valid = false;
+
+		IMenu pere = new Menu(parent,name,questionType,questionBehavior,valid,
+			dialogAllowed,stopAuthorized,
+			outputFormalism,activate,children);
+
+		this.assertEquals(false, pere.isActivate());
+		this.assertEquals(children, pere.getChildren());
+		this.assertEquals(true, pere.isDialogAllowed());
+		this.assertEquals("AMI-NET", pere.getName());
+		this.assertEquals("fd", pere.outputFormalism());
+		this.assertEquals(parent, pere.getParent());
+		this.assertEquals(1, pere.getQuestionBehavior());
+		this.assertEquals(5, pere.getQuestionType());
+		this.assertEquals(false, pere.stopAuthorized());
+		this.assertEquals(false, pere.isValid());
+
+	}
 }
