fef92ff0f93a9dd7b3996eba43002741883aa4c7
ybelattaf <ybelattaf@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-05-06 16:14:36 +0000

    Api + Logger

--- branches/coloane-antlr/apicami/src/fr/lip6/move/coloane/api/apiPackage/Api.java
+++ branches/coloane-antlr/apicami/src/fr/lip6/move/coloane/api/apiPackage/Api.java
@@ -1,8 +1,12 @@
 package fr.lip6.move.coloane.api.apiPackage;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import fr.lip6.move.coloane.api.interfaces.IApiConnection;
+import fr.lip6.move.coloane.api.log.ApiHandler;
 
 
 /**
@@ -17,9 +21,38 @@ public class Api {
 	//TODO set pour l'uiName et la version
 	static String uiName = "Coloane";
 	static String uiVersion = "X.x";
+	static Logger logger;
 
+
+	// sert à initialiser l'api
+	public static void initialize(){
+
+		/* initialiser le logger*/
+		logger = Logger.getLogger("fr.lip6.move.coloane.api");
+		logger.setLevel(Level.FINEST); // On loggue tout !
+		try {
+			logger.addHandler(new ApiHandler());
+		} catch (SecurityException e) {
+			System.err.println("Impossible d'initialiser le gestionnaire de logs sur fichier");
+		} catch (FileNotFoundException e) {
+			System.err.println("Impossible d'initialiser le gestionnaire de logs sur fichier");
+		} catch (IOException e) {
+			System.err.println("Impossible d'initialiser le gestionnaire de logs sur fichier");
+		}
+	}
+
+
+	/**
+	 *
+	 * @return
+	 * @throws IOException
+	 */
 	public static IApiConnection getApiConnection() throws IOException{
 		return new ApiConnection();
 	}
+
+
+
+
 
 }
