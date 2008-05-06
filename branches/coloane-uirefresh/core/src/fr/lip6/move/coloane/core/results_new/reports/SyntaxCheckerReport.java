5ddad30f38e70e1735f39b7103dfa46ce87a28c5
cdemoulins <cdemoulins@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-05-06 16:47:52 +0000

    Ajout du SyntaxCheckerReport

--- branches/coloane-uirefresh/core/src/fr/lip6/move/coloane/core/results_new/reports/SyntaxCheckerReport.java
+++ branches/coloane-uirefresh/core/src/fr/lip6/move/coloane/core/results_new/reports/SyntaxCheckerReport.java
@@ -2,21 +2,35 @@ package fr.lip6.move.coloane.core.results_new.reports;
 
 import java.util.List;
 
+import fr.lip6.move.coloane.core.main.Coloane;
 import fr.lip6.move.coloane.core.results_new.IResultTree;
 import fr.lip6.move.coloane.core.results_new.ResultTreeImpl;
+import fr.lip6.move.coloane.core.ui.model.IModelImpl;
 import fr.lip6.move.coloane.interfaces.objects.IResultsCom;
+import fr.lip6.move.coloane.interfaces.objects.SubResultsCom;
 
 public class SyntaxCheckerReport implements IReport {
 
 	@Override
 	public IResultTree build(IResultsCom result) {
-		return new ResultTreeImpl("Test SyntaxCheckerReport");
+		ResultTreeImpl root = new ResultTreeImpl(result.getQuestion());
+//		IModelImpl model = Coloane.getDefault().getMotor().getSessionManager().getCurrentSessionModel();
+		
+		for(SubResultsCom sub:result.getSubResults()) {
+			ResultTreeImpl node = new ResultTreeImpl(sub.getCmdRT().get(0));
+			for(String obj:sub.getCmdRO()) {
+				int id = Integer.valueOf(obj);
+				node.addChild(new ResultTreeImpl(id,obj));
+			}
+			root.addChild(node);
+		}
+		
+		return root;
 	}
 
 	@Override
 	public List<Integer> highlightNode(IResultsCom result) {
-		// TODO Auto-generated method stub
-		return null;
+		return new GenericReport().highlightNode(result);
 	}
 
 }
