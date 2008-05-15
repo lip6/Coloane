e86e4638f035bcf42d81bf91dfc3f8d3e12bfefa
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-05-15 15:17:26 +0000

    add a test to determine wether we are in night or release procedure

--- tools/continuum/deploy.sh
+++ tools/continuum/deploy.sh
@@ -1,23 +1,29 @@
-#le but de ce script est de modifier le nom du jar puis ensuite de le deplyer dans night-updates
-#deploy.sh ne s'execute que lorsque la commande mvn deploy est lancé sur le pom pere.
-#Dans le pom sont mentionnés les trois arguments de la commande:
-#-${build.finalName}.${packaging} :le nom du jar
-#-${buildNumber} le numero attribué par le buildnumber numero de revision
-#-/coloane/public/night-updates/plugins le chemin du dossier des plugins
-
-
 #! /bin/sh 
 
+# Auteur  : Jean-Baptiste Voron
+# Contact : jean-baptiste.voron@lip6.fr
+# Projet  : Coloane (http://coloane.lip6.fr) 
 
+# Le but de ce script est de modifier le nom du jar puis ensuite de le deployer au bon endroit
+# Le script ne s'execute que lorsque la commande mvn deploy est lancé sur le pom pere.
 
-if [ $# -ne 3 ]; then
-	echo "FAILURE: This script needs 3 arguments : Jar's name, Build number and Plugin's directory"
+# Dans le pom sont mentionnés les trois arguments de la commande:
+#  -${project.version} le numero de version du projet
+#  -${build.finalName}.${packaging} :le nom du jar
+#  -${buildNumber} le numero attribué par le buildnumber numero de revision
+#  -/coloane/public le chemin du dossier public
+
+# Controle des arguments
+if [ $# -ne 4 ]; then
+	echo "FAILURE: This script needs 4 arguments : <version of project>, <jar's final name>, <build number> and <public directory>"
 	exit 1
 fi
 
-jar=$1
-rev=$2
-directory=$3
+# Recuperation des arguments
+version=$1
+jar=$2
+rev=$3
+directory=$4
 
 if [ ! -f target/$jar ]; then 
 	echo "FAILURE: Jar file does not exist !"
@@ -32,20 +38,29 @@ fi
 # Recupere le nom du projet
 echo "Fetching information..."
 
-#bundleSymbolicName contiendra le deuxieme champs de la ligne contenant "Bundle-SymbolicName:...",chaque champs etant separé par un espace.
-#concretement pour Iinterface on va avoir :"fr.lip6.move.coloane.interfaces"
+# BundleSymbolicName contiendra le deuxieme champs de la ligne contenant "Bundle-SymbolicName:..."
 bundleSymbolicName=`grep ^Bundle-SymbolicName: META-INF/MANIFEST.MF | awk -F ' ' '{ print $2}' | tr -d "\r"`
-#Pour la condition suivante on teste quand meme si il y a un ";" en plus et on l'enleve si necessaire
-if [ `echo $bundleSymbolicName | grep ";"` ];then
+
+# Pour la condition suivante on teste quand meme si il y a un ";" en plus et on l'enleve si necessaire
+if [ `echo $bundleSymbolicName | grep ";"` ]; then
 	bundleSymbolicName=`echo $bundleSymbolicName | awk -F ';' '{print $1}'`
 fi
 
 # Recupere la version du projet
-#on recupere la version qu'il y a dans le manifest,on aura quelque chose du genre x.x.x.rxxxx
+# On recupere la version qu'il y a dans le manifest,on aura quelque chose du genre x.x.x.rxxxx
 bundleVersion=`grep ^Bundle-Version: META-INF/MANIFEST.MF | awk -F ' ' '{ print $2}' | tr -d "\r"`
 
 # Affichage indicatif
 newjar=`echo $bundleSymbolicName\_$bundleVersion.jar`
+
+# Calcul des chemins
+if [ `echo $version | grep "SNAPSHOT"` ]; then
+	branch=`echo night-updates`
+else
+	branch=`echo updates`
+fi
+
+newjar=`echo $directory/$branch/plugins`
 
 # Copie...
 cp target/$jar $directory/$newjar
