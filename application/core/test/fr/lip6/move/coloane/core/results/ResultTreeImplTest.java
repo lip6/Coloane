d8af91e89e0d4da3d947aef31beff41ab8213452
jbvoron <Jean-Baptiste.Voron@lip6.fr>
2008-06-01 15:58:12 +0000

    New test with Mock Objects

--- application/core/test/fr/lip6/move/coloane/core/results/ResultTreeImplTest.java
+++ application/core/test/fr/lip6/move/coloane/core/results/ResultTreeImplTest.java
@@ -1,18 +1,35 @@
 package fr.lip6.move.coloane.core.results;
 
+import fr.lip6.move.coloane.core.motor.session.ISessionManager;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
+import org.jmock.integration.junit4.JUnit4Mockery;
+
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(JMock.class)
 public class ResultTreeImplTest {
-	private IResultTree tree1, tree2, tree3, tree4, tree5;
+	private IResultTree root, tree2, tree3, tree4, tree5;
+
+	// Definition du Mock SessionManager
+	private Mockery context = new JUnit4Mockery();
+	private final ISessionManager manager = (ISessionManager) context.mock(ISessionManager.class);
 
 	@Before
 	public final void setUp() throws Exception {
-		tree1 = new ResultTreeImpl(1, "t1_elt1", "t1_elt2", "t1_elt3");
+		root = new ResultTreeImpl("t1_elt1", "t1_elt2", "t1_elt3");
+		root.setSessionManager(manager);
+
 		tree2 = new ResultTreeImpl(2, "t2_elt1", "t2_elt2", "t2_elt3");
 
 		List<Integer> tmpList = new ArrayList<Integer>();
@@ -20,59 +37,64 @@ public class ResultTreeImplTest {
 		tmpList.add(Integer.valueOf(5));
 		tmpList.add(Integer.valueOf(7));
 		tree3 = new ResultTreeImpl(tmpList, "t3_elt1", "t3_elt2", "t3_elt3");
-
 		tree4 = new ResultTreeImpl("t4_elt1");
 		tree5 = new ResultTreeImpl();
 	}
 
 	@Test
 	public final void testGetParent() {
-		tree1.addChild(tree2);
-		tree3.addChild(tree1);
-		assertTrue(tree1.getParent().equals(tree3));
-		assertTrue(tree2.getParent().equals(tree1));
+		root.addChild(tree2);
+		tree2.addChild(tree3);
+		assertTrue(tree2.getParent().equals(root));
+		assertTrue(tree3.getParent().equals(tree2));
 
-		/* Le tree3 n' pas de pere... C'estl a racine */
-		assertNull(tree3.getParent());
+		/* Le root1 n'a pas de pere... C'est la racine */
+		assertNull(root.getParent());
 	}
 
 	@Test
 	public final void testSetParent() {
 		/* Le lien doit etre fait dans le sens fils -> pere seulelement */
-		tree4.setParent(tree1);
-		assertTrue(tree4.getParent().equals(tree1)); /* sens correct */
-		assertFalse(tree1.getChildren().size() > 0);
+		tree4.setParent(root);
+		assertTrue(tree4.getParent().equals(root)); /* sens correct */
+		assertFalse(root.getChildren().size() > 0);
 	}
 
 	@Test
 	public final void testGetChildren() {
-		tree3.addChild(tree1);
+		root.addChild(tree3);
+		root.addChild(tree4);
 		tree3.addChild(tree2);
-		List<IResultTree> list = tree3.getChildren();
+		List<IResultTree> list = root.getChildren();
 		assertTrue(list.size() == 2);
-		assertTrue(list.get(0).equals(tree1));
-		assertTrue(list.get(1).equals(tree2));
-		assertFalse(tree1.getChildren().size() > 0);
+		assertTrue(list.get(0).equals(tree3));
+		assertTrue(list.get(1).equals(tree4));
+		assertFalse(tree4.getChildren().size() > 0);
+
+		assertTrue(tree3.getChildren().size() == 1);
+		assertTrue(tree3.getChildren().get(0).equals(tree2));
+
+		assertTrue(tree2.getChildren().size() == 0);
 	}
 
 	@Test
 	public final void testAddChild() {
-		tree1.addChild(tree3);
-		assertTrue(tree1.getChildren().size() == 1);
-		assertTrue(tree3.getParent().equals(tree1));
-		tree1.addChild(tree2);
-		assertTrue(tree1.getChildren().size() == 2);
-		assertTrue(tree2.getParent().equals(tree1));
+		root.addChild(tree3);
+		assertTrue(root.getChildren().size() == 1);
+		assertTrue(tree3.getParent().equals(root));
+		root.addChild(tree2);
+		assertTrue(root.getChildren().size() == 2);
+		assertTrue(tree2.getParent().equals(root));
 
 		/* Verification de l'ordre d'insertion dans la liste */
-		assertTrue(tree1.getChildren().get(0).equals(tree3));
+		assertTrue(root.getChildren().get(0).equals(tree3));
 	}
 
 	@Test
 	public final void testGetElement() {
-		assertTrue(tree1.getElement().get(0).equals("t1_elt1"));
-		assertTrue(tree1.getElement().get(1).equals("t1_elt2"));
-		assertTrue(tree1.getElement().get(2).equals("t1_elt3"));
+		assertTrue(root.getElement().get(0).equals("t1_elt1"));
+		assertTrue(root.getElement().get(1).equals("t1_elt2"));
+		assertTrue(root.getElement().get(2).equals("t1_elt3"));
 		assertTrue(tree4.getElement().get(0).equals("t4_elt1"));
 		assertTrue(tree5.getElement().size() == 0);
 	}
@@ -80,8 +102,8 @@ public class ResultTreeImplTest {
 	@Test
 	public final void testGetHighlighted() {
 		List<Integer> tmpList = new ArrayList<Integer>();
-		tmpList = tree1.getHighlighted();
-		assertTrue((tmpList.size() == 1) && (tmpList.get(0) == 1));
+		tmpList = root.getHighlighted();
+		assertTrue(tmpList.size() == 0);
 
 		tmpList = tree3.getHighlighted();
 		assertTrue((tmpList.size() == 3) && (tmpList.get(0) == 3) && (tmpList.get(1) == 5) && (tmpList.get(2) == 7));
@@ -104,19 +126,18 @@ public class ResultTreeImplTest {
 
 	@Test
 	public final void testRemove() {
-		tree1.addChild(tree2);
-		tree1.addChild(tree4);
+		root.addChild(tree2);
+		root.addChild(tree4);
 		tree2.addChild(tree3);
 		tree3.addChild(tree5);
 
-		assertTrue(tree1.getChildren().size() == 2);
+		assertTrue(root.getChildren().size() == 2);
 
 		tree2.remove();
 		assertNull(tree2.getParent());
-		assertFalse(tree1.getChildren().size() == 2);
-		assertTrue(tree1.getChildren().size() == 1);
+		assertFalse(root.getChildren().size() == 2);
+		assertTrue(root.getChildren().size() == 1);
 
 		assertTrue(tree3.getParent().equals(tree2));
 	}
-
 }
