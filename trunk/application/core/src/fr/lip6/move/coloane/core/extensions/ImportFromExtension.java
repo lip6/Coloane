283bbfb62f2bbb2e5285c363b1b259cafb86cf5a
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-06-03 08:43:32 +0000

    Add debug traces to solve the bug #201

--- trunk/application/core/src/fr/lip6/move/coloane/core/extensions/ImportFromExtension.java
+++ trunk/application/core/src/fr/lip6/move/coloane/core/extensions/ImportFromExtension.java
@@ -1,11 +1,14 @@
 package fr.lip6.move.coloane.core.extensions;
 
+import java.util.logging.Logger;
+
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.Platform;
 
-
 public final class ImportFromExtension {
+	private static final Logger LOG = Logger.getLogger("fr.lip6.move.coloane.core");
+
 	/**
 	 * Attributs du point d'extension
 	 */
@@ -24,8 +27,12 @@ public final class ImportFromExtension {
 	public static IImportFrom createConvertInstance(String ref) throws CoreException {
 		IConfigurationElement[] contributions = Platform.getExtensionRegistry().getConfigurationElementsFor(EXTENSION_POINT_ID);
 		IConfigurationElement convertContribution = null;
+
+		LOG.finest("Parcours des contributions :");
 		for (int i = 0; i < contributions.length; i++) {
+			LOG.finest(i + " -> " + contributions[i].getAttribute(WIZREF_EXTENSION) + " " + contributions[i].getAttribute(CLASS_EXTENSION));
 			if (contributions[i].getAttribute(WIZREF_EXTENSION).equals(ref)) {
+				LOG.finest("... Extension decouverte !");
 				convertContribution = contributions[i];
 				break;
 			}
@@ -33,8 +40,11 @@ public final class ImportFromExtension {
 
 		IImportFrom convertInstance = null;
 		if (convertContribution != null) {
+			LOG.finest("Instantiation de l'extension (classe : " + convertContribution.getAttribute(CLASS_EXTENSION));
 			convertInstance = (IImportFrom) convertContribution.createExecutableExtension(CLASS_EXTENSION);
 		}
+
+		LOG.finest("Retour de l'instance de conversion");
 		return convertInstance;
 	}
 }
