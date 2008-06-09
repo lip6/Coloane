7c43c952d4280031fb37e21a8d1a821b8b59e943
ryasri <ryasri@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-06-09 14:02:28 +0000

    Règle de connectique uniquement

---
+++ branches/coloane-newformalism/application/core/src/fr/lip6/move/coloane/core/motor/formalism/ConnexionRule.java
@@ -0,0 +1,55 @@
+package fr.lip6.move.coloane.core.motor.formalism;
+
+/**
+ * Regle de connexion pour un formalisme
+ */
+public class ConnexionRule implements IRule{
+
+	/** Element en entree de l'arc. */
+	private ElementFormalism elementIn;
+
+	/** Element en sortie de l'arc. */
+	private ElementFormalism elementOut;
+
+	
+	/**
+	 * Constructeur
+	 * Etablit quelles sont les connexions impossibles
+	 * @param eltIn Element en entree
+	 * @param eltOut Element en Sortie
+	 */
+	public ConnexionRule(ElementFormalism eltIn, ElementFormalism eltOut) {
+		this.elementIn = eltIn;
+		this.elementOut = eltOut;
+	}
+
+	/**
+	 * Retourne l'element en entree de l'arc.
+	 * @return ElementBase
+	 * @see ElementFormalism
+	 */
+	public final ElementFormalism getElementIn() {
+		return elementIn;
+	}
+
+	/**
+	 * Retourne l'element en sortie de l'arc.
+	 * @return ElementBase
+	 * @see ElementFormalism
+	 */
+	public final ElementFormalism getElementOut() {
+		return elementOut;
+	}
+	
+	// ***** Rajout
+	public final int getIdIn() {
+		return 0;
+	}
+	
+	public final int getIdOut() {
+		return 0;
+	}
+	
+	// ***** Rajout Fin
+	
+}
