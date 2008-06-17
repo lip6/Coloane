f64c337df74e33a6063dab751333049e61f47db7
mchaouki <mchaouki@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-06-17 12:48:56 +0000

    Creation des interfaces pour la gestion des sessions

--- branches/coloane-apiws/application/apiws/src/fr/lip6/move/coloane/apiws/interfaces/session/IApiSession.java
+++ branches/coloane-apiws/application/apiws/src/fr/lip6/move/coloane/apiws/interfaces/session/IApiSession.java
@@ -1,5 +1,140 @@
 package fr.lip6.move.coloane.apiws.interfaces.session;
 
+import fr.lip6.move.coloane.apiws.interfaces.objects.IModel;
+
 public interface IApiSession {
+
+	/**
+	 * Ouvre une session
+	 * @param sessionDate date de la session.
+	 * @param sessionFormalism son formalisme.
+	 * @param sessionName son nom.
+	 * @param interlocutor son interlocuteur (l'outil).
+	 * @param mode: son mode (interactif ou batch).
+	 */
+	public void openSession(String sessionDate, String sessionFormalism,String sessionName,String interlocutor,int mode);
+	
+	/**
+	 * Ouvre une session
+	 * @param sessionDate date de la session.
+	 * @param sessionFormalism son formalisme.
+	 * @param sessionName son nom.
+	 */
+	public void openSession(String sessionDate, String sessionFormalism,String sessionName);
+
+	/**
+	 * Suspendre la session courrante
+	 * @return vraie si la suspension de la session reussie, faux sinon.
+	 */
+	public boolean suspendSession();
+
+	/**
+	 * Reprendre la session courrante.
+	 * @return vraie si la reprise de la session reussie, faux sinon.
+	 */
+	public boolean resumeSession();
+
+	/**
+	 * Fermer la session courrante.
+	 */
+	public void closeSession();
+
+
+	/**
+	 * Demander un service sur la session courrante
+	 * Le model sera toujours envoyer au wrapper pour tous service
+	 * @param rootName 
+	 * @param menuName
+	 * @param serviceName
+	 * @param model
+	 */
+	public void askForService(String rootName,String menuName, String serviceName,IModel model);
+
+
+	/**
+	 * Demander un service sur la session courrante
+	 * Le model sera toujours envoyer au wrapper pour tous service
+	 * @param rootName
+	 * @param menuName
+	 * @param serviceName
+	 * @param date
+	 * @param model
+	 */
+	public void askForService(String rootName,String menuName, String serviceName, String date,IModel model);
+
+	
+	/*
+	Inutile d'avoir cette fonction: le model sera envoyer par a chaque demande de service
+	public void sendModel(IModel model);
+	*/
+	
+	/**
+	 * Recupere l'automate de la session courrante
+	 * @return l'automate de la session courrante
+	 */
+	public ISessionStateMachine getSessionStateMachine();
+	
+	/**
+	 * Recupere le nom de la session courrante
+	 * @return le nom de la session
+	 */
+	public String getSessionName();
+	
+	/**
+	 * Recupere le formalise de la session courrante
+	 * @return le formalise de la session courrante
+	 */
+	public String getSessionFormalism();
+	
+	/**
+	 * Recupere la date de la session courrante
+	 * @return la date de la session courrante
+	 */
+	public String getSessionDate();
+	
+	/**
+	 * Recupere l'interlocuteur (l'outil) de la session courrante
+	 * @return l'interlocuteur de la session courrante
+	 */
+	public String getInterlocutor();
+	
+	/**
+	 * Recupere le mode de la session courrante
+	 * @return le mode de la session courrante
+	 */
+	public int getMode();
+	
+	/**
+	 * Recupere le gestionnaire de session
+	 * @return le gestionnaire de session
+	 */
+	public ISessionController getSessionController();
+	
+	
+	/**
+	 * 
+	 */
+	public void invalidModel();
+	
+	/**
+	 * 
+	 */
+	public void notifyEndOpenSession();
+
+	/**
+	 * 
+	 */
+	public void notifyEndSuspendSession();
+	
+	/**
+	 * 
+	 * @param nameSession
+	 */
+	public void notifyEndResumeSession(String nameSession);
+
+	/**
+	 * 
+	 */
+	public void notifyEndCloseSession();
 
 }
--- branches/coloane-apiws/application/apiws/src/fr/lip6/move/coloane/apiws/interfaces/session/ISessionController.java
+++ branches/coloane-apiws/application/apiws/src/fr/lip6/move/coloane/apiws/interfaces/session/ISessionController.java
@@ -2,4 +2,88 @@ package fr.lip6.move.coloane.apiws.interfaces.session;
 
 public interface ISessionController {
 
+	/**
+	 * Recupere la session active.
+	 * @return la session active.
+	 */
+	public IApiSession getActiveSession();
+
+	/**
+	 * Ajouter une session dans la liste des sessions.
+	 * @param la session à rajouter.
+	 * @return true si c'est bien ajouter, false sinon.
+	 */
+	public boolean addSession(IApiSession s);
+
+	/**
+	 * Supprimer une session de la liste des sessions.
+	 * @param la session a supprimer.
+	 * @return true si c'est bien supprimer, false sinon.
+	 */
+	public boolean removeSession(IApiSession s);
+
+	/**
+	 * Verifie si la session est active ou pas.
+	 * @param la session à verifier.
+	 * @return true, si la session est active, false sinon.
+	 */
+	public boolean isActivateSession(IApiSession s);
+
+	/**
+	 * Verifie si on peut ouvrir une session.
+	 * @param s la session qu'on veut ouvrir.
+	 * @return true si on peut ouvrir une session, false sinon.
+	 */
+	public boolean openSession(IApiSession s);
+
+	/**
+	 * Verifie si on peut suspendre la session.
+	 * @param la session à suspendre.
+	 * @return true, si on peut suspendre la session, false sinon.
+	 */
+	public boolean suspendSession(IApiSession s);
+
+	/**
+	 * Verifie si on peut reprendre la session.
+	 * @param s la session a reprendre.
+	 * @return true, si on peut reprendre la session, false sinon.
+	 */
+	public boolean resumeSession(IApiSession s);
+	
+	/**
+	 * Verifie si on peut fermer la session
+	 * @param s la session a fermer
+	 * @return true, si on peut fermer la session, false sinon
+	 */
+	public boolean closeSession(IApiSession s);
+
+	/**
+	 * Verifie si on peut demander un service sur la session
+	 * @param s la session sur qui on demande un service
+	 * @return true, si on peut demander un service sur la session, false sinon
+	 */
+	public boolean askForService(IApiSession s);
+
+	/**
+	 * 
+	 */
+	public void notifyEndOpenSession();
+
+	/**
+	 * 
+	 */
+	public void notifyEndSuspendSession();
+
+	/**
+	 * 
+	 * @param nameSession
+	 */
+	public void notifyEndResumeSession(String nameSession);
+
+	/**
+	 * 
+	 */
+	public void notifyEndCloseSession();
+
+	
 }
