bbd8da9f878fd88fb44c2f604a39745d78b4e6aa
mchaouki <mchaouki@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-06-17 07:43:18 +0000

    Creation des packages pour les interfaces

---
+++ branches/coloane-apiws/application/apiws/src/fr/lip6/move/coloane/apiws/interfaces/observables/IObservable.java
@@ -0,0 +1,5 @@
+package fr.lip6.move.coloane.apiws.interfaces.observables;
+
+public interface IObservable {
+
+}
---
+++ branches/coloane-apiws/application/apiws/src/fr/lip6/move/coloane/apiws/interfaces/observers/IObserver.java
@@ -0,0 +1,5 @@
+package fr.lip6.move.coloane.apiws.interfaces.observers;
+
+public interface IObserver {
+
+}
