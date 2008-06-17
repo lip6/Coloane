0dd6747183b7dabb4037f04048a09eb0a99d8f20
mchaouki <mchaouki@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-06-17 16:14:44 +0000

    Creation de l'interface IApiConnection a finir

--- branches/coloane-apiws/application/apiws/src/fr/lip6/move/coloane/apiws/interfaces/api/IApiConnection.java
+++ branches/coloane-apiws/application/apiws/src/fr/lip6/move/coloane/apiws/interfaces/api/IApiConnection.java
@@ -2,4 +2,27 @@ package fr.lip6.move.coloane.apiws.interfaces.api;
 
 public interface IApiConnection {
 
+	/**
+	 * Initialiser l'adresse IP du serveur
+	 * @return true, si l'initialisation a reussie, false sinon
+	 */
+	public boolean setIpServer();
+
+	/**
+	 * Initialiser le port du serveur
+	 * @return true, si l'initialisation a reussie, false sinon
+	 */
+	public boolean setPortServer();
+
+	/**
+	 * Initialiser le login
+	 * @return true, si l'initialisation a reussie, false sinon
+	 */
+	public boolean setLogin();
+
+	/**
+	 * Initialiser le password
+	 * @return true, si l'initialisation a reussie, false sinon
+	 */
+	public boolean setPassword();
 }
