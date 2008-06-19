873c399fe65231f176790a5e77239d2026515105
mchaouki <mchaouki@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-06-19 12:47:55 +0000

    Ajout des methodes qui seront probablement utiliser dans les interfaces ISpeaker et IListener. Voir avec Silien la signiature des methodes.

--- branches/coloane-apiws/application/apiws/src/fr/lip6/move/coloane/apiws/interfaces/wrapperCommunication/IListener.java
+++ branches/coloane-apiws/application/apiws/src/fr/lip6/move/coloane/apiws/interfaces/wrapperCommunication/IListener.java
@@ -2,4 +2,12 @@ package fr.lip6.move.coloane.apiws.interfaces.wrapperCommunication;
 
 public interface IListener {
 
+	/**
+	 * Fonction de polling.
+	 * Permet de demander periodiquement au wrapper s'il y a des messages ou boites de dialog a traiter
+	 * 
+	 * ATTENTION: SE METTRE D'ACCORD AVEC SILIEN POUR LA SIGNIATURE
+	 */
+	public void ping();
+	
 }
--- branches/coloane-apiws/application/apiws/src/fr/lip6/move/coloane/apiws/interfaces/wrapperCommunication/ISpeaker.java
+++ branches/coloane-apiws/application/apiws/src/fr/lip6/move/coloane/apiws/interfaces/wrapperCommunication/ISpeaker.java
@@ -2,4 +2,47 @@ package fr.lip6.move.coloane.apiws.interfaces.wrapperCommunication;
 
 public interface ISpeaker {
 
+	/**
+	 * Demander une ouverture de connexion au wrapper
+	 * 
+	 * ATTENTION: SE METTRE D'ACCORD AVEC SILIEN POUR LA SIGNIATURE
+	 */
+	public void openConnection();
+	
+	/**
+	 * Demander une ouverture de session au wrapper
+	 * 
+	 * ATTENTION: SE METTRE D'ACCORD AVEC SILIEN POUR LA SIGNIATURE
+	 */
+	public void openSession();
+	
+	/**
+	 * Demander un changement de session au wrapper
+	 * 
+	 * ATTENTION: SE METTRE D'ACCORD AVEC SILIEN POUR LA SIGNIATURE
+	 */
+	public void changeSession();
+	
+	/**
+	 * Demander une fermeture de session au wrapper
+	 * 
+	 * ATTENTION: SE METTRE D'ACCORD AVEC SILIEN POUR LA SIGNIATURE
+	 */
+	public void closeSession();
+	
+	/**
+	 * Demander une fermeture de connexion
+	 * 
+	 * ATTENTION: SE METTRE D'ACCORD AVEC SILIEN POUR LA SIGNIATURE
+	 */
+	public void closeConnection();
+	
+	/**
+	 * Demander l'execution d'un service au wrapper
+	 * 
+	 * ATTENTION: SE METTRE D'ACCORD AVEC SILIEN POUR LA SIGNIATURE
+	 */
+	public void executService();
+	
+	
 }
