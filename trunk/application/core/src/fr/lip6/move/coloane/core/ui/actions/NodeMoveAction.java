92a2d3a71ec3fb85bec2d5088ee75b21f66494f7
cdemoulins <cdemoulins@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-06-20 09:29:48 +0000

    Close enhancement #221 (move with arrows)

---
+++ trunk/application/core/src/fr/lip6/move/coloane/core/ui/actions/NodeMoveAction.java
@@ -0,0 +1,68 @@
+package fr.lip6.move.coloane.core.ui.actions;
+
+import org.eclipse.draw2d.geometry.Rectangle;
+import org.eclipse.gef.EditPart;
+import org.eclipse.gef.ui.actions.SelectionAction;
+import org.eclipse.ui.IWorkbenchPart;
+
+import fr.lip6.move.coloane.core.ui.commands.NodeSetConstraintCmd;
+import fr.lip6.move.coloane.core.ui.editpart.ElementEditPart;
+import fr.lip6.move.coloane.core.ui.model.INodeGraphicInfo;
+import fr.lip6.move.coloane.core.ui.model.INodeImpl;
+
+public class NodeMoveAction extends SelectionAction {
+	/** Déplacement vers le haut */
+	public static final String UP = "move.up"; //$NON-NLS-1$
+
+	/** Déplacement vers le bas */
+	public static final String DOWN = "move.down"; //$NON-NLS-1$
+
+	/** Déplacement vers la gauche */
+	public static final String LEFT = "move.left"; //$NON-NLS-1$
+
+	/** Déplacement vers la droite */
+	public static final String RIGHT = "move.right"; //$NON-NLS-1$
+	
+	private int dx = 0;
+	private int dy = 0;
+
+	/**
+	 * @param part
+	 * @param action voir les constantes défini dans NodeMoveAction
+	 */
+	public NodeMoveAction(IWorkbenchPart part, String action) {
+		super(part);
+		setId(action);
+		if (UP.equals(action)) {
+			dy = -1;
+		} else if (DOWN.equals(action)) {
+			dy = 1;
+		} else if (LEFT.equals(action)) {
+			dx = -1;
+		} else if (RIGHT.equals(action)) {
+			dx = 1;
+		}
+	}
+
+	@Override
+	protected void init() {
+		super.init();
+		setEnabled(false);
+	}
+
+	@Override
+	protected boolean calculateEnabled() {
+		return !getSelectedObjects().isEmpty() && getSelectedObjects().get(0) instanceof ElementEditPart;
+	}
+
+	@Override
+	public void run() {
+		INodeImpl node = (INodeImpl) ((EditPart) getSelectedObjects().get(0)).getModel();
+		INodeGraphicInfo graphicInfo = node.getGraphicInfo();
+		execute(new NodeSetConstraintCmd(node, new Rectangle(
+				graphicInfo.getLocation().x + dx,
+				graphicInfo.getLocation().y + dy,
+				graphicInfo.getWidth(),
+				graphicInfo.getHeight())));
+	}
+}
