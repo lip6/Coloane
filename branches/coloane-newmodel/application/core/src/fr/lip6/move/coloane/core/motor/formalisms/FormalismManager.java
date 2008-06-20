c9bae9f1281d97877c73175413c679cc96a55190
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-06-20 11:43:38 +0000

    Refactor the formalism manager

--- branches/coloane-newmodel/application/core/src/fr/lip6/move/coloane/core/motor/formalisms/FormalismManager.java
+++ branches/coloane-newmodel/application/core/src/fr/lip6/move/coloane/core/motor/formalisms/FormalismManager.java
@@ -1,10 +1,10 @@
 package fr.lip6.move.coloane.core.motor.formalisms;
 
 import fr.lip6.move.coloane.core.motor.formalisms.definitions.PetriNets;
-import fr.lip6.move.coloane.core.motor.formalisms.definitions.PrefixNets;
 import fr.lip6.move.coloane.core.motor.formalisms.definitions.ReachabilityGraph;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Classe du gestionnaire de formalismes.
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 public final class FormalismManager {
 
 	/** Liste des formalismes disponibles. */
-	private ArrayList<Formalism> listOfFormalisms;
+	private List<Formalism> formalisms;
 
 	/** L'instance du singleton : FormalismManager */
 	private static FormalismManager instance = null;
@@ -21,19 +21,18 @@ public final class FormalismManager {
 	/**
 	 * Constructeur de la classe FormalismsManager
 	 * Constitution de la liste des formalismes
-	 * Pour eviter les doublonson utilise le pattern <b>Singleton</b>
-	 * @see #getInstance()
+	 * Pour eviter les doublonson utilise le pattern <b>singleton</b>
+	 * @see {@link #getInstance()}
 	 */
 	private FormalismManager() {
-		listOfFormalisms = new ArrayList<Formalism>();
-		listOfFormalisms.add(new PetriNets());
-		listOfFormalisms.add(new PrefixNets());
-		listOfFormalisms.add(new ReachabilityGraph());
+		formalisms = new ArrayList<Formalism>();
+		formalisms.add(new PetriNets());
+		formalisms.add(new ReachabilityGraph());
 	}
 
 	/**
 	 * Retourne le gestionnaire de formalismes
-	 * @return FormalismManager Une isntance du gestionnaire de formalismes
+	 * @return FormalismManager Une instance du gestionnaire de formalismes
 	 */
 	public static synchronized FormalismManager getInstance() {
 		if (instance == null) { instance = new FormalismManager(); }
@@ -41,12 +40,12 @@ public final class FormalismManager {
 	}
 
 	/**
-	 * Cette methode retourne un formalisme a partir de son nom
+	 * Cette methode retourne un formalisme à partir de son nom
 	 * @param name Le nom du formalism qu'on cherche
-	 * @return Formalism
+	 * @return Le formalisme attendu ou <code>null</code> sinon
 	 */
 	public Formalism getFormalismByName(String name) {
-		for (Formalism form : listOfFormalisms) {
+		for (Formalism form : formalisms) {
 			if (name.toLowerCase().equals(form.getName().toLowerCase())) {
 				return form;
 			}
@@ -55,10 +54,9 @@ public final class FormalismManager {
 	}
 
 	/**
-	 * Retourne la liste des formalismes disponibles
-	 * @return listOfFormalism
+	 * @return La liste des formalismes disponibles
 	 */
-	public ArrayList<Formalism> getListOfFormalisms() {
-		return listOfFormalisms;
+	public List<Formalism> getListOfFormalisms() {
+		return formalisms;
 	}
 }
