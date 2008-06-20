a7d498b6a37250fb99d7fad62b13be597c89e9e0
mchaouki <mchaouki@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-06-20 16:31:28 +0000

    Modification des interfaces ISpeaker et Listener, pour respecter la signatures des methodes de communication avec le wrapper.

--- branches/coloane-apiws/application/apiws/src/fr/lip6/move/coloane/apiws/interfaces/wrapperCommunication/IListener.java
+++ branches/coloane-apiws/application/apiws/src/fr/lip6/move/coloane/apiws/interfaces/wrapperCommunication/IListener.java
@@ -1,13 +1,13 @@
 package fr.lip6.move.coloane.apiws.interfaces.wrapperCommunication;
 
+import fr.lip6.move.wrapper.ws.WrapperStub.AsyncMessage;
+
 public interface IListener {
 
 	/**
 	 * Fonction de polling.
 	 * Permet de demander periodiquement au wrapper s'il y a des messages ou boites de dialog a traiter
-	 * 
-	 * ATTENTION: SE METTRE D'ACCORD AVEC SILIEN POUR LA SIGNIATURE
 	 */
-	public void ping();
+	public AsyncMessage ping();
 	
 }
--- branches/coloane-apiws/application/apiws/src/fr/lip6/move/coloane/apiws/interfaces/wrapperCommunication/ISpeaker.java
+++ branches/coloane-apiws/application/apiws/src/fr/lip6/move/coloane/apiws/interfaces/wrapperCommunication/ISpeaker.java
@@ -1,41 +1,35 @@
 package fr.lip6.move.coloane.apiws.interfaces.wrapperCommunication;
 
+import fr.lip6.move.wrapper.ws.WrapperStub.Authentification;
+import fr.lip6.move.wrapper.ws.WrapperStub.Session;
+import fr.lip6.move.wrapper.ws.WrapperStub.Unauthentification;
+
 public interface ISpeaker {
 
 	/**
 	 * Demander une ouverture de connexion au wrapper
-	 * 
-	 * ATTENTION: SE METTRE D'ACCORD AVEC SILIEN POUR LA SIGNIATURE
 	 */
-	public void openConnection();
+	public Authentification openConnection(String login, String pass);
 	
 	/**
 	 * Demander une ouverture de session au wrapper
-	 * 
-	 * ATTENTION: SE METTRE D'ACCORD AVEC SILIEN POUR LA SIGNIATURE
 	 */
-	public void openSession();
+	public Session openSession(String nameFormalism);
 	
 	/**
 	 * Demander un changement de session au wrapper
-	 * 
-	 * ATTENTION: SE METTRE D'ACCORD AVEC SILIEN POUR LA SIGNIATURE
 	 */
-	public void changeSession();
+	public Session changeSession(String nameSession);
 	
 	/**
 	 * Demander une fermeture de session au wrapper
-	 * 
-	 * ATTENTION: SE METTRE D'ACCORD AVEC SILIEN POUR LA SIGNIATURE
 	 */
-	public void closeSession();
+	public Session closeSession(String idSession);
 	
 	/**
 	 * Demander une fermeture de connexion
-	 * 
-	 * ATTENTION: SE METTRE D'ACCORD AVEC SILIEN POUR LA SIGNIATURE
 	 */
-	public void closeConnection();
+	public Unauthentification closeConnection();
 	
 	/**
 	 * Demander l'execution d'un service au wrapper
