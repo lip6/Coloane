a8ccae3cfbe8a5872f8036c2698719d209cb089a
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-06-20 11:24:10 +0000

    No need for order in attribute definition. The order in property view is defined by the order of add in parent element

--- branches/coloane-newmodel/application/core/src/fr/lip6/move/coloane/core/motor/formalisms/elements/Attribute.java
+++ branches/coloane-newmodel/application/core/src/fr/lip6/move/coloane/core/motor/formalisms/elements/Attribute.java
@@ -18,33 +18,27 @@ public class Attribute {
 	/** Valeur par défaut de l'attribut. */
 	private String defaultValue = null;
 
-	/** Ordre d'affichage dans la fenetre des proprietes */
-	private int order;
-
 	/**
 	 * Construit un nouvel attribut
-	 * @param order L'ordre d'affichage dans la fenêtre de propiétés
 	 * @param name Le nom de l'attribut.
 	 * @param drawable L'information est elle affichable a l'ecran ?
 	 * @param multiline L'attribut est il multi-lignes ?
 	 */
-	public Attribute(int order, String name, boolean drawable, boolean multiline) {
+	public Attribute(String name, boolean drawable, boolean multiline) {
 		this.name = name;
 		this.drawable = drawable;
 		this.multiline = multiline;
-		this.order = order;
 	}
 
 	/**
 	 * Construit un nouvel attribut (avec une valeur par defaut)
-	 * @param order L'ordre d'affichage dans la fenetre des proprietes
 	 * @param name Le nom de l'attribut.
 	 * @param drawable L'information est elle affichable a l'ecran ?
 	 * @param multiline L'attribut est il multi-lignes ?
 	 * @param defaultValue La valeur par defaut de l'attribut.
 	 */
-	public Attribute(int order, String name, boolean drawable, boolean multiline, String defaultValue) {
-		this(order,name,drawable,multiline);
+	public Attribute(String name, boolean drawable, boolean multiline, String defaultValue) {
+		this(name,drawable,multiline);
 		this.defaultValue = defaultValue;
 	}
 
@@ -71,9 +65,4 @@ public class Attribute {
 		if (defaultValue != null) { return defaultValue; }
 		return ""; //$NON-NLS-1$
 	}
-
-	/**
-	 * @return L'index d'affichage à l'intérieur de la fenêtre de propriété
-	 */
-	public final int getOrder() { return this.order; }
 }
