7ad795f63adf1bfd6b0ad7a22d026989677d4afa
mchaouki <mchaouki@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-06-24 11:26:30 +0000

    Ajout d'une methode pour recuperer les messages de trace.

--- branches/coloane-apiws/application/apiws/src/fr/lip6/move/coloane/apiws/interfaces/evenements/IReceptTraceMessage.java
+++ branches/coloane-apiws/application/apiws/src/fr/lip6/move/coloane/apiws/interfaces/evenements/IReceptTraceMessage.java
@@ -1,5 +1,12 @@
 package fr.lip6.move.coloane.apiws.interfaces.evenements;
 
-public interface IReceptTraceMessage {
+import java.util.ArrayList;
 
+public interface IReceptTraceMessage {
+	
+	/**
+	 * Recupere les messages de traces
+	 * @return les messages de traces
+	 */
+	public ArrayList<String> getTraceMessages();
 }
