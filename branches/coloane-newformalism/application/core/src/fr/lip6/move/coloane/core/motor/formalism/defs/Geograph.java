d44f083e03d0ab4c796af8498651655164d03efd
ryasri <ryasri@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-06-26 13:11:27 +0000

    update

--- branches/coloane-newformalism/application/core/src/fr/lip6/move/coloane/core/motor/formalism/defs/Geograph.java
+++ branches/coloane-newformalism/application/core/src/fr/lip6/move/coloane/core/motor/formalism/defs/Geograph.java
@@ -5,7 +5,8 @@ import fr.lip6.move.coloane.core.motor.formalism.AttributeFormalism;
 import fr.lip6.move.coloane.core.motor.formalism.ElementFormalism;
 import fr.lip6.move.coloane.core.motor.formalism.Formalism;
 import fr.lip6.move.coloane.core.motor.formalism.NodeFormalism;
-import fr.lip6.move.coloane.core.motor.formalism.permissionRule.CardinalityRule;
+import fr.lip6.move.coloane.core.motor.formalism.permissionRule.CardinalityMaxInRule;
+import fr.lip6.move.coloane.core.motor.formalism.permissionRule.CardinalityMaxOutRule;
 import fr.lip6.move.coloane.core.motor.formalism.permissionRule.ConnexionRule;
 import fr.lip6.move.coloane.core.ui.model.IArcGraphicInfo;
 import fr.lip6.move.coloane.core.ui.model.IAttributeGraphicInfo;
@@ -13,15 +14,15 @@ import fr.lip6.move.coloane.core.ui.model.INodeGraphicInfo;
 
 public class Geograph extends Formalism {
 
-	private static final String NAME = "Geographie"; //$NON-NLS-1$
-	private static final String IMG = "/resources/icons/instance.gif"; //$NON-NLS-1$
-	private static final String EXTENSION = "geo"; //$NON-NLS-1$
-	private static final String XSCHEMA = "global.xsd"; //$NON-NLS-1$
+	private static final String NAME = "Geographie";
+	private static final String IMG = "/resources/icons/instance.gif";
+	private static final String EXTENSION = "geo";
+	private static final String XSCHEMA = "global.xsd";
 	
-	private static final int PLACE_WIDTH = 20;
-	private static final int PLACE_HEIGHT = 20;
-	private static final int TRANSITION_WIDTH = 20;
-	private static final int TRANSITION_HEIGHT = 10;
+	private static final int CONTINENT_WIDTH = 20;
+	private static final int CONTINENT_HEIGHT = 20;
+	private static final int PAYS_WIDTH = 20;
+	private static final int PAYS_HEIGHT = 10;
 
 	/**
 	 * Constructeur du formalisme
@@ -30,87 +31,72 @@ public class Geograph extends Formalism {
 		super(NAME, IMG, EXTENSION, XSCHEMA);
 		int i = 1;
 
-		addAttributeFormalism(new AttributeFormalism(i++, "declaration", IAttributeGraphicInfo.NOR, true, true)); //$NON-NLS-1$
-		addAttributeFormalism(new AttributeFormalism(i++, "author(s)", IAttributeGraphicInfo.NOR, true, true)); //$NON-NLS-1$
-		addAttributeFormalism(new AttributeFormalism(i++, "version", IAttributeGraphicInfo.NOR, true, false, "0,0")); //$NON-NLS-1$ //$NON-NLS-2$
-		addAttributeFormalism(new AttributeFormalism(i++, "project", IAttributeGraphicInfo.NOR, true, true)); //$NON-NLS-1$
-		addAttributeFormalism(new AttributeFormalism(i++, "title", IAttributeGraphicInfo.NOR, true, true)); //$NON-NLS-1$
-		addAttributeFormalism(new AttributeFormalism(i++, "date", IAttributeGraphicInfo.NOR, true, false)); //$NON-NLS-1$
-		addAttributeFormalism(new AttributeFormalism(i++, "code", IAttributeGraphicInfo.NOR, true, true)); //$NON-NLS-1$
-		addAttributeFormalism(new AttributeFormalism(i++, "note", false, true)); //$NON-NLS-1$
+		addAttributeFormalism(new AttributeFormalism(i++, "declaration", IAttributeGraphicInfo.NOR, true, true));
+		addAttributeFormalism(new AttributeFormalism(i++, "author(s)", IAttributeGraphicInfo.NOR, true, true));
+		addAttributeFormalism(new AttributeFormalism(i++, "version", IAttributeGraphicInfo.NOR, true, false, "0,0"));
+		addAttributeFormalism(new AttributeFormalism(i++, "project", IAttributeGraphicInfo.NOR, true, true));
+		addAttributeFormalism(new AttributeFormalism(i++, "title", IAttributeGraphicInfo.NOR, true, true));
+		addAttributeFormalism(new AttributeFormalism(i++, "date", IAttributeGraphicInfo.NOR, true, false));
+		addAttributeFormalism(new AttributeFormalism(i++, "code", IAttributeGraphicInfo.NOR, true, true));
+		addAttributeFormalism(new AttributeFormalism(i++, "note", false, true)); 
 
-		/* Creation ajout des differents elements de base d'un Reseau de Petri :
-		 * <ul>
-		 *   <li>Place</li>
-		 *   <li>Transition</li>
-		 *   <li>Arc</li>
-		 *   <li>Arc inhibiteur</li>
-		 * </ul>
-		 */
-
-		// Remise a zero de l'indicateur d'affichage
-		i = 1;
-
-		// La place:
+		// Le continent
 		
-		ElementFormalism elt = new NodeFormalism("Continent", Messages.PetriNets_13, this, INodeGraphicInfo.FIG_CIRCLE, PLACE_WIDTH, PLACE_HEIGHT, false); //$NON-NLS-1$
+		ElementFormalism elt = new NodeFormalism("Continent", Messages.Geo_1, this, INodeGraphicInfo.FIG_CIRCLE, CONTINENT_WIDTH, CONTINENT_HEIGHT, false); 
 		
-		elt.addAttributeFormalism(new AttributeFormalism(i++, "Nom Continent", IAttributeGraphicInfo.L2, true, true)); //$NON-NLS-1$
-		elt.addAttributeFormalism(new AttributeFormalism(i++, "Geographiquement", IAttributeGraphicInfo.L2, true, true)); //$NON-NLS-1$
-		elt.addAttributeFormalism(new AttributeFormalism(i++, "Superficie", IAttributeGraphicInfo.L2, true, true)); //$NON-NLS-1$
-		elt.addAttributeFormalism(new AttributeFormalism(i++, "Nombre Pays", IAttributeGraphicInfo.L2, true, true)); //$NON-NLS-1$		//elt.addAttributeFormalism(new AttributeFormalism(i++, "Mers", false, true)); //$NON-NLS-1$
+		elt.addAttributeFormalism(new AttributeFormalism(i++, "Nom Continent", IAttributeGraphicInfo.L2, true, true)); 
+		elt.addAttributeFormalism(new AttributeFormalism(i++, "Geographiquement", IAttributeGraphicInfo.L2, true, true)); 
+		elt.addAttributeFormalism(new AttributeFormalism(i++, "Superficie", IAttributeGraphicInfo.L2, true, true)); 
+		elt.addAttributeFormalism(new AttributeFormalism(i++, "Nombre Pays", IAttributeGraphicInfo.L2, true, true));  
 		
-		elt.setAddrIcone16("/resources/formalisms/place16.png"); //$NON-NLS-1$
-		elt.setAddrIcone24("/resources/formalisms/place24.png"); //$NON-NLS-1$
+		elt.setAddrIcone16("/resources/formalisms/continent.png"); 
+		elt.setAddrIcone24("/resources/formalisms/continent.png"); 
 
 		addElementBase(elt);
 
 		// Remise a zero de l'indicateur d'affichage
 		i = 1;
 
-		// La transition:
-		elt = new NodeFormalism("Pays", Messages.PetriNets_22, this, INodeGraphicInfo.FIG_RECT, TRANSITION_WIDTH, TRANSITION_HEIGHT, false); //$NON-NLS-1$
+		// Le Pays:
+		elt = new NodeFormalism("Pays", Messages.Geo_2, this, INodeGraphicInfo.FIG_RECT, PAYS_WIDTH, PAYS_HEIGHT, false); 
 				
-		elt.addAttributeFormalism(new AttributeFormalism(i++, "Nom Pays", IAttributeGraphicInfo.L1, true, false)); //$NON-NLS-1$
-		elt.addAttributeFormalism(new AttributeFormalism(i++, "Superficie", IAttributeGraphicInfo.L1, true, false)); //$NON-NLS-1$
-		elt.addAttributeFormalism(new AttributeFormalism(i++, "Capitale", IAttributeGraphicInfo.L1, true, false)); //$NON-NLS-1$
-		elt.addAttributeFormalism(new AttributeFormalism(i++, "Langue", IAttributeGraphicInfo.L1, true, false)); //$NON-NLS-1$
-		elt.addAttributeFormalism(new AttributeFormalism(i++, "Monnaie", IAttributeGraphicInfo.L1, true, false)); //$NON-NLS-1$
+		elt.addAttributeFormalism(new AttributeFormalism(i++, "Nom Pays", IAttributeGraphicInfo.L1, true, false)); 
+		elt.addAttributeFormalism(new AttributeFormalism(i++, "Superficie", IAttributeGraphicInfo.L1, true, false)); 
+		elt.addAttributeFormalism(new AttributeFormalism(i++, "Capitale", IAttributeGraphicInfo.L1, true, false)); 
+		elt.addAttributeFormalism(new AttributeFormalism(i++, "Langue", IAttributeGraphicInfo.L1, true, false)); 
+		elt.addAttributeFormalism(new AttributeFormalism(i++, "Monnaie", IAttributeGraphicInfo.L1, true, false));
 		
-		elt.setAddrIcone16("/resources/formalisms/transition16.png"); //$NON-NLS-1$
-		elt.setAddrIcone24("/resources/formalisms/transition24.png"); //$NON-NLS-1$
+		elt.setAddrIcone16("/resources/formalisms/pays.png");
 
 		addElementBase(elt);
 
-		// Remise a zero de l'indicateur d'affichage
-		i = 1;
+		//Remiseazerodel'indicateurd'affichage
+		i=1;
 
-		// L'arc
-		elt = new ArcFormalism("arc", Messages.PetriNets_52, this, IArcGraphicInfo.FIG_ARC_SIMPLE); //$NON-NLS-1$
+		//L'arc
+		elt=new ArcFormalism("arc",Messages.Geo_3,this,IArcGraphicInfo.FIG_ARC_SIMPLE);
 
-		elt.addAttributeFormalism(new AttributeFormalism(i++, "Depuis", IAttributeGraphicInfo.NOR, true, true, "1")); //$NON-NLS-1$ //$NON-NLS-2$
-		//elt.addAttributeFormalism(new AttributeFormalism(i++, "", false, true)); //$NON-NLS-1$
-		elt.setAddrIcone16("/resources/formalisms/arc16.png"); //$NON-NLS-1$
-		elt.setAddrIcone24("/resources/formalisms/arc24.png"); //$NON-NLS-1$
+		elt.addAttributeFormalism(new AttributeFormalism(i++,"Depuis",IAttributeGraphicInfo.NOR,true,true,"1"));
 
+		elt.setAddrIcone16("/resources/formalisms/arc16.png");
 		addElementBase(elt);
 
 
-		// Ajout des regles gerant le formalisme, ces regles definissent ce qu'on ne peut pas faire.
-		// Interdit Continent - Continent
-		addRule(new ConnexionRule(this.getNodeFormalism("Continent"), this.getNodeFormalism("Continent"))); //$NON-NLS-1$ //$NON-NLS-2$
+		//Ajout des regles gerant le formalisme, ces regles definissent ce qu'on ne peut pas faire.
+		//Interdit Continent - Continent
+		addRule(new ConnexionRule(this.getNodeFormalism("Continent"),this.getNodeFormalism("Continent")));
 	
-		// Interdit Pays - Pays
-		addRule(new ConnexionRule(this.getNodeFormalism("Pays"), this.getNodeFormalism("Pays"))); //$NON-NLS-1$ //$NON-NLS-2$
+		//Interdit Pays - Pays
+		addRule(new ConnexionRule(this.getNodeFormalism("Pays"),this.getNodeFormalism("Pays")));
 		
+		//Règle de Cardinalité, ie:un Pays appartient à un seul pays
 		
-		// essai avec nouvelle règle 
+		addRule(new CardinalityMaxOutRule(this.getNodeFormalism("Pays"),1,this.getNodeFormalism("Continent")));
 		
-		addRule(new ConnexionRule(this.getNodeFormalism("Continent"),this.getNodeFormalism("Pays"))); //$NON-NLS-1$ //$NON-NLS-2$
+		//Un Contient peut Etre relié au maximum à 2 Pays
 		
-		// Regle de Cardinalité
+		addRule(new CardinalityMaxInRule(this.getNodeFormalism("Continent"),2,this.getNodeFormalism("Pays")));
 		
-		addRule(new CardinalityRule(this.getNodeFormalism("Pays"),3,this.getNodeFormalism("Continent"))); //$NON-NLS-1$ //$NON-NLS-2$
 		
 	}
 }
