b1c6a602dee46680205043183aa566ef7b1ef42a
mchaouki <mchaouki@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-06-26 11:42:18 +0000

    Creation d'un package pour les exceptions.

---
+++ branches/coloane-apiws/application/apiws/src/fr/lip6/move/coloane/apiws/exceptions/ApiConnectionException.java
@@ -0,0 +1,14 @@
+package fr.lip6.move.coloane.apiws.exceptions;
+
+public class ApiConnectionException extends Exception {
+
+	/**
+	 * Genere par Eclipse
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public ApiConnectionException(String message){
+		super(message);
+	}
+
+}
---
+++ branches/coloane-apiws/application/apiws/src/fr/lip6/move/coloane/apiws/exceptions/WrapperException.java
@@ -0,0 +1,14 @@
+package fr.lip6.move.coloane.apiws.exceptions;
+
+public class WrapperException extends Exception{
+
+	/**
+	 * Genere par Eclipse
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public WrapperException(String message){
+		super(message);
+	}
+
+}
