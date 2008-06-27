c431694fa94be29901b56994f2b2be0aee6d72ef
mchaouki <mchaouki@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-06-27 15:55:59 +0000

    Correction de l'initialisation de la liste des observateurs. Ajout de la dure du ping.

--- branches/coloane-apiws/application/apiws/src/fr/lip6/move/coloane/apiws/wrapperCommunication/Listener.java
+++ branches/coloane-apiws/application/apiws/src/fr/lip6/move/coloane/apiws/wrapperCommunication/Listener.java
@@ -23,25 +23,34 @@ import fr.lip6.move.wrapper.ws.WrapperStub.PingResponse;
 public class Listener extends Thread implements IListener{
 
 	private Authentification auth = null;
-	
+
 	private WrapperStub stub = null;
 	
+	private int durePing = 3*1000;
+
 	private boolean stopThread = false;
-	
+
 	private HashMap<Integer, Object> listObservable= null;
-	
+
 	public Listener(Authentification auth, WrapperStub stub, HashMap<Integer, Object> listObservables){
 		this.auth = auth;
 		this.stub = stub;
 		this.stopThread = false;
-		this.listObservable = new HashMap<Integer, Object>();
+		this.listObservable = listObservables;
 	}
-	
+
 	public void run() {
-		
+
 		boolean stop = false;
-		
+
 		while (!stop){ 
+			try {
+				sleep(durePing);
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
 			AsyncMessage message = null;        
 
 			try {
@@ -61,51 +70,51 @@ public class Listener extends Thread implements IListener{
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 			if (message.getTraces() != null){
 				ReceptTraceMessage traceMessage = new ReceptTraceMessage(message);
 				((ITraceMessageObservable) listObservable.get(IObservables.TRACE_MESSAGE)).notifyObservers(traceMessage);
 			}
-			
+
 			if (message.getDbs() != null){
 				AskDialog dialog = new AskDialog(message);
 				((IAskDialogObservable ) listObservable.get(IObservables.ASK_DIALOG)).notifyObservers(dialog);
 			}
-			
+
 			synchronized(this) {
-                stop = this.stopThread;
+				stop = this.stopThread;
 			}
-			
+
 		}
 	}
-	
+
 	public synchronized void stopper() {
-        this.stopThread = true;
+		this.stopThread = true;
 	}
 
-	
+
 	public String answerToDialogBox(DialogBox answer) throws CException{
-        String toReturn = null;        
-        
-        try {
-            if(stub==null)
-                throw new CException("Error of communcation : Stub is null",CException.COMM_ERROR);
-            AnswerDb req = new AnswerDb();
-            req.setAuth(auth);
-            req.setDialog(answer);
-            AnswerDbResponse res=stub.answerDb(req);
-            toReturn=res.get_return();
-        }catch (RemoteException e) {
-            CException ee = new CException();
-            ee.initialize(e.getMessage());
-            // TODO Auto-generated catch block
-            throw ee;
-        } catch (GExceptionException0 e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } 
-        
-        return toReturn;    
+		String toReturn = null;        
+
+		try {
+			if(stub==null)
+				throw new CException("Error of communcation : Stub is null",CException.COMM_ERROR);
+			AnswerDb req = new AnswerDb();
+			req.setAuth(auth);
+			req.setDialog(answer);
+			AnswerDbResponse res=stub.answerDb(req);
+			toReturn=res.get_return();
+		}catch (RemoteException e) {
+			CException ee = new CException();
+			ee.initialize(e.getMessage());
+			// TODO Auto-generated catch block
+			throw ee;
+		} catch (GExceptionException0 e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+
+		return toReturn;    
 	}
 
 
