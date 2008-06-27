509f128d6db5626208347c56d5f922b6699fd882
mchaouki <mchaouki@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-06-27 10:00:17 +0000

    Correction mineur: ajout de 'implements IAnswerSendDialog'.

--- branches/coloane-apiws/application/apiws/src/fr/lip6/move/coloane/apiws/evenements/AnswerSendDialog.java
+++ branches/coloane-apiws/application/apiws/src/fr/lip6/move/coloane/apiws/evenements/AnswerSendDialog.java
@@ -1,6 +1,8 @@
 package fr.lip6.move.coloane.apiws.evenements;
 
-public class AnswerSendDialog {
+import fr.lip6.move.coloane.apiws.interfaces.evenements.IAnswerSendDialog;
+
+public class AnswerSendDialog implements IAnswerSendDialog{
 	
 	public AnswerSendDialog(String m){
 		
