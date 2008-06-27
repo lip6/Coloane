d2ad721efd5342b12dd3180f7d76555d32643bbc
mchaouki <mchaouki@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-06-27 16:25:20 +0000

    Ajout d'un methode pour recupere la reponse de l'envoie d'une boite de dialogue.

--- branches/coloane-apiws/application/apiws/src/fr/lip6/move/coloane/apiws/evenements/AnswerSendDialog.java
+++ branches/coloane-apiws/application/apiws/src/fr/lip6/move/coloane/apiws/evenements/AnswerSendDialog.java
@@ -4,7 +4,13 @@ import fr.lip6.move.coloane.apiws.interfaces.evenements.IAnswerSendDialog;
 
 public class AnswerSendDialog implements IAnswerSendDialog{
 	
+	private String returnMsg;
+	
 	public AnswerSendDialog(String m){
-		
+		this.returnMsg = m;
+	}
+	
+	public String getReturnMsg(){
+		return returnMsg;
 	}
 }
--- branches/coloane-apiws/application/apiws/src/fr/lip6/move/coloane/apiws/interfaces/evenements/IAnswerSendDialog.java
+++ branches/coloane-apiws/application/apiws/src/fr/lip6/move/coloane/apiws/interfaces/evenements/IAnswerSendDialog.java
@@ -1,5 +1,10 @@
 package fr.lip6.move.coloane.apiws.interfaces.evenements;
 
 public interface IAnswerSendDialog {
-
+	
+	/**
+	 * Recupere le message en retour apres l'envoie de la boite de dialogue
+	 * @return le message en retour apres l'envoie de la boite de dialogue
+	 */
+	public String getReturnMsg();
 }
