59de199c406cfb3dbffcd338899e3f15fb81e0f9
mchaouki <mchaouki@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-06-30 16:50:28 +0000

    Ajout d'un constructeur.

--- branches/coloane-apiws/application/apiws/src/fr/lip6/move/coloane/apiws/evenements/AnswerCloseSession.java
+++ branches/coloane-apiws/application/apiws/src/fr/lip6/move/coloane/apiws/evenements/AnswerCloseSession.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import fr.lip6.move.coloane.apiws.interfaces.evenements.IAnswerCloseSession;
 import fr.lip6.move.coloane.apiws.interfaces.objects.IMenu;
+import fr.lip6.move.coloane.apiws.interfaces.session.IApiSession;
 import fr.lip6.move.wrapper.ws.WrapperStub.Session;
 
 public class AnswerCloseSession implements IAnswerCloseSession{
@@ -19,6 +20,14 @@ public class AnswerCloseSession implements IAnswerCloseSession{
 		this.idSession = s.getSessionId();
 		// TODO Voir avec J-B et Silien comment recuperer ces elements.
 		this.menus = new ArrayList<IMenu>();
+	}
+	
+	public AnswerCloseSession(IApiSession s){
+		this.formalism = s.getSessionFormalism();
+		this.idSession = s.getIdSession();
+		// TODO Voir avec J-B et Silien comment recuperer ces elements.
+		this.menus = new ArrayList<IMenu>();
+		
 	}
 
 	public String getFormalism() {
