749986a177dec6a3b2d491362b764c71c9e7212e
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-06-30 19:37:10 +0000

    Simple modification to the pn formalisme described in plugin.xml
    FormalismManager updated to deal with XML descriptions (building from plugin.xml description)

--- branches/coloane-newmodel/application/core/src/fr/lip6/move/coloane/core/motor/formalisms/FormalismManager.java
+++ branches/coloane-newmodel/application/core/src/fr/lip6/move/coloane/core/motor/formalisms/FormalismManager.java
@@ -1,10 +1,16 @@
 package fr.lip6.move.coloane.core.motor.formalisms;
 
-import fr.lip6.move.coloane.core.motor.formalisms.definitions.PetriNets;
-import fr.lip6.move.coloane.core.motor.formalisms.definitions.ReachabilityGraph;
+import fr.lip6.move.coloane.core.motor.formalisms.elements.Arc;
+import fr.lip6.move.coloane.core.motor.formalisms.elements.Attribute;
+import fr.lip6.move.coloane.core.motor.formalisms.elements.FormalismElement;
+import fr.lip6.move.coloane.core.motor.formalisms.elements.Graph;
+import fr.lip6.move.coloane.core.motor.formalisms.elements.Node;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.Platform;
 
 /**
  * Classe du gestionnaire de formalismes.
@@ -12,8 +18,10 @@ import java.util.List;
  */
 public final class FormalismManager {
 
+	private static final String EXTENSION_ID = "fr.lip6.move.coloane.core.formalisms"; //$NON-NLS-1$
+
 	/** Liste des formalismes disponibles. */
-	private List<Formalism> formalisms;
+	private List<Formalism> formalisms = new ArrayList<Formalism>();
 
 	/** L'instance du singleton : FormalismManager */
 	private static FormalismManager instance = null;
@@ -25,9 +33,66 @@ public final class FormalismManager {
 	 * @see {@link #getInstance()}
 	 */
 	private FormalismManager() {
-		formalisms = new ArrayList<Formalism>();
-		formalisms.add(new PetriNets());
-		formalisms.add(new ReachabilityGraph());
+		IConfigurationElement[] formalisms = Platform.getExtensionRegistry().getConfigurationElementsFor(EXTENSION_ID);
+		for (int i = 0; i < formalisms.length; i++) {
+			buildFormalism(formalisms[i]);
+		}
+	}
+	
+	/**
+	 * 
+	 * @param description
+	 */
+	private void buildFormalism (IConfigurationElement description) {
+		String name, xschema, extension, image;
+		name = description.getAttribute("name");  //$NON-NLS-1$
+		xschema = description.getAttribute("xschema"); //$NON-NLS-1$
+		extension = description.getAttribute("extension"); //$NON-NLS-1$
+		image = description.getAttribute("image"); //$NON-NLS-1$
+		
+		// Creation et ajout du formalisme a la liste du manager
+		Formalism form = new Formalism(name,extension, xschema, image);
+		this.formalisms.add(form);
+		
+		IConfigurationElement[] XMLDescription = description.getChildren("XmlDescription"); //$NON-NLS-1$
+		
+		// Ajout des definitions de graphes
+		IConfigurationElement[] graphes = XMLDescription[0].getChildren("Graph"); //$NON-NLS-1$
+		for (IConfigurationElement graph : graphes) {
+			Graph g = new Graph(graph.getAttribute("name")); //$NON-NLS-1$
+			this.getAttributes(g, graph);
+			form.addElement(g);
+			
+			// Ajout des definitions des noeuds
+			IConfigurationElement[] nodes = graph.getChildren("Node"); //$NON-NLS-1$
+			for (IConfigurationElement node : nodes) {
+				Node n = new Node(node.getAttribute("name")); //$NON-NLS-1$
+				this.getAttributes(n, node);
+				g.addElement(n);
+			}
+			
+			// Ajout des definitions des arcs
+			IConfigurationElement[] arcs = graph.getChildren("Arc"); //$NON-NLS-1$
+			for (IConfigurationElement arc : arcs) {
+				Arc a = new Arc(arc.getAttribute("name")); //$NON-NLS-1$
+				this.getAttributes(a, arc);
+				g.addElement(a);
+			}			
+		}
+	}
+	
+	/**
+	 * Extrait les attributs des elements de la description et les ajoute à l'élément de formalisme
+	 * @param element L'élément de formalisme qui est entrain d'être construit
+	 * @param current L'élément de description entrain d'être lu
+	 */
+	private void getAttributes(FormalismElement element, IConfigurationElement current) {
+		// Ajout des definitions des attributs
+		IConfigurationElement[] attributes = current.getChildren("Attribute"); //$NON-NLS-1$
+		for (IConfigurationElement attribute : attributes) {
+			Attribute a = new Attribute(attribute.getAttribute("name"), getBool(attribute.getAttribute("multiline")), getBool(attribute.getAttribute("drawable")));  //$NON-NLS-1$ //$NON-NLS-2$//$NON-NLS-3$
+			element.addAttribute(a);
+		}
 	}
 
 	/**
@@ -58,5 +123,14 @@ public final class FormalismManager {
 	 */
 	public List<Formalism> getListOfFormalisms() {
 		return formalisms;
+	}
+	
+	/**
+	 * Convertit la chaine de caractères true en boolean <code>true</code> et inversement pour false
+	 * @param value true or false
+	 * @return <code>true</code> ou <code>false</code>
+	 */
+	private boolean getBool (String value) {
+		return "true".equalsIgnoreCase(value); //$NON-NLS-1$
 	}
 }
--- branches/coloane-newmodel/application/core/src/fr/lip6/move/coloane/core/motor/formalisms/elements/Graph.java
+++ branches/coloane-newmodel/application/core/src/fr/lip6/move/coloane/core/motor/formalisms/elements/Graph.java
@@ -1,11 +1,16 @@
 package fr.lip6.move.coloane.core.motor.formalisms.elements;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Cette classe décrit un conteneur de noeuds, d'arcs et d'attributs d'un formalisme.<br>
  * Chaque instance de formalisme contient forcément au moins un élément de ce type.<br>
  * Dans le cas de la hiérachie, ce conteneur peut être contenu dans un noeud de plus haut niveau.
  */
 public class Graph extends FormalismElement {
+	
+	private List<FormalismElement> children = new ArrayList<FormalismElement>();
 
 	/**
 	 * Constructeur
@@ -13,5 +18,14 @@ public class Graph extends FormalismElement {
 	 */
 	public Graph(String name) {
 		super(name);
+	}
+	
+	/**
+	 * Ajout d'un element de base a la definition d'un graphe. Notion de hierarchie
+	 * @param element {@link FormalismElement} de base a ajouter aux enfants possibles du graphe.
+	 */
+	public final void addElement(FormalismElement element) {
+		if (element == null) { return; }
+		this.children.add(element);
 	}
 }
