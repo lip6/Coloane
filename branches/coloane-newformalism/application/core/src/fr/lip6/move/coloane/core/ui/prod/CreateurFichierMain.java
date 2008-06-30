4be90549ad9cee74db475a28b90a2080ab9bdf86
ryasri <ryasri@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-06-30 16:57:56 +0000

    Classe Main créatrice du Creatrice du Formalism

---
+++ branches/coloane-newformalism/application/core/src/fr/lip6/move/coloane/core/ui/prod/CreateurFichierMain.java
@@ -0,0 +1,27 @@
+package fr.lip6.move.coloane.core.ui.prod;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+public final class CreateurFichierMain {
+
+	public static void main(String args[]){
+
+		String xmlFile= "./src/fr/lip6/move/coloane/core/ui/prod/producteurFormalism.xml";
+		
+		ProdForm newF = new ProdForm();
+		
+		SAXParserFactory factory;
+
+		factory = SAXParserFactory.newInstance();
+		try {
+			// Phase de Creatin de la Classe correspondante au formalism voulu
+
+			SAXParser saxParser = factory.newSAXParser();
+			saxParser.parse(xmlFile, newF);
+
+		} catch (Exception e) {
+				e.printStackTrace();
+		}
+	}
+}
