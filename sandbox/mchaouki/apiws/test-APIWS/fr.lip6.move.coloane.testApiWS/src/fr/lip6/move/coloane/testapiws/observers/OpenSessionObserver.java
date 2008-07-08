eda0c0a7a5d6a317a28c71c50d1f4c599863caea
mchaouki <mchaouki@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-07-08 12:15:59 +0000

    Modification de l'observer pour la nouvelle implementation des menus.

--- sandbox/mchaouki/apiws/test-APIWS/fr.lip6.move.coloane.testApiWS/src/fr/lip6/move/coloane/testapiws/observers/OpenSessionObserver.java
+++ sandbox/mchaouki/apiws/test-APIWS/fr.lip6.move.coloane.testApiWS/src/fr/lip6/move/coloane/testapiws/observers/OpenSessionObserver.java
@@ -2,16 +2,21 @@ package fr.lip6.move.coloane.testapiws.observers;
 
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.Shell;
 
 import fr.lip6.move.coloane.apiws.interfaces.evenements.IAnswerOpenSession;
 import fr.lip6.move.coloane.apiws.interfaces.objects.menu.IItemMenu;
-import fr.lip6.move.coloane.apiws.interfaces.objects.menu.IOptionMenu;
+import fr.lip6.move.coloane.apiws.interfaces.objects.menu.IOptionItem;
 import fr.lip6.move.coloane.apiws.interfaces.objects.menu.IRootMenu;
 import fr.lip6.move.coloane.apiws.interfaces.objects.menu.ISubMenu;
+import fr.lip6.move.coloane.apiws.interfaces.objects.service.IServiceSimple;
 import fr.lip6.move.coloane.apiws.interfaces.observers.IOpenSessionObserver;
+import fr.lip6.move.coloane.apiws.interfaces.session.IApiSession;
+import fr.lip6.move.coloane.testapiws.Activator;
 
 public class OpenSessionObserver implements IOpenSessionObserver {
 
@@ -30,6 +35,7 @@ public class OpenSessionObserver implements IOpenSessionObserver {
 			
 			MenuItem rootMenuHeader = new MenuItem(menu,SWT.CASCADE);
 			rootMenuHeader.setText(m.getName());
+			
 			Menu rootMenu = new Menu(shell,SWT.DROP_DOWN);
 			rootMenuHeader.setMenu(rootMenu);
 			
@@ -41,20 +47,90 @@ public class OpenSessionObserver implements IOpenSessionObserver {
 	
 	public void printMenu(ISubMenu sm,String dec, Menu m1){
 		System.out.println(dec+"["+(sm.isVisibility()?"+":"-")+"SUB  ] "+sm.getName()+" : validation="+sm.isValidation());
-		if (sm.getItems()!=null){
-			for (IItemMenu smenu : sm.getItems()){
-				System.out.println(dec+"   ["+(sm.isVisibility()?"+":"-")+"Items]"+smenu.getName()+" : validation="+sm.isValidation());
-				MenuItem item =  new MenuItem(m1,SWT.PUSH);
-				item.setText(smenu.getName());
-			}
-		}
-		if (sm.getOption()!=null){
-			for (IOptionMenu smenu : sm.getOption()){
-				System.out.println(dec+"   ["+(sm.isVisibility()?"+":"-")+"OP   ] "+smenu.getName()+" : validation="+sm.isValidation());
+
+		if (sm.getOptions()!=null){
+			for (IOptionItem smenu : sm.getOptions()){
+				System.out.println(dec+"   ["+(sm.isVisibility()?"+":"-")+"OP  ] "+smenu.getName()+" : validation="+sm.isValidation());
 				MenuItem item =  new MenuItem(m1,SWT.CHECK);
 				item.setText(smenu.getName());
+				item.setEnabled(smenu.isVisibility());
 			}
 		}
+
+		if (sm.getServiceSimpleItems()!=null){
+			for (final IItemMenu smenu : sm.getServiceSimpleItems()){
+				System.out.println(dec+"   ["+(sm.isVisibility()?"+":"-")+"ss   ]"+smenu.getName()+" : validation="+sm.isValidation());
+				MenuItem item =  new MenuItem(m1,SWT.PUSH);
+				item.setText(smenu.getName());
+				item.setEnabled(smenu.isVisibility());
+				item.addSelectionListener(
+						new SelectionListener(){
+
+							public void widgetDefaultSelected(SelectionEvent e) {
+								// TODO Auto-generated method stub
+
+							}
+
+							public void widgetSelected(SelectionEvent e) {
+								// TODO Auto-generated method stub
+								IApiSession session = Activator.getDefault().getSession();
+								IServiceSimple ss = (IServiceSimple) session.getServicesAvailable().getServices().get(smenu.getName());
+								String msg = 
+									"Name\t\t: "+ss.getName()+"\n"+
+									"Domaine\t\t: "+ss.getDomain()+"\n"+
+									"Cardinality\t: "+ss.getCardinality()+"\n"+
+									"Interaction\t: "+ss.isInteraction()+"\n"+
+									"Stop\t\t\t: "+ss.isStop()+"\n"+
+									"Validation\t\t: "+ss.isValidation()+"\n"+
+									"Visibility\t\t: "+ss.isVisibility()+"\n"+
+									"Help\t\t\t: "+"\n";
+								for (String help : ss.getHelps()){
+									msg= msg+help+"\n";
+								}
+								MessageDialog.openInformation(shell, "APIWS", msg );
+							}
+
+						});
+			}
+		}
+
+		if (sm.getServiceWithOneObjectItems()!=null){
+			for (IItemMenu smenu : sm.getServiceWithOneObjectItems()){
+				System.out.println(dec+"   ["+(sm.isVisibility()?"+":"-")+"soo  ]"+smenu.getName()+" : validation="+sm.isValidation());
+				MenuItem item =  new MenuItem(m1,SWT.PUSH);
+				item.setText(smenu.getName());
+				item.setEnabled(smenu.isVisibility());
+			}
+		}
+		
+		if (sm.getServiceWithObjectsItems()!=null){
+			for (IItemMenu smenu : sm.getServiceWithObjectsItems()){
+				System.out.println(dec+"   ["+(sm.isVisibility()?"+":"-")+"sos  ]"+smenu.getName()+" : validation="+sm.isValidation());
+				MenuItem item =  new MenuItem(m1,SWT.PUSH);
+				item.setText(smenu.getName());
+				item.setEnabled(smenu.isVisibility());
+			}
+		}
+		
+		if (sm.getServiceWithOneTextItems()!=null){
+			for (IItemMenu smenu : sm.getServiceWithOneTextItems()){
+				System.out.println(dec+"   ["+(sm.isVisibility()?"+":"-")+"sot  ]"+smenu.getName()+" : validation="+sm.isValidation());
+				MenuItem item =  new MenuItem(m1,SWT.PUSH);
+				item.setText(smenu.getName());
+				item.setEnabled(smenu.isVisibility());
+			}
+		}
+		
+		if (sm.getServiceWithTextsItems()!=null){
+			for (IItemMenu smenu : sm.getServiceWithTextsItems()){
+				System.out.println(dec+"   ["+(sm.isVisibility()?"+":"-")+"sts  ]"+smenu.getName()+" : validation="+sm.isValidation());
+				MenuItem item =  new MenuItem(m1,SWT.PUSH);
+				item.setText(smenu.getName());
+				item.setEnabled(smenu.isVisibility());
+			}
+		}
+		
+		
 		if (sm.getSubMenus()!=null){
 			for (ISubMenu smenu : sm.getSubMenus()){
 				MenuItem subMenuHeader = new MenuItem(m1,SWT.CASCADE);
@@ -63,7 +139,7 @@ public class OpenSessionObserver implements IOpenSessionObserver {
 				Menu subMenu = new Menu(shell,SWT.DROP_DOWN);
 				subMenuHeader.setMenu(subMenu);
 				
-				printMenu(smenu,"",subMenu);
+				printMenu(smenu,"  "+dec,subMenu);
 			}
 		}
 	}
