6a389076f167220d2cd68c23c6b6db924875be1b
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-07-09 16:43:02 +0000

    fix fetch phase of element formalism

--- branches/coloane-newmodel/application/core/src/fr/lip6/move/coloane/core/ui/palette/PaletteFactory.java
+++ branches/coloane-newmodel/application/core/src/fr/lip6/move/coloane/core/ui/palette/PaletteFactory.java
@@ -70,7 +70,7 @@ public final class PaletteFactory {
 		CombinedTemplateCreationEntry component; // Un element de la palette
 
 		// Parcours de la liste des elements de base associe au formalisme
-		for (IElementFormalism element : formalism.getListOfFormalismElement()) {
+		for (IElementFormalism element : formalism.getMasterGraph().getAllElementFormalism()) {
 
 			// Si l'element parcouru est un noeur, on l'insere dans la palette
 			if (element instanceof INodeFormalism) {
@@ -106,7 +106,7 @@ public final class PaletteFactory {
 		ConnectionCreationToolEntry component; /* Un element de la palette */
 
 		// Parcours de la liste des elements de base
-		for (IElementFormalism element : formalism.getListOfFormalismElement()) {
+		for (IElementFormalism element : formalism.getMasterGraph().getAllElementFormalism()) {
 
 			// Si l'element parcouru est un arc
 			if (element instanceof IArcFormalism) {
