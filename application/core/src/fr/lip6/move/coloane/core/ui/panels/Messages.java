9246d713adc630dbd0363d518cd51f0b9d7c7c1c
jbvoron <Jean-Baptiste.Voron@lip6.fr>
2008-07-10 11:53:34 +0000

    externalize strings

--- application/core/src/fr/lip6/move/coloane/core/ui/panels/Messages.java
+++ application/core/src/fr/lip6/move/coloane/core/ui/panels/Messages.java
@@ -8,6 +8,10 @@ public final class Messages extends NLS {
 	public static String HistoryView_1;
 	public static String HistoryView_2;
 	public static String HistoryView_3;
+	public static String ResultsView_0;
+	public static String ResultsView_1;
+	public static String ResultsView_2;
+	public static String ResultsView_3;
 	static {
 		// initialize resource bundle
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
