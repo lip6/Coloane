63c1762622f0afe3daa2f2c079fccca65baabb59
mchaouki <mchaouki@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-07-11 13:12:45 +0000

    Ajout d'une methode getResult().

--- sandbox/mchaouki/apiws/interfaces/fr.lip6.move.coloane.interfaces2/src/fr/lip6/move/coloane/interfaces/api/objects/result/IResult.java
+++ sandbox/mchaouki/apiws/interfaces/fr.lip6.move.coloane.interfaces2/src/fr/lip6/move/coloane/interfaces/api/objects/result/IResult.java
@@ -2,4 +2,9 @@ package fr.lip6.move.coloane.interfaces.api.objects.result;
 
 public interface IResult {
 
+	/**
+	 * Recupere le resultat
+	 * @return le resultat
+	 */
+	public IResult getResult();
 }
