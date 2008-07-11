a0b79274407ddf13ac41f9959e22ae174587e672
mchaouki <mchaouki@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-07-11 13:37:25 +0000

    Initial import: Creation des interfaces

---
+++ branches/coloane-apiws/application/interfaces/src/fr/lip6/move/coloane/interfaces/api/evenements/IReceptResult.java
@@ -0,0 +1,12 @@
+package fr.lip6.move.coloane.interfaces.api.evenements;
+
+import fr.lip6.move.coloane.interfaces.objects.result.IResult;
+
+public interface IReceptResult {
+
+	/**
+	 * Recupere le resultat
+	 * @return le resultat
+	 */
+	public IResult getResult();
+}
