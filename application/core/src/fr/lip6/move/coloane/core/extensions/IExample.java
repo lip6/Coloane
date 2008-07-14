0dd395ea41792249517ddc27a1e59364f2458665
jbvoron <Jean-Baptiste.Voron@lip6.fr>
2008-07-14 14:14:15 +0000

    example extensions are using IGraph instead of IModelImpl

--- application/core/src/fr/lip6/move/coloane/core/extensions/IExample.java
+++ application/core/src/fr/lip6/move/coloane/core/extensions/IExample.java
@@ -1,6 +1,6 @@
 package fr.lip6.move.coloane.core.extensions;
 
-import fr.lip6.move.coloane.core.ui.model.IModelImpl;
+import fr.lip6.move.coloane.interfaces.model.IGraph;
 
 /**
  * Interface que doivent implémenter tous les <i>constructeur</i> de modèles d'exemples.
@@ -12,6 +12,6 @@ public interface IExample {
 	 * Construction d'un modèle de haut-niveau
 	 * @return Le modèle construit
 	 */
-	IModelImpl buildModel();
+	IGraph buildModel();
 
 }
