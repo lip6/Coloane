8570bf6d071ce994f35490859a04d0ed0ea204a6
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-07-14 14:59:59 +0000

    checkstyle and externalize strings

--- trunk/application/core/src/fr/lip6/move/coloane/core/ui/palette/Messages.java
+++ trunk/application/core/src/fr/lip6/move/coloane/core/ui/palette/Messages.java
@@ -4,6 +4,8 @@ import org.eclipse.osgi.util.NLS;
 
 public final class Messages extends NLS {
 	private static final String BUNDLE_NAME = "fr.lip6.move.coloane.core.ui.palette.messages"; //$NON-NLS-1$
+	public static String PaletteFactory_2;
+	public static String PaletteFactory_3;
 	public static String PaletteFactory_4;
 	public static String PaletteFactory_5;
 	public static String PaletteFactory_6;
