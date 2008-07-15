d4ed8da4bd83abd1d538c8d86eb3162ca37d7316
jbvoron <Jean-Baptiste.Voron@lip6.fr>
2008-07-15 17:09:01 +0000

    Define directedit for attributes

---
+++ application/core/src/fr/lip6/move/coloane/core/ui/editpart/AttributeCellEditorLocator.java
@@ -0,0 +1,44 @@
+package fr.lip6.move.coloane.core.ui.editpart;
+
+import org.eclipse.draw2d.Label;
+import org.eclipse.draw2d.geometry.Rectangle;
+import org.eclipse.gef.tools.CellEditorLocator;
+import org.eclipse.jface.viewers.CellEditor;
+import org.eclipse.swt.widgets.Text;
+
+
+public final class AttributeCellEditorLocator implements CellEditorLocator {
+
+	private Label attributeFigure;
+
+	public AttributeCellEditorLocator(Label attributeFigure) {
+		setLabel(attributeFigure);
+	}
+
+	public void relocate(CellEditor celleditor) {
+		Text text = (Text) celleditor.getControl();
+		Rectangle rect = attributeFigure.getClientArea();
+		attributeFigure.translateToAbsolute(rect);
+		org.eclipse.swt.graphics.Rectangle trim = text.computeTrim(0, 0, 0, 0);
+		rect.translate(trim.x, trim.y);
+		rect.width += trim.width;
+		rect.height += trim.height;
+		text.setBounds(rect.x, rect.y, rect.width, rect.height);
+	}
+
+	/**
+	 * Returns the stickyNote figure.
+	 */
+	protected Label getLabel() {
+		return attributeFigure;
+	}
+
+	/**
+	 * Sets the Sticky note figure.
+	 * @param stickyNote The stickyNote to set
+	 */
+	protected void setLabel(Label attributeFigure) {
+		this.attributeFigure = attributeFigure;
+	}
+
+}
