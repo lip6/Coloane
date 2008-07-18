b0e13866a39021d8a63616324e196e337d406253
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-07-18 17:43:30 +0000

    adapt export PNML code to PNML plugins (2.0.0)

--- trunk/extensions/importExportPNML/src/fr/lip6/move/coloane/extension/importExportPNML/exportToPNML/ExportToImpl.java
+++ trunk/extensions/importExportPNML/src/fr/lip6/move/coloane/extension/importExportPNML/exportToPNML/ExportToImpl.java
@@ -8,6 +8,7 @@ import fr.lip6.move.coloane.interfaces.model.INode;
 import fr.lip6.move.pnml.framework.general.PnmlExport;
 import fr.lip6.move.pnml.framework.utils.IdRepository;
 import fr.lip6.move.pnml.framework.utils.exception.InvalidIDException;
+import fr.lip6.move.pnml.framework.utils.exception.VoidRepositoryException;
 import fr.lip6.move.pnml.ptnet.hlapi.ArcHLAPI;
 import fr.lip6.move.pnml.ptnet.hlapi.NameHLAPI;
 import fr.lip6.move.pnml.ptnet.hlapi.NodeHLAPI;
@@ -38,27 +39,36 @@ public class ExportToImpl implements IExportTo {
 			PetriNetHLAPI net = new PetriNetHLAPI("colo-export", PNTypeHLAPI.COREMODEL, new NameHLAPI("coloane"), doc);
 			PageHLAPI page = new PageHLAPI("main", new NameHLAPI("main"), null, net);
 
+			// Création des noeuds
 			for (INode node : graph.getNodes()) {
+				
+				// Les places
 				if (node.getNodeFormalism().getName().equalsIgnoreCase("place")) {
 					PlaceHLAPI place = new PlaceHLAPI(String.valueOf(node.getId()), page);
 					place.setName(new NameHLAPI(node.getAttribute("name").getValue()));
 					place.setInitialMarking(new PTMarkingHLAPI(Integer.valueOf(node.getAttribute("marking").getValue())));
 				}
 
+				// Les transitions
 				if (node.getNodeFormalism().getName().equalsIgnoreCase("transition")) {
 					TransitionHLAPI transition = new TransitionHLAPI(String.valueOf(node.getId()), page);
 					transition.setName(new NameHLAPI(node.getAttribute("name").getValue()));
 				}
 			}
 
+			// Création des arcs
+			IdRepository repo = new IdRepository();
 			for (IArc arc : graph.getArcs()) {
-				NodeHLAPI source = (NodeHLAPI) IdRepository.getObject(String.valueOf(arc.getSource().getId()));
-				NodeHLAPI target = (NodeHLAPI) IdRepository.getObject(String.valueOf(arc.getTarget().getId()));
+				NodeHLAPI source = (NodeHLAPI) repo.getObject(String.valueOf(arc.getSource().getId()));
+				NodeHLAPI target = (NodeHLAPI) repo.getObject(String.valueOf(arc.getTarget().getId()));
 				ArcHLAPI a = new ArcHLAPI(String.valueOf(arc.getId()), source, target);
 				a.setContainerPage(page);
 			}
 		} catch (InvalidIDException e) {
 			System.out.println("Double aie...");
+			return;
+		} catch (VoidRepositoryException vre) {
+			System.out.println("Triple aie...");
 			return;
 		}
 
