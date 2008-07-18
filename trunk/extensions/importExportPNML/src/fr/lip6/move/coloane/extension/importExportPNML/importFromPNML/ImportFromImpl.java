be5c9d5f508b1c9f16975091a9677aec58c24062
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-07-18 18:12:40 +0000

    remove warnings waiting for a real implement of import from PNML

--- trunk/extensions/importExportPNML/src/fr/lip6/move/coloane/extension/importExportPNML/importFromPNML/ImportFromImpl.java
+++ trunk/extensions/importExportPNML/src/fr/lip6/move/coloane/extension/importExportPNML/importFromPNML/ImportFromImpl.java
@@ -3,9 +3,6 @@ package fr.lip6.move.coloane.extension.importExportPNML.importFromPNML;
 import fr.lip6.move.coloane.core.exceptions.ColoaneException;
 import fr.lip6.move.coloane.core.extensions.IImportFrom;
 import fr.lip6.move.coloane.interfaces.model.IGraph;
-import fr.lip6.move.pnml.framework.general.PnmlImport;
-import fr.lip6.move.pnml.framework.hlapi.HLAPIClass;
-import fr.lip6.move.pnml.ptnet.PetriNetDoc;
 
 public class ImportFromImpl implements IImportFrom {
 
@@ -20,10 +17,7 @@ public class ImportFromImpl implements IImportFrom {
 	 */
 	public final IGraph importFrom(String filePath, String formalism) throws ColoaneException {
 
-		PnmlImport pi = new PnmlImport();
 		try {
-		    HLAPIClass retour = pi.importFile(filePath);
-		    PetriNetDoc document = (PetriNetDoc) retour;
 		    return null;
 		} catch (Exception e) {
 		    System.out.println("Aie...");
