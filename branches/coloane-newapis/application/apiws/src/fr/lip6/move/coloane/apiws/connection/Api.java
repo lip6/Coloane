b23399620d02c3b684b5f9a58af84cd6a8641c4d
mchaouki <mchaouki@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-07-21 14:04:10 +0000

    Implementation de IApi pour la classe Api.

--- branches/coloane-newapis/application/apiws/src/fr/lip6/move/coloane/apiws/connection/Api.java
+++ branches/coloane-newapis/application/apiws/src/fr/lip6/move/coloane/apiws/connection/Api.java
@@ -1,22 +1,23 @@
 package fr.lip6.move.coloane.apiws.connection;
 
+import fr.lip6.move.coloane.interfaces.api.connection.IApi;
 import fr.lip6.move.coloane.interfaces.api.connection.IApiConnection;
 
-public class Api {
+public class Api implements IApi{
 	
 	private static final String uiName = "Coloane";
 	
 	private static final String uiVersion ="0.0.1-alpha";
 
-	public static String getUiName() {
+	public String getUiName() {
 		return uiName;
 	}
 
-	public static String getUiVersion() {
+	public String getUiVersion() {
 		return uiVersion;
 	}
 	
-	public static IApiConnection getApiConnection() {
+	public IApiConnection getApiConnection() {
 		return new ApiConnection();
 	}
 
