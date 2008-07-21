f436902a11d6dcc053304a15316817d4246589d7
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-07-21 16:16:41 +0000

    checkstyle modification (javadoc)

--- branches/coloane-newapis/application/core/src/fr/lip6/move/coloane/core/ui/dialogs/AuthenticationInformation.java
+++ branches/coloane-newapis/application/core/src/fr/lip6/move/coloane/core/ui/dialogs/AuthenticationInformation.java
@@ -2,6 +2,9 @@ package fr.lip6.move.coloane.core.ui.dialogs;
 
 import fr.lip6.move.coloane.core.main.Coloane;
 
+/**
+ * Définition des informations d'authentification auprès de la palte-forme
+ */
 public class AuthenticationInformation {
 
 	private String login;
@@ -9,27 +12,52 @@ public class AuthenticationInformation {
 	private String ip;
 	private int port;
 
-	public AuthenticationInformation(String loginI, String passI, String ipI, int portI) {
-		this.login = loginI;
-		this.pass = passI;
-		this.ip = ipI;
-		this.port = portI;
+	/**
+	 * Constructeur
+	 * @param login Le login de connexion
+	 * @param pass Le mot de passe pour ce compte
+	 * @param ip L'ip de la plate-forme
+	 * @param port Le port de la plate-forme
+	 */
+	public AuthenticationInformation(String login, String pass, String ip, int port) {
+		this.login = login;
+		this.pass = pass;
+		this.ip = ip;
+		this.port = port;
 	}
 
+	/**
+	 * @return le login de connexion
+	 */
 	public final String getLogin() {
 		return login;
 	}
+
+	/**
+	 * @return le mot de passe
+	 */
 	public final String getPass() {
 		return pass;
 	}
+
+	/**
+	 * @return l'ip de la plate-forme
+	 */
 	public final String getIp() {
 		return ip;
 	}
+
+	/**
+	 * @return le port de la plate-forme
+	 */
 	public final int getPort() {
 		return port;
 	}
-	
+
+	/**
+	 * @return le type de la plate-forme
+	 */
 	public final String getApiType() {
-		return Coloane.getParam("API_TYPE");
+		return Coloane.getParam("API_TYPE"); //$NON-NLS-1$
 	}
 }
