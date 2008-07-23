1ff0fb10d63f8571bb1b22897576cabdc7eb411e
mchaouki <mchaouki@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-07-23 14:04:00 +0000

    * Modifications pour le checkStyle. * Modification de getDefault en getDefaultValue. * Ajout d'une methode getLines.

--- branches/coloane-newapis/application/interfaces/src/fr/lip6/move/coloane/interfaces/objects/dialog/IDialog.java
+++ branches/coloane-newapis/application/interfaces/src/fr/lip6/move/coloane/interfaces/objects/dialog/IDialog.java
@@ -1,5 +1,7 @@
 package fr.lip6.move.coloane.interfaces.objects.dialog;
 
+import java.util.List;
+
 /**
  * Interface fournie par l'API pour Coloane
  * Cette interface propose les interractions possibles avec un element de dialogue
@@ -39,63 +41,64 @@ public interface IDialog {
 
 	/**
 	 * Retourne le type des boutons affiches dans la boite dialogue
-	 * @return un indicateur
-	 * @see IDialog
+	 * @return le type des boutons affiches dans la boite dialogue
 	 */
-	public int getButtonType();
+	int getButtonType();
 
 	/**
 	 * Retourne le message par defaut a afficher dans la boite de dialogue
 	 * @return La chaine de caracteres contenant la valeur par defaut
 	 */
-	public String getDefault();
+	String getDefaultValue();
 
 	/**
 	 * Indique le message d'aide associe avec la boite de dialogue
 	 * @return La chaine de caracteres contenant le message d'aide
 	 */
-	public String getHelp();
+	String getHelp();
 
 	/**
 	 * Indique l'identifiant de la boite de dialogue.<br>
-	 * Chaque boite de dialogue creee par la plate-forme possede un identifiant.
-	 * @return L'identifiant
+	 * Chaque boite de dialogue créée par la plate-forme possede un identifiant.
+	 * @return L'identifiant de la boite de dialogue.
 	 */
-	public int getId();
+	int getId();
 
 	/**
 	 * Indique le type de saisie autorise<br>
 	 * Les valeurs possibles sont presentees dans IDialogCom
 	 * @return Le type de saisie de la boite de dialogue
 	 */
-	public int getInputType();
+	int getInputType();
 
 	/**
 	 * Indique le message a afficher dans la boite de dialogue
 	 * @return La chaine de caractere a afficher dans la boite de dialogue
 	 */
-	public String getMessage();
+	String getMessage();
 
 	/**
 	 * Indique si la saisie a le droit d'etre multilignes
 	 * @return Un indicateur
-	 * @see IDialog
 	 */
-	public int getMultiLine();
+	int getLineType();
 
 	/**
 	 * Indique le titre de la boite de dialogue
 	 * @return Le titre a afficher en tant que titre de la boite de dialogue
 	 */
-	public String getTitle();
+	String getTitle();
 
 	/**
 	 * Retourne le type de boite de dialogue
 	 * @return Le type de boite de dialogue
-	 * @see IDialog
 	 */
-	public int getType();
+	int getType();
 
+	/**
+	 * Recupere la liste des lignes.
+	 * @return la liste des lignes.
+	 */
+	List<String> getLines();
 
-	
 }
