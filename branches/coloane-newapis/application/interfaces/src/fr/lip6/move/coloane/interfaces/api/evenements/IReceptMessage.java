03cc0e04794c6abc9b09b166f49a8f6ff3c43f2a
mchaouki <mchaouki@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-07-23 15:56:47 +0000

    Ajout de constantes pour differencie les messages.

--- branches/coloane-newapis/application/interfaces/src/fr/lip6/move/coloane/interfaces/api/evenements/IReceptMessage.java
+++ branches/coloane-newapis/application/interfaces/src/fr/lip6/move/coloane/interfaces/api/evenements/IReceptMessage.java
@@ -5,6 +5,13 @@ package fr.lip6.move.coloane.interfaces.api.evenements;
  */
 public interface IReceptMessage {
 
+	int ADMINISTRATOR_MESSAGE = 1;
+	int WARRNING_MESSAGE = 2;
+	int COPYRIGHT_MESSAGE = 3;
+	int TRACE_MESSAGE = 4;
+	int ERROR_MESSAGE = 5;
+
+
 	/**
 	 * Recupere le type du message recu
 	 * @return le type du message recu
