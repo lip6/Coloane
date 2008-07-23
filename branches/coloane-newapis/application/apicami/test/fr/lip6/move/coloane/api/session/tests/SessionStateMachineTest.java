a2744c2834027f2238bfbf7811a78125cdc5a353
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-07-23 19:04:44 +0000

    déplacement des tests du src vers test

---
+++ branches/coloane-newapis/application/apicami/test/fr/lip6/move/coloane/api/session/tests/SessionStateMachineTest.java
@@ -0,0 +1,15 @@
+package fr.lip6.move.coloane.api.session.tests;
+
+import junit.framework.TestCase;
+
+public class SessionStateMachineTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
