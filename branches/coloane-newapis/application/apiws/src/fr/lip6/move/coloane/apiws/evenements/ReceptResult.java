ed37049579b5f703d4e16151dc6d7b561951e6d1
mchaouki <mchaouki@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-07-23 15:12:49 +0000

    Petit modification.

--- branches/coloane-newapis/application/apiws/src/fr/lip6/move/coloane/apiws/evenements/ReceptResult.java
+++ branches/coloane-newapis/application/apiws/src/fr/lip6/move/coloane/apiws/evenements/ReceptResult.java
@@ -1,19 +1,10 @@
 package fr.lip6.move.coloane.apiws.evenements;
 
 import fr.lip6.move.coloane.interfaces.api.evenements.IReceptResult;
-import fr.lip6.move.coloane.interfaces.objects.result.IResult;
 
 /**
  * Cette classe représent un objet (qui définie des résultats) à envoyer aux observateurs d'événements: réception de résultats.
  */
 public class ReceptResult implements IReceptResult {
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public final IResult getResult() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 }
