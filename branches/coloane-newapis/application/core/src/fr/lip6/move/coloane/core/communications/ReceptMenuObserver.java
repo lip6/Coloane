5162bfba307a05ebc3a2b93426fcf21384de984d
cdemoulins <cdemoulins@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-07-25 15:28:47 +0000

    Affichage du menu dans la console pour le debug

--- branches/coloane-newapis/application/core/src/fr/lip6/move/coloane/core/communications/ReceptMenuObserver.java
+++ branches/coloane-newapis/application/core/src/fr/lip6/move/coloane/core/communications/ReceptMenuObserver.java
@@ -1,25 +1,42 @@
 package fr.lip6.move.coloane.core.communications;
 
-import java.util.List;
-
+import fr.lip6.move.coloane.core.ui.UserInterface;
 import fr.lip6.move.coloane.interfaces.api.evenements.IReceptMenu;
 import fr.lip6.move.coloane.interfaces.api.observers.IReceptMenuObserver;
-import fr.lip6.move.coloane.interfaces.objects.menu.IRootMenu;
+import fr.lip6.move.coloane.interfaces.objects.menu.IOptionMenu;
+import fr.lip6.move.coloane.interfaces.objects.menu.IServiceMenu;
+import fr.lip6.move.coloane.interfaces.objects.menu.ISubMenu;
 
 /**
  * Permet d'être notifié des changements de menu
  */
 public class ReceptMenuObserver implements IReceptMenuObserver {
-
 	/** {@inheritDoc} */
-	public final void update(IReceptMenu e) {
-		displayMenu("MENU ", e.getMenus());
-	}
-
-	private void displayMenu(String s, List<IRootMenu> menus) {
-		for (IRootMenu menu : menus) {
-			System.out.println(menu.getName());
+	public final void update(IReceptMenu menu) {
+		UserInterface.getInstance().drawMenu(menu);
+		for (ISubMenu subMenu : menu.getMenus()) {
+			printMenus(subMenu, "");
 		}
 	}
 
+	/**
+	 * Affichage d'un menu
+	 * @param menu menu qui doit être affiché
+	 * @param dec décalage correspondant à la profondeur actuelle
+	 */
+	private void printMenus(ISubMenu menu, String dec) {
+		System.out.println(dec + (menu.isVisible() ? "[+]" : "[-]") + " " + "[menu ]" + " " + menu.getName());
+
+		for (IServiceMenu service : menu.getServiceMenus()) {
+			System.out.println("   " + dec + (service.isVisible() ? "[+]" : "[-]") + " " + "[service]" + " " + service.getName());
+		}
+
+		for (IOptionMenu option : menu.getOptions()) {
+			System.out.println("   " + dec + (option.isVisible() ? "[+]" : "[-]") + (option.isValidated() ? " [X] " : " [ ] ") + " [option]" + " " + option.getName());
+		}
+
+		for (ISubMenu subMenu : menu.getSubMenus()) {
+			printMenus(subMenu, "   " + dec);
+		}
+	}
 }
