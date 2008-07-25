4d5b6c66713d0b6c0fa1f2aa6d68260989507d8b
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-07-25 17:33:26 +0000

    N'utilise plus les interfaces

--- branches/coloane-newapis/application/apicami/src/fr/lip6/move/coloane/api/observables/ObservableFactory.java
+++ branches/coloane-newapis/application/apicami/src/fr/lip6/move/coloane/api/observables/ObservableFactory.java
@@ -1,38 +1,46 @@
 package fr.lip6.move.coloane.api.observables;
 
-import fr.lip6.move.coloane.interfaces.api.observables.IBrutalInterruptObservable;
-import fr.lip6.move.coloane.interfaces.api.observables.IDisconnectObservable;
-import fr.lip6.move.coloane.interfaces.api.observables.IReceptDialogObservable;
-import fr.lip6.move.coloane.interfaces.api.observables.IReceptMessageObservable;
-import fr.lip6.move.coloane.interfaces.api.observables.IReceptResultObservable;
+/**
+ * Fabrique des objets observables de l'API
+ *
+ * @author Kahina Bouarab
+ * @author Youcef Belattaf
+ */
+public final class ObservableFactory {
 
-public class ObservableFactory {
+	/** Constructeur privé */
+	private ObservableFactory() { }
 
-	public static ISessionObservable getNewSessionObservable(){
-		return (ISessionObservable) new SessionObservable();
+	/**
+	 * Création d'un observable pour l'ouverture de session.<br>
+	 * Responsable du passage des menus et des modifications sur ceux-ci
+	 * @return L'observeur
+	 */
+	public static ReceptMenuObservable getNewSessionObservable(){
+		return (ReceptMenuObservable) new ReceptMenuObservable();
 	}
 
-	public static IBrutalInterruptObservable getNewBrutalInterruptObservable() {
-		return (IBrutalInterruptObservable) new BrutalInterruptObservable();
+	public static BrutalInterruptObservable getNewBrutalInterruptObservable() {
+		return new BrutalInterruptObservable();
 	}
 
-	public static IReceptDialogObservable getNewreceptDialogObservable() {
-		return (IReceptDialogObservable) new ReceptDialogObservable();
+	public static ReceptDialogObservable getNewreceptDialogObservable() {
+		return new ReceptDialogObservable();
 	}
 
-	public static IDisconnectObservable getNewCloseConnectionObservable() {
-		return (IDisconnectObservable) new DisconnectObservable();
+	public static DisconnectObservable getNewCloseConnectionObservable() {
+		return new DisconnectObservable();
 	}
 
-	public static IReceptMessageObservable getNewSpecialMessageObservable() {
-		return (IReceptMessageObservable) new SpecialMessageObservable();
+	public static ReceptMessageObservable getNewSpecialMessageObservable() {
+		return new ReceptMessageObservable();
 	}
 
-	public static IReceptResultObservable getNewReceptResultObservable() {
-		return (IReceptResultObservable) new ReceptResultObservable();
+	public static ReceptResultObservable getNewReceptResultObservable() {
+		return new ReceptResultObservable();
 	}
 
-	public static IReceptDialogObservable getNewReceptDialogObservable() {
-		return (IReceptDialogObservable) new ReceptDialogObservable();
+	public static ReceptDialogObservable getNewReceptDialogObservable() {
+		return new ReceptDialogObservable();
 	}
 }
