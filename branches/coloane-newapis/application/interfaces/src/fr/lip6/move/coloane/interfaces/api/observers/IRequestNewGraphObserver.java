463460d2fc2ccacc03e3668db8dbcbe352d79d07
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-07-25 20:39:19 +0000

    javadoc... et plus de warning dansles interfaces

--- branches/coloane-newapis/application/interfaces/src/fr/lip6/move/coloane/interfaces/api/observers/IRequestNewGraphObserver.java
+++ branches/coloane-newapis/application/interfaces/src/fr/lip6/move/coloane/interfaces/api/observers/IRequestNewGraphObserver.java
@@ -1,9 +1,12 @@
 package fr.lip6.move.coloane.interfaces.api.observers;
 
+/**
+ * Cette interface définie un observeur pour l'événement: demande d'un nouveau modèle vide.
+ */
 public interface IRequestNewGraphObserver {
 
 	/**
-	 * @param formalism
+	 * @param formalism Le formalisme que doit instancier le graphe
 	 */
 	void update(String formalism);
 }
