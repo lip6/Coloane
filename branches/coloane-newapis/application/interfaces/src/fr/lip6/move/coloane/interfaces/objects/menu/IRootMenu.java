79cdd80539bfc0027f4eaf55cf162a3f01ec946c
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-07-25 10:08:24 +0000

    suppression de setName
    La methode getsubmenus renvoie tous les submenus

--- branches/coloane-newapis/application/interfaces/src/fr/lip6/move/coloane/interfaces/objects/menu/IRootMenu.java
+++ branches/coloane-newapis/application/interfaces/src/fr/lip6/move/coloane/interfaces/objects/menu/IRootMenu.java
@@ -1,26 +1,19 @@
 package fr.lip6.move.coloane.interfaces.objects.menu;
 
+import java.util.List;
+
 /**
  * Cette interface représent un menu principal
  */
 public interface IRootMenu {
 
 	/**
-	 * Récupére le nom du menu principal
 	 * @return le nom du menu principal
 	 */
 	String getName();
 
 	/**
-	 * Modifie le nom du menu principal
-	 * @param name le nouveau nom du menu principal
+	 * @return les sous-menus qui composent le menu principal
 	 */
-	void setName(String name);
-
-	/**
-	 * Récupére le sous-menu qui compose le menu principal
-	 * @return le sous-menu qui compose le menu principal
-	 */
-	ISubMenu getSubMenu();
-
+	List<ISubMenu> getAllSubMenus();
 }
