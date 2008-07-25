227679c1f028214c17d226232c6ac6f1a5fb14a0
cdemoulins <cdemoulins@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-07-25 18:49:14 +0000

    Prise en compte de la mise à jour du menu dans l'affichage de debug du menu.

--- branches/coloane-newapis/application/core/src/fr/lip6/move/coloane/core/communications/ReceptMenuObserver.java
+++ branches/coloane-newapis/application/core/src/fr/lip6/move/coloane/core/communications/ReceptMenuObserver.java
@@ -1,11 +1,15 @@
 package fr.lip6.move.coloane.core.communications;
 
-import fr.lip6.move.coloane.core.ui.UserInterface;
 import fr.lip6.move.coloane.interfaces.api.evenements.IReceptMenu;
 import fr.lip6.move.coloane.interfaces.api.observers.IReceptMenuObserver;
+import fr.lip6.move.coloane.interfaces.objects.menu.IItemMenu;
 import fr.lip6.move.coloane.interfaces.objects.menu.IOptionMenu;
 import fr.lip6.move.coloane.interfaces.objects.menu.IServiceMenu;
 import fr.lip6.move.coloane.interfaces.objects.menu.ISubMenu;
+import fr.lip6.move.coloane.interfaces.objects.menu.IUpdateMenu;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Permet d'être notifié des changements de menu
@@ -13,30 +17,57 @@ import fr.lip6.move.coloane.interfaces.objects.menu.ISubMenu;
 public class ReceptMenuObserver implements IReceptMenuObserver {
 	/** {@inheritDoc} */
 	public final void update(IReceptMenu menu) {
-		UserInterface.getInstance().drawMenu(menu);
+//		UserInterface.getInstance().drawMenus(menu.getMenus());
 		for (ISubMenu subMenu : menu.getMenus()) {
-			printMenus(subMenu, "");
+			Map<String, IUpdateMenu> mapUpdateMenu = new HashMap<String, IUpdateMenu>();
+			for (IUpdateMenu element : menu.getUpdateMenus()) {
+				mapUpdateMenu.put(element.getServiceName(), element);
+			}
+			printMenus(subMenu, mapUpdateMenu, ""); //$NON-NLS-1$
 		}
 	}
 
 	/**
-	 * Affichage d'un menu
+	 * @param item item concerné
+	 * @param map map contenant les mises à jours a effectuer
+	 * @return [+] si sa item est actif [-] sinon
+	 */
+	private String visibility(IItemMenu item, Map<String, IUpdateMenu> map) {
+		if (map.containsKey(item.getName())) {
+			if (map.get(item.getName()).getState()) {
+				return "[+]"; //$NON-NLS-1$
+			} else {
+				return "[-]"; //$NON-NLS-1$
+			}
+		} else {
+			if (item.isVisible()) {
+				return "[+]"; //$NON-NLS-1$
+			} else {
+				return "[-]"; //$NON-NLS-1$
+			}
+		}
+	}
+
+	/**
+	 * Affichage du menu dans la console
 	 * @param menu menu qui doit être affiché
+	 * @param mapUpdateMenu mise à jours à appliquer
 	 * @param dec décalage correspondant à la profondeur actuelle
 	 */
-	private void printMenus(ISubMenu menu, String dec) {
-		System.out.println(dec + (menu.isVisible() ? "[+]" : "[-]") + " " + "[menu ]" + " " + menu.getName());
+	private void printMenus(ISubMenu menu, Map<String, IUpdateMenu> mapUpdateMenu, String dec) {
+
+		System.out.println(dec + visibility(menu, mapUpdateMenu) + " " + "[menu ]" + " " + menu.getName()); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 
 		for (IServiceMenu service : menu.getServiceMenus()) {
-			System.out.println("   " + dec + (service.isVisible() ? "[+]" : "[-]") + " " + "[service]" + " " + service.getName());
+			System.out.println("   " + dec + visibility(service, mapUpdateMenu) + " " + "[service]" + " " + service.getName()); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 		}
 
 		for (IOptionMenu option : menu.getOptions()) {
-			System.out.println("   " + dec + (option.isVisible() ? "[+]" : "[-]") + (option.isValidated() ? " [X] " : " [ ] ") + " [option]" + " " + option.getName());
+			System.out.println("   " + dec + visibility(option, mapUpdateMenu) + (option.isValidated() ? " [X] " : " [ ] ") + " [option]" + " " + option.getName()); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
 		}
 
 		for (ISubMenu subMenu : menu.getSubMenus()) {
-			printMenus(subMenu, "   " + dec);
+			printMenus(subMenu, mapUpdateMenu, "   " + dec); //$NON-NLS-1$
 		}
 	}
 }
