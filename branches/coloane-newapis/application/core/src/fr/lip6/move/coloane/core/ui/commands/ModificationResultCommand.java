35f49b5479b6ffcda318b29eecce803c1d5dd475
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-08-08 15:08:55 +0000

    gestion des commandes null

--- branches/coloane-newapis/application/core/src/fr/lip6/move/coloane/core/ui/commands/ModificationResultCommand.java
+++ branches/coloane-newapis/application/core/src/fr/lip6/move/coloane/core/ui/commands/ModificationResultCommand.java
@@ -45,7 +45,10 @@ public class ModificationResultCommand extends Command {
 	public final void execute() {
 		try {
 			// Parcours de toutes les commandes et execution de celles-ci
-			for (ICommand command : commands) { command.execute(currentModel); }
+			for (ICommand command : commands) {
+				if (command == null) { continue; }
+				command.execute(currentModel);
+			}
 		} catch (ModelException me) {
 			System.err.println("Erreur lors de l'application des modifications " + me.getMessage()); //$NON-NLS-1$
 			me.printStackTrace();
@@ -68,6 +71,7 @@ public class ModificationResultCommand extends Command {
 	public final void undo() {
 		try {
 			for (int i = this.commands.size() - 1; i >= 0; i--) {
+				if (this.commands.get(i) == null) { continue; }
 				this.commands.get(i).undo(this.currentModel);
 			}
 		} catch (ModelException me) {
