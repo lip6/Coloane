b4c5e4ec41690c32870f481df4e650514ea06b70
Clément Démoulins <clement@archivel.fr>
2008-08-19 17:11:27 +0000

    Correction d'une faute d'orthographe

--- application/interfaces/src/fr/lip6/move/coloane/interfaces/api/evenements/IReceptMessage.java
+++ application/interfaces/src/fr/lip6/move/coloane/interfaces/api/evenements/IReceptMessage.java
@@ -6,7 +6,7 @@ package fr.lip6.move.coloane.interfaces.api.evenements;
 public interface IReceptMessage {
 
 	int ADMINISTRATOR_MESSAGE = 1;
-	int WARRNING_MESSAGE = 2;
+	int WARNING_MESSAGE = 2;
 	int COPYRIGHT_MESSAGE = 3;
 	int TRACE_MESSAGE = 4;
 	int ERROR_MESSAGE = 5;
