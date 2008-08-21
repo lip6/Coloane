131e714e68c994f7cf33df490a12c4568dfe6863
Clément Démoulins <clement@archivel.fr>
2008-08-21 13:02:36 +0000

    Fix ticket #234

--- application/core/src/fr/lip6/move/coloane/core/copypast/PasteCommand.java
+++ application/core/src/fr/lip6/move/coloane/core/copypast/PasteCommand.java
@@ -9,12 +9,17 @@ import fr.lip6.move.coloane.interfaces.model.IArc;
 import fr.lip6.move.coloane.interfaces.model.IGraph;
 import fr.lip6.move.coloane.interfaces.model.INode;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
 
+import org.eclipse.gef.EditPart;
+import org.eclipse.gef.GraphicalViewer;
 import org.eclipse.gef.commands.Command;
 import org.eclipse.gef.ui.actions.Clipboard;
+import org.eclipse.jface.viewers.IStructuredSelection;
 
 /**
  * Commande COLLER
@@ -25,8 +30,10 @@ public class PasteCommand extends Command {
 	private GraphContainer graphContainer;
 	private Map<NodeContainer, INode> nodes = new HashMap<NodeContainer, INode>();
 	private Map<ArcContainer, IArc> arcs = new HashMap<ArcContainer, IArc>();
+	private List<EditPart> selection = new ArrayList<EditPart>();
 
 	private IGraph graph;
+	private GraphicalViewer viewer;
 
 	/**
 	 * Création de la commande de collage
@@ -34,6 +41,7 @@ public class PasteCommand extends Command {
 	 */
 	public PasteCommand(ColoaneEditor editor) {
 		graph = editor.getGraph();
+		viewer = (GraphicalViewer) editor.getAdapter(GraphicalViewer.class);
 	}
 
 	/** {@inheritDoc} */
@@ -54,14 +62,29 @@ public class PasteCommand extends Command {
 			return;
 		}
 		logger.fine("Collage de la sélection"); //$NON-NLS-1$
+
+		// Sauvegarde de la sélection
+		for (Object obj : ((IStructuredSelection) viewer.getSelection()).toList()) {
+			if (obj instanceof EditPart) {
+				selection.add((EditPart) obj);
+			}
+		}
+		viewer.deselectAll();
+
+		// Création des noeuds
 		for (NodeContainer nc : graphContainer.getNodes()) {
 			try {
-				nodes.put(nc, nc.copy(graph));
+				INode node = nc.copy(graph);
+				// On sélectionne les nouveaux noeuds
+				viewer.appendSelection((EditPart) viewer.getEditPartRegistry().get(node));
+				nodes.put(nc, node);
 			} catch (ModelException e) {
 				logger.warning(e.getMessage());
 				e.printStackTrace();
 			}
 		}
+
+		// Création des arcs
 		for (ArcContainer ac : graphContainer.getArcs()) {
 			INode source = nodes.get(graphContainer.getNode(ac.getIdSource()));
 			INode target = nodes.get(graphContainer.getNode(ac.getIdTarget()));
@@ -79,8 +102,10 @@ public class PasteCommand extends Command {
 	/** {@inheritDoc} */
 	@Override
 	public final void redo() {
+		viewer.deselectAll();
 		for (INode node : nodes.values()) {
 			graph.addNode(node);
+			viewer.appendSelection((EditPart) viewer.getEditPartRegistry().get(node));
 		}
 		for (IArc arc : arcs.values()) {
 			graph.addArc(arc);
@@ -96,11 +121,15 @@ public class PasteCommand extends Command {
 	/** {@inheritDoc} */
 	@Override
 	public final void undo() {
+		viewer.deselectAll();
 		for (IArc arc : arcs.values()) {
 			graph.deleteArc(arc);
 		}
 		for (INode node : nodes.values()) {
 			graph.deleteNode(node);
+		}
+		for (EditPart editPart : selection) {
+			viewer.appendSelection(editPart);
 		}
 	}
 }
