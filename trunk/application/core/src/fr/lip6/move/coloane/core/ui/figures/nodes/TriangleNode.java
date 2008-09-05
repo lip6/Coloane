877d77806647cad0a3d3b77b3471e2211590edad
cdemoulins <cdemoulins@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-09-05 15:35:34 +0000

    I finish figure TriangleNode.

--- trunk/application/core/src/fr/lip6/move/coloane/core/ui/figures/nodes/TriangleNode.java
+++ trunk/application/core/src/fr/lip6/move/coloane/core/ui/figures/nodes/TriangleNode.java
@@ -2,17 +2,16 @@ package fr.lip6.move.coloane.core.ui.figures.nodes;
 
 import fr.lip6.move.coloane.core.ui.figures.AbstractNodeFigure;
 
-import org.eclipse.draw2d.ChopboxAnchor;
+import org.eclipse.draw2d.AbstractConnectionAnchor;
 import org.eclipse.draw2d.ConnectionAnchor;
 import org.eclipse.draw2d.Graphics;
+import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.draw2d.geometry.PointList;
 import org.eclipse.draw2d.geometry.Rectangle;
 
 /**
  * Description d'une figure en forme de triangle.<br>
  * Cette figure peut être utilisée pour représenter un noeud sur un modèle.
- *
- * @author Clément Demoulins
  */
 public class TriangleNode extends AbstractNodeFigure {
 	/**
@@ -33,17 +32,20 @@ public class TriangleNode extends AbstractNodeFigure {
 		// Contraintes de la figure imposée par le formalisme
 		Rectangle r = getBounds();
 
-		int p1x = r.x + (r.width / 2);
-		int p1y = r.y + (lineWidth / 2);
-		list.addPoint(p1x, p1y);
+//		int p1x = r.x + (r.width / 2);
+//		int p1y = r.y + (lineWidth / 2);
+//		list.addPoint(p1x, p1y);
+		list.addPoint(r.getTop());
 
-		int p2x = r.x + (lineWidth / 2);
-		int p2y = r.y + r.height - Math.max(1, lineWidth);
-		list.addPoint(p2x, p2y);
+//		int p2x = r.x + (lineWidth / 2);
+//		int p2y = r.y + r.height - Math.max(1, lineWidth);
+//		list.addPoint(p2x, p2y);
+		list.addPoint(r.getBottomLeft().getTranslated(0, -1));
 
-		int p3x = r.x + r.width - Math.max(1, lineWidth);
-		int p3y = r.y + r.height - Math.max(1, lineWidth);
-		list.addPoint(p3x, p3y);
+//		int p3x = r.x + r.width - Math.max(1, lineWidth);
+//		int p3y = r.y + r.height - Math.max(1, lineWidth);
+//		list.addPoint(p3x, p3y);
+		list.addPoint(r.getBottomRight().getTranslated(0, -1));
 
 		//list.addPoint(p1x, p1y);
 
@@ -54,6 +56,27 @@ public class TriangleNode extends AbstractNodeFigure {
 	 * {@inheritDoc}
 	 */
 	public final ConnectionAnchor getConnectionAnchor() {
-		return new ChopboxAnchor(this);
+		return new AbstractConnectionAnchor(this) {
+			public Point getLocation(Point reference) {
+				Rectangle r = getBounds();
+
+				Point[] points = new Point[3];
+				points[0] = r.getTop();
+				points[1] = r.getBottomLeft().getTranslated(0, -1);
+				points[2] = r.getBottomRight().getTranslated(0, -1);
+
+				int distMin = Integer.MAX_VALUE;
+				int indexMin = 0;
+				for (int i = 0; i < points.length; i++) {
+					int distTmp = points[i].getDistance2(reference);
+					if (distTmp < distMin) {
+						indexMin = i;
+						distMin = distTmp;
+					}
+				}
+
+				return points[indexMin];
+			}
+		};
 	}
 }
