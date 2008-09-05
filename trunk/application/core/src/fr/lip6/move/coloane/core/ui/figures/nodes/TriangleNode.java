d82fdf4ad5ba791b18f010167e021a79517c1bdc
cdemoulins <cdemoulins@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-09-05 15:43:26 +0000

    Clean comments

--- trunk/application/core/src/fr/lip6/move/coloane/core/ui/figures/nodes/TriangleNode.java
+++ trunk/application/core/src/fr/lip6/move/coloane/core/ui/figures/nodes/TriangleNode.java
@@ -32,22 +32,9 @@ public class TriangleNode extends AbstractNodeFigure {
 		// Contraintes de la figure imposée par le formalisme
 		Rectangle r = getBounds();
 
-//		int p1x = r.x + (r.width / 2);
-//		int p1y = r.y + (lineWidth / 2);
-//		list.addPoint(p1x, p1y);
 		list.addPoint(r.getTop());
-
-//		int p2x = r.x + (lineWidth / 2);
-//		int p2y = r.y + r.height - Math.max(1, lineWidth);
-//		list.addPoint(p2x, p2y);
 		list.addPoint(r.getBottomLeft().getTranslated(0, -1));
-
-//		int p3x = r.x + r.width - Math.max(1, lineWidth);
-//		int p3y = r.y + r.height - Math.max(1, lineWidth);
-//		list.addPoint(p3x, p3y);
 		list.addPoint(r.getBottomRight().getTranslated(0, -1));
-
-		//list.addPoint(p1x, p1y);
 
 		graphics.drawPolygon(list);
 	}
@@ -57,6 +44,11 @@ public class TriangleNode extends AbstractNodeFigure {
 	 */
 	public final ConnectionAnchor getConnectionAnchor() {
 		return new AbstractConnectionAnchor(this) {
+			/**
+			 * Calcul du point de connection a cette figure.<br>
+			 *
+			 * Le point retourné est le sommet le plus proche du point de référence
+			 */
 			public Point getLocation(Point reference) {
 				Rectangle r = getBounds();
 
