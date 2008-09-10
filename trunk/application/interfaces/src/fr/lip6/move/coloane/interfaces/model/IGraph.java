245df1fbfe28722598d2882c02f18d61445eff26
cdemoulins <cdemoulins@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2008-09-10 09:51:27 +0000

    Début d'implementation des liens.

--- trunk/application/interfaces/src/fr/lip6/move/coloane/interfaces/model/IGraph.java
+++ trunk/application/interfaces/src/fr/lip6/move/coloane/interfaces/model/IGraph.java
@@ -23,12 +23,6 @@ public interface IGraph extends IElement {
 	/** ID de propriete lorsqu'un arc est supprime du modele */
 	String ARC_REMOVED_PROP = "Model.RemovingArc"; //$NON-NLS-1$
 
-	/** ID de propriété lorsqu'une note est ajoutée au modèle */
-	String STICKY_ADD_PROP = "Model.AddSticky"; //$NON-NLS-1$
-
-	/** ID de propriete lorsqu'une note est supprimée du modèle */
-	String STICKY_REMOVED_PROP = "Model.RemovingSticky"; //$NON-NLS-1$
-
 	/** ID de propriete lorsqu'un attribut doit etre ajoute au modele */
 	String ATTRIBUTE_ADDED_PROP = "Model.AddingAttribute"; //$NON-NLS-1$
 
