83798e96d2349d1050b888d407806663defcda1a
jbvoron <Jean-Baptiste.Voron@lip6.fr>
2009-04-02 13:36:11 +0000

    Use of last version of PNML HLAPI
    Use pretty print feature during export

--- extensions/importExportPNML/src/fr/lip6/move/coloane/extension/importExportPNML/exportToPNML/ExportToImpl.java
+++ extensions/importExportPNML/src/fr/lip6/move/coloane/extension/importExportPNML/exportToPNML/ExportToImpl.java
@@ -27,7 +27,7 @@ public class ExportToImpl implements IExportTo {
 	/**
 	 * Constructeur
 	 */
-	public ExportToImpl() { System.err.println("mayou"); }
+	public ExportToImpl() { }
 
 	/** {@inheritDoc} */
 	public final void export(IGraph graph, String filePath, IProgressMonitor monitor) throws ColoaneException {
@@ -37,11 +37,14 @@ public class ExportToImpl implements IExportTo {
 		monitor.beginTask("Export to PNML", totalWork);
 
 		try {
+			// Remise a zero pour eviter des collisions entre ID d'objets
 			ModelRepository.reset();
-			ModelRepository.getInstance().createModelWorkspace("toto");
+			ModelRepository.getInstance().createDocumentWorkspace("coloane-workspace-export");
+			
+			// Creation de l'interface de haut-niveau
 			doc = new PetriNetDocHLAPI();
 			PetriNetHLAPI net = new PetriNetHLAPI("colo-export", PNTypeHLAPI.COREMODEL, new NameHLAPI("coloane"), doc);
-			PageHLAPI page = new PageHLAPI("main", new NameHLAPI("main"), null, net);
+			PageHLAPI page = new PageHLAPI("main-page", new NameHLAPI("main-page"), null, net);
 
 			// Création des noeuds
 			monitor.subTask("Export nodes");
@@ -49,15 +52,15 @@ public class ExportToImpl implements IExportTo {
 				
 				// Les places
 				if (node.getNodeFormalism().getName().equalsIgnoreCase("place")) {
-					PlaceHLAPI place = new PlaceHLAPI(String.valueOf("colo" + node.getId()), page);
-					place.setName(new NameHLAPI(node.getAttribute("name").getValue()));
-					place.setInitialMarking(new PTMarkingHLAPI(Integer.valueOf(node.getAttribute("marking").getValue())));
+					PlaceHLAPI place = new PlaceHLAPI(String.valueOf("n" + node.getId()), page);
+					place.setNameHLAPI(new NameHLAPI(node.getAttribute("name").getValue()));
+					place.setInitialMarkingHLAPI(new PTMarkingHLAPI(Integer.valueOf(node.getAttribute("marking").getValue())));
 				}
 
 				// Les transitions
 				if (node.getNodeFormalism().getName().equalsIgnoreCase("transition")) {
-					TransitionHLAPI transition = new TransitionHLAPI(String.valueOf("colo" + node.getId()), page);
-					transition.setName(new NameHLAPI(node.getAttribute("name").getValue()));
+					TransitionHLAPI transition = new TransitionHLAPI(String.valueOf("n" + node.getId()), page);
+					transition.setNameHLAPI(new NameHLAPI(node.getAttribute("name").getValue()));
 				}
 				monitor.worked(1);
 			}
@@ -65,10 +68,9 @@ public class ExportToImpl implements IExportTo {
 			// Création des arcs
 			monitor.subTask("Export arcs");
 			for (IArc arc : graph.getArcs()) {
-				NodeHLAPI source = (NodeHLAPI) ModelRepository.getInstance().getCurrentIdRepository().getObject("colo" + String.valueOf(arc.getSource().getId()));
-				NodeHLAPI target = (NodeHLAPI) ModelRepository.getInstance().getCurrentIdRepository().getObject("colo" + String.valueOf(arc.getTarget().getId()));
-				ArcHLAPI a = new ArcHLAPI(String.valueOf("colo" + arc.getId()), source, target);
-				a.setContainerPage(page);
+				NodeHLAPI source = (NodeHLAPI) ModelRepository.getInstance().getCurrentIdRepository().getObject("n" + String.valueOf(arc.getSource().getId()));
+				NodeHLAPI target = (NodeHLAPI) ModelRepository.getInstance().getCurrentIdRepository().getObject("n" + String.valueOf(arc.getTarget().getId()));
+				ArcHLAPI a = new ArcHLAPI(String.valueOf("a" + arc.getId()), source, target, page);
 				monitor.worked(1);
 			}
 			
@@ -85,7 +87,7 @@ public class ExportToImpl implements IExportTo {
 
 		try {
 			PnmlExport pex = new PnmlExport();
-			pex.exportObject(doc, filePath);
+			pex.exportObject(doc, filePath, true);
 		} catch (Exception e) {
 			System.out.println("Echec...");
 		}
