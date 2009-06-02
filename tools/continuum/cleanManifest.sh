48fa490dfad23f703a316fee5a533f6a5620cfd9
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2009-06-02 13:10:46 +0000

    Check if other resources need to be reverted... Use only for core project

--- tools/continuum/cleanManifest.sh
+++ tools/continuum/cleanManifest.sh
@@ -15,6 +15,11 @@ if [ -d resources/.svn ]; then
 	svn revert --recursive resources
 fi
 
+if [ -d .svn ]; then
+	echo "Suppression des descripteurs XML"
+	svn revert --recursive .
+fi
+
 if [ `svn stat | wc -l` -gt 0 ]; then
 	echo "Suppression des fichiers non versionnes"
 	svn status --no-ignore | grep '^\?' | sed 's/^\?      //'
