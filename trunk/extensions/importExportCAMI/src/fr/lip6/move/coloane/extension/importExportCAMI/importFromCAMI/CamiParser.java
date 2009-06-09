1e6d966d1b50ce4521e625c909dfca34f11ef1b5
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2009-06-09 18:35:26 +0000

    Checkstyle and English translations (comments)

--- trunk/extensions/importExportCAMI/src/fr/lip6/move/coloane/extension/importExportCAMI/importFromCAMI/CamiParser.java
+++ trunk/extensions/importExportCAMI/src/fr/lip6/move/coloane/extension/importExportCAMI/importFromCAMI/CamiParser.java
@@ -5,94 +5,89 @@ import fr.lip6.move.coloane.interfaces.exceptions.SyntaxErrorException;
 import java.util.StringTokenizer;
 
 /**
- * Ensemble d'outils pour la manipulation de commandes CAMI.<br>
- * On trouvera entre autre :
+ * Tools used for handle CAMI commands such as:<br>
  * <ul>
- * <li> Un parseur d'entier</li>
- * <li> Un parseur de chaine de caracteres</li>
- * <li> Un producteur de chaines conformes CAMI</li>
+ * <li> Integer parser</li>
+ * <li> String parser</li>
+ * <li> CAMI string producer</li>
  * </ul>
  */
 
 public class CamiParser {
 
-	private String message;
+	private String command;
 
 	/**
-	 * Constructeur
-	 * @param commandeCAMI La commande CAMI qui va être parsée
+	 * Constructor
+	 * @param cami The command that is going to be parsed
 	 */
-	public CamiParser(String commandeCAMI) {
-		this.message = commandeCAMI.substring(3);
+	public CamiParser(String cami) {
+		this.command = cami.substring(3);
 	}
 
 	/**
-	 * Retourne l'entier CAMI du message CAMI
-	 * Si aucun entier n'est trouve... La methode renvoie la chaine vide
-	 * @param token Le separateur
-	 * @return l'entier CAMI decouvert (sous forme de chaine de caracteres)
+	 * Parse the command and return the inside integer.<br>
+	 * If no integer is fount, an empty String is returned.
+	 * @param token The separator
+	 * @return The integer (or an empty string is no integer)
 	 */
-	public final String parseInt(String token) throws SyntaxErrorException {
-		try {
-			String res = ""; //$NON-NLS-1$
-			int cpt = 0;
+	public final String parseInt(String token) {
+		String res = ""; //$NON-NLS-1$
+		int cpt = 0;
 
-			if (!(this.message.subSequence(cpt, cpt + 1)).equals(token)) {
-				while (!(this.message.subSequence(cpt, cpt + 1)).equals(token)) {
-					res += this.message.subSequence(cpt, cpt + 1);
-					cpt++;
-				}
+		if (!(this.command.subSequence(cpt, cpt + 1)).equals(token)) {
+			while (!(this.command.subSequence(cpt, cpt + 1)).equals(token)) {
+				res += this.command.subSequence(cpt, cpt + 1);
 				cpt++;
-				this.message = this.message.substring(cpt);
-				return res;
-			} else {
-				this.message = this.message.substring(1);
-				return ""; //$NON-NLS-1$
 			}
-		} catch (Exception e) {
-			throw new SyntaxErrorException();
+			cpt++;
+			this.command = this.command.substring(cpt);
+			return res;
+		} else {
+			this.command = this.command.substring(1);
+			return ""; //$NON-NLS-1$
 		}
 	}
 
 	/**
-	 * Retourne la chaine CAMI du message CAMI
-	 * @param token Le separateur
-	 * @return la chaine CAMI
+	 * Parse the string contained into the CAMI command
+	 * @param token The separator
+	 * @return The string contained into the CAMI command
+	 * @throws SyntaxErrorException The length of the string is incorrect
 	 */
 	public final String parseString(String token) throws SyntaxErrorException {
 		try {
-			StringTokenizer st = new StringTokenizer(this.message);
+			StringTokenizer st = new StringTokenizer(this.command);
 
-			if (!(this.message.subSequence(0, 1)).equals(token)) {
+			if (!(this.command.subSequence(0, 1)).equals(token)) {
 
-				// Recupere la taille indiquee par la commande CAMI
+				// Fetch the length contained into the CAMI command
 				String tailleChaine = st.nextToken(":"); //$NON-NLS-1$
 				int taille = Integer.parseInt(tailleChaine);
 
-				// Recupere le texte ecrit apres la taille (et les :) jusqu'au bout
-				String res = this.message.substring(tailleChaine.length() + 1, tailleChaine.length() + 1 + taille);
-				this.message = this.message.substring(tailleChaine.length() + 1 + taille + 1); // ??
+				// Fetch the remaining string
+				String res = this.command.substring(tailleChaine.length() + 1, tailleChaine.length() + 1 + taille);
+				this.command = this.command.substring(tailleChaine.length() + 1 + taille + 1); // ??
 
 				return res;
 			} else {
-				this.message = this.message.substring(1);
+				this.command = this.command.substring(1);
 				return ""; //$NON-NLS-1$
 			}
-		} catch (Exception e) {
+		} catch (NumberFormatException e) {
 			throw new SyntaxErrorException("Erreur de parse : " + token); //$NON-NLS-1$
 		}
 	}
 
 	/**
-	 * Conversion d'une chaine de carateres en message CAMI.<br>
-	 * En CAMI chaque chaine de carateres est precede de sa taille et de ':'
-	 * @param s la chaine a convertir
-	 * @return la chaine convertie
+	 * Convert a string to a CAMI string.<br>
+	 * A CAMI string is always print after its length
+	 * @param string The string to convert
+	 * @return The CAMI formatted string
 	 */
-	public static String stringToCAMI(String s) {
-
-		if (!s.equals("") || s == null) { //$NON-NLS-1$
-			String cami = "" + s.length() + ":" + s; //$NON-NLS-1$ //$NON-NLS-2$
+	public static String stringToCAMI(String string) {
+		if (!string.equals("") || string == null) { //$NON-NLS-1$
+			String cami = "" + string.length() + ":" + string; //$NON-NLS-1$ //$NON-NLS-2$
 			return cami;
 		} else {
 			return ""; //$NON-NLS-1$
