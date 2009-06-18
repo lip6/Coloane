fc52fb15c85a4234804cc3dd7293674ef42dd340
ythierry <ythierry@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2009-06-18 15:25:30 +0000

    the formalism contribution includes its composite and time petri net v0.0

---
+++ branches/itsFormalism/fr.lip6.move.coloane.core.formalisms.its/src/ResetArc.java
@@ -0,0 +1,44 @@
+
+
+import fr.lip6.move.coloane.core.ui.figures.IArcFigure;
+import fr.lip6.move.coloane.core.ui.figures.RoundedPolylineConnection;
+
+import org.eclipse.draw2d.ColorConstants;
+import org.eclipse.draw2d.PolygonDecoration;
+import org.eclipse.draw2d.geometry.PointList;
+
+/**
+ * Test arc definition.<br>
+ * This arc has a black losange at its end.
+ *
+ * @author yann based on jbv
+ */
+public class ResetArc extends RoundedPolylineConnection implements IArcFigure {
+	/**
+	 * Constructor
+	 */
+	public ResetArc() {
+		setTargetDecoration(buildDecoration());
+	}
+
+	/**
+	 * Create the decoration that will be used for the target-side of the arc
+	 * @return the decoration
+	 */
+	private PolygonDecoration buildDecoration() {
+		PolygonDecoration decoration = new PolygonDecoration();
+
+		PointList decorationPointList = new PointList();
+		decorationPointList.addPoint(0, 0);
+		decorationPointList.addPoint(-4, 4);
+		decorationPointList.addPoint(-8, 0);
+		decorationPointList.addPoint(-4, -4);
+
+		decoration.setTemplate(decorationPointList);
+		decoration.setFill(true);
+		decoration.setBackgroundColor(ColorConstants.black);
+		decoration.setScale(.8, .8);
+
+		return decoration;
+	}
+}
---
+++ branches/itsFormalism/fr.lip6.move.coloane.core.formalisms.its/src/UndirectedArc.java
@@ -0,0 +1,18 @@
+
+
+import fr.lip6.move.coloane.core.ui.figures.IArcFigure;
+import fr.lip6.move.coloane.core.ui.figures.RoundedPolylineConnection;
+
+import org.eclipse.draw2d.ColorConstants;
+import org.eclipse.draw2d.PolygonDecoration;
+import org.eclipse.draw2d.geometry.PointList;
+
+/**
+ * Test arc definition.<br>
+ * This arc has a grey losange at its end.
+ *
+ * @author yann based on jbv
+ */
+public class UndirectedArc extends RoundedPolylineConnection implements IArcFigure {
+
+}
