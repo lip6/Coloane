59eb6ffd947f6a18e1422c95f3a61cdc9317ad77
ythierry <ythierry@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2009-06-28 02:48:11 +0000

    slight cleanup

--- branches/itsFormalism/fr.lip6.move.coloane.core.formalisms.its/src/UndirectedArc.java
+++ branches/itsFormalism/fr.lip6.move.coloane.core.formalisms.its/src/UndirectedArc.java
@@ -3,13 +3,8 @@
 import fr.lip6.move.coloane.core.ui.figures.IArcFigure;
 import fr.lip6.move.coloane.core.ui.figures.RoundedPolylineConnection;
 
-import org.eclipse.draw2d.ColorConstants;
-import org.eclipse.draw2d.PolygonDecoration;
-import org.eclipse.draw2d.geometry.PointList;
-
 /**
- * Test arc definition.<br>
- * This arc has a grey losange at its end.
+ * A basic link, no decoration.
  *
  * @author yann based on jbv
  */
