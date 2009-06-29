99e7132f5c3a5bdba673372e081a6770d4485eba
ythierry <ythierry@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2009-06-29 22:32:41 +0000

    a more complex implem with support for undo (thx to clement)

--- branches/layoutAction/fr.lip6.move.coloane.graphviz/src/fr/lip6/move/coloane/graphviz/LayoutEditorActionDelegate.java
+++ branches/layoutAction/fr.lip6.move.coloane.graphviz/src/fr/lip6/move/coloane/graphviz/LayoutEditorActionDelegate.java
@@ -1,13 +1,17 @@
 package fr.lip6.move.coloane.graphviz;
 
+import fr.lip6.move.coloane.core.ui.ColoaneEditor;
+import fr.lip6.move.coloane.core.ui.commands.ModificationResultCommand;
+import fr.lip6.move.coloane.interfaces.model.IGraph;
+import fr.lip6.move.graphviz.coloane.GraphLayout;
+
+import org.eclipse.gef.GraphicalViewer;
+import org.eclipse.gef.commands.CommandStack;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.IEditorActionDelegate;
 import org.eclipse.ui.IEditorPart;
 
-import fr.lip6.move.coloane.core.ui.ColoaneEditor;
-import fr.lip6.move.coloane.interfaces.model.IGraph;
-import fr.lip6.move.graphviz.coloane.GraphLayout;
 
 /**
  * The action of laying out a graph.
@@ -16,6 +20,7 @@ import fr.lip6.move.graphviz.coloane.GraphLayout;
 public class LayoutEditorActionDelegate implements IEditorActionDelegate {
 
 	private IGraph graph = null;
+	private CommandStack stack;
 
 	/**
 	 * {@inheritDoc}
@@ -23,18 +28,24 @@ public class LayoutEditorActionDelegate implements IEditorActionDelegate {
 	@Override
 	public final void setActiveEditor(IAction action, IEditorPart targetEditor) {
 		graph = ((ColoaneEditor) targetEditor).getGraph();
+		ColoaneEditor ce = (ColoaneEditor) targetEditor;
+		GraphicalViewer viewer = (GraphicalViewer) ce.getAdapter(GraphicalViewer.class);
+		stack = viewer.getEditDomain().getCommandStack();
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
-	public final void run(IAction action) {
+	public final void run(IAction action)  {
 		if (graph != null) {
-			GraphLayout.layout(graph);
+			ModificationResultCommand  c = new ModificationResultCommand(graph, GraphLayout.layout(graph));
+			stack.execute(c);
 		}
 	}
 
+	
+	
 	/**
 	 * {@inheritDoc}
 	 */
