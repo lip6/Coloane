84d4ac020a572d92e46399f7b8fe2d6e212d3d3f
ythierry <ythierry@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2009-07-03 17:29:35 +0000

    fully working version (except for infinite lft)

--- branches/itsFormalism/fr.lip6.move.coloane.extensions.importExportRomeo/src/fr/lip6/move/coloane/extension/importExportRomeo/exportToRomeo/ExportToImpl.java
+++ branches/itsFormalism/fr.lip6.move.coloane.extensions.importExportRomeo/src/fr/lip6/move/coloane/extension/importExportRomeo/exportToRomeo/ExportToImpl.java
@@ -13,19 +13,16 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 import java.util.logging.Logger;
 
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.draw2d.AbsoluteBendpoint;
 import org.eclipse.draw2d.geometry.Point;
 
+
 /**
- * Export models to CAMI format
+ * Export models to Romeo format
  *
- * @author Jean-Baptiste Voron
+ * @author Yann Thierry-Mieg
  */
 public class ExportToImpl implements IExportTo {
 	
@@ -57,11 +54,31 @@ public class ExportToImpl implements IExportTo {
 			// header
 			sb.append("<TPN name=\"" + filePath + "\">\n");
 			indent++;
+			// nodes
 			for (INode node : model.getNodes()) {
 				if ("place".equals(node.getNodeFormalism().getName())) {
 					exportPlace(node, sb);
 				} else {
 					exportTransition(node, sb);
+				}
+			}
+			// arcs
+			for (IArc arc : model.getArcs()) {
+				String arcType = arc.getArcFormalism().getName(); 
+				if ("arc".equals(arcType)) {
+					if (arc.getSource().getNodeFormalism().getName().equals("place")) {
+						exportArc(arc, "PlaceTransition", sb);
+					} else {
+						exportArc(arc, "TransitionPlace", sb);						
+					}
+				} else if ("inhibitor".equals(arcType)){
+					exportArc(arc, "LogicalInhibitor", sb);											
+				} else if ("reset".equals(arcType)){
+					exportArc(arc, "flush", sb);
+				} else if ("test".equals(arcType)){
+					exportArc(arc, "read", sb);
+				} else {
+					throw new UnsupportedOperationException("unknown arc type!!");
 				}
 			}
 
@@ -84,14 +101,53 @@ public class ExportToImpl implements IExportTo {
 		monitor.done();
 	}
 
+	private void exportArc(IArc arc, String type, BufferedWriter sb) throws IOException {
+		INode place, trans;
+		if ("place".equals(arc.getSource().getNodeFormalism().getName())) {
+			place = arc.getSource();
+			trans = arc.getTarget();
+		} else {
+			place = arc.getTarget();
+			trans = arc.getSource();
+		}
+		indent(sb);
+		sb.append("<arc ");
+		sb.append("place=\"" + place.getId() + "\" ");
+		sb.append("transition=\"" + trans.getId() + "\" ");
+		sb.append("type=\""+type+"\" ");
+		IAttribute val = arc.getAttribute("valuation");
+		if (val != null) {
+			sb.append("weight=\""+ val.getValue()+"\" ");
+		} else {
+			sb.append("weight=\"1\" ");			
+		}
+		sb.append(">\n");
+		//inflex points
+		indent++;
+		indent(sb);
+		if (!arc.getInflexPoints().isEmpty()) {
+			// Romeo only supports a single nail per arc.
+			Point loc = arc.getGraphicInfo().findMiddlePoint();
+			sb.append("<nail xnail=\"" + loc.x + "\" ynail=\"" + loc.y + "\"/>\n");
+		} else {
+			// no inflex point
+			sb.append("<nail xnail=\"0\" ynail=\"0\"/>\n");			
+		}
+		indent--;
+		indent(sb);
+		// close arc
+		sb.append("</arc>\n");
+		
+	}
+
 	private void exportTransition(INode node, BufferedWriter sb) throws IOException {
 		indent(sb);
 		sb.append("<transition ");
 		sb.append("id=\"" + node.getId() + "\" ");
 		if ("".equals(node.getAttribute("label").getValue())) {
-			sb.append("label=\"" + node.getAttribute("name").getValue() + "\" public=\"0\"");
+			sb.append("label=\"" + node.getAttribute("name").getValue() + "\" public=\"0\" ");
 		} else {
-			sb.append("label=\"" + node.getAttribute("name").getValue() + "\" public=\"1\"");
+			sb.append("label=\"" + node.getAttribute("name").getValue() + "\" public=\"1\" ");
 		}
 		sb.append("eft=\"" + node.getAttribute("earliestFiringTime").getValue() + "\" ");
 		String lft = node.getAttribute("latestFiringTime").getValue();
@@ -147,7 +203,7 @@ public class ExportToImpl implements IExportTo {
 		// label position (delta)
 		Point loctag = node.getAttribute("name").getGraphicInfo().getLocation();
 		indent(sb);
-		sb.append("<deltaLabel deltax=\"" + (loctag.x - loc.x) + "\" deltay==\"" + (loctag.y - loc.y) + "\"/>\n");
+		sb.append("<deltaLabel deltax=\"" + (loctag.x - loc.x) + "\" deltay=\"" + (loctag.y - loc.y) + "\"/>\n");
 		indent--;
 
 		indent(sb);
