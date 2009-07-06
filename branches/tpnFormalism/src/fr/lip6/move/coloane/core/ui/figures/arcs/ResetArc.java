19d110d385d00ac7e49b4de390e3d2e494b18f7b
ythierry <ythierry@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2009-07-06 19:27:08 +0000

    add arc figures necessary for TPN. TODO : should move these classes into "core" probably, package naming already set up to do this...

---
+++ branches/tpnFormalism/src/fr/lip6/move/coloane/core/ui/figures/arcs/ResetArc.java
@@ -0,0 +1,20 @@
+package fr.lip6.move.coloane.core.ui.figures.arcs;
+
+
+import org.eclipse.draw2d.ColorConstants;
+
+/**
+ * Test arc definition.<br>
+ * This arc has a black losange at its end.
+ *
+ * @author yann based on jbv
+ */
+public class ResetArc extends DiamondArc {
+	/**
+	 * Constructor
+	 */
+	public ResetArc() {
+		super(ColorConstants.black);
+	}
+
+}
---
+++ branches/tpnFormalism/src/fr/lip6/move/coloane/core/ui/figures/arcs/TestArc.java
@@ -0,0 +1,19 @@
+package fr.lip6.move.coloane.core.ui.figures.arcs;
+
+import org.eclipse.draw2d.ColorConstants;
+
+/**
+ * Test arc definition.<br>
+ * This arc has a grey losange at its end.
+ *
+ * @author yann based on jbv
+ */
+public class TestArc extends DiamondArc {
+	/**
+	 * Constructor
+	 */
+	public TestArc() {
+		super(ColorConstants.gray);
+	}
+
+}
