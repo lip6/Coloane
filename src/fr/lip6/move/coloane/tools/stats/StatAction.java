d1b9636e722e3daaef40331c8d4352f1548a1163
Florian David <flodav@users.noreply.github.com>
2009-07-07 23:06:10 +0000

    Gestion des objectsOutline finie + commencement des textualsResults

--- src/fr/lip6/move/coloane/tools/stats/StatAction.java
+++ src/fr/lip6/move/coloane/tools/stats/StatAction.java
@@ -1,39 +1,48 @@
 package fr.lip6.move.coloane.tools.stats;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import java.util.List;
 
-import fr.lip6.move.coloane.apicami.objects.result.Result;
-import fr.lip6.move.coloane.apicami.objects.result.SubResult;
-import fr.lip6.move.coloane.apicami.objects.result.Tip;
+import fr.lip6.move.coloane.tools.stats.results.Result;
+import fr.lip6.move.coloane.tools.stats.results.SubResult;
 import fr.lip6.move.coloane.core.extensions.IColoaneAction;
+import fr.lip6.move.coloane.interfaces.model.IArc;
 import fr.lip6.move.coloane.interfaces.model.IGraph;
+import fr.lip6.move.coloane.interfaces.model.INode;
 import fr.lip6.move.coloane.interfaces.objects.result.IResult;
-import fr.lip6.move.coloane.interfaces.objects.result.ITip;
 
 public class StatAction implements IColoaneAction {
 
 	public List<IResult> run(IGraph model) {
-		//System.out.println("Nombre de noeuds : "+model.getNodes().size());
-		//System.out.println("Nombre d'arcs : "+model.getArcs().size());
-
-		Result res1 = new Result("", "Plop", null);
-		SubResult subres1 = new SubResult("Name subresult1", 1);
-		SubResult subres2 = new SubResult("Name subresult2", 2);
-		SubResult subres3 = new SubResult("Name subresult3", 3);
-		/*
-		ITip tip = new Tip(1, "Name tip", "Value tip");
-		res.addSubResult(subres1);
-		res.addSubResult(subres2);
-		res.addTip(tip);
-		*/
-
-		res1.addSubResult(subres1);
-		res1.addSubResult(subres2);
-		res1.addSubResult(subres3);
 		
-		//res.addSubResult(subres1);
+		Result res1 = new Result("Nom du menu racine", "Plop", null);
+		SubResult subres1 = new SubResult("Places et transitions");
+		SubResult subres2 = new SubResult("Arcs");
+		
+		Iterator<INode> itn = model.getNodes().iterator();
+		while(itn.hasNext()) {
+			INode node = itn.next();
+			subres1.addObjectOutline(node.getId());
+		}
+		
+		subres1.setTextualResultsMenu("1","2","3");
+		subres1.addTextualResult("plop","plip","plup");
+		
+		
+		
+		/*
+		Iterator<IArc> it = model.getArcs().iterator();
+		while(it.hasNext()) {
+			IArc arc = it.next();
+			subres2.addObjectOutline(arc.getId());
+		}
+		*/
+		
+		res1.addSubResult(subres1);
+		//res1.addSubResult(subres2);
+			
 		
 		ArrayList<IResult> al = new ArrayList<IResult>();
 		al.add(res1);
