008a860e75634c8f96e02dc9367b3bc20285b583
jbvoron <Jean-Baptiste.Voron@lip6.fr>
2009-07-08 14:40:38 +0000

    No more warnings and errors

--- src/fr/lip6/move/coloane/extensions/exporttostats/ExportToImpl.java
+++ src/fr/lip6/move/coloane/extensions/exporttostats/ExportToImpl.java
@@ -1,11 +1,20 @@
 package fr.lip6.move.coloane.extensions.exporttostats;
 
-import fr.lip6.move.coloane.core.interfaces.IExportTo;
+import fr.lip6.move.coloane.core.exceptions.ColoaneException;
+import fr.lip6.move.coloane.core.extensions.IExportTo;
+import fr.lip6.move.coloane.interfaces.model.IGraph;
+
+import org.eclipse.core.runtime.IProgressMonitor;
 
 public class ExportToImpl implements IExportTo {
 
 	public ExportToImpl() {
 		// TODO Auto-generated constructor stub
+	}
+
+	public void export(IGraph graph, String filePath, IProgressMonitor monitor) throws ColoaneException {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
