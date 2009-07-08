05de20f7aba92db1c5bf81b11039277432917128
jbvoron <Jean-Baptiste.Voron@lip6.fr>
2009-07-08 14:35:21 +0000

    refactor stats export plugin

---
+++ src/fr/lip6/move/coloane/extensions/exporttostats/ExportToImpl.java
@@ -0,0 +1,11 @@
+package fr.lip6.move.coloane.extensions.exporttostats;
+
+import fr.lip6.move.coloane.core.interfaces.IExportTo;
+
+public class ExportToImpl implements IExportTo {
+
+	public ExportToImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
