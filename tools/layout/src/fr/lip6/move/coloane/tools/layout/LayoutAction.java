378cbd78d3da4bb2faaa1fee393fb409346e17df
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2009-07-09 18:22:24 +0000

    implement adapter to match new interface for a model service.

--- tools/layout/src/fr/lip6/move/coloane/tools/layout/LayoutAction.java
+++ tools/layout/src/fr/lip6/move/coloane/tools/layout/LayoutAction.java
@@ -3,7 +3,9 @@ package fr.lip6.move.coloane.tools.layout;
 import fr.lip6.move.coloane.core.extensions.IColoaneAction;
 import fr.lip6.move.coloane.interfaces.model.IGraph;
 import fr.lip6.move.coloane.interfaces.model.command.ICommand;
+import fr.lip6.move.coloane.interfaces.objects.result.IResult;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,9 +15,12 @@ public class LayoutAction implements IColoaneAction {
 	/**
 	 * {@inheritDoc}
 	 */
-	public final List<ICommand> run(IGraph model) {
+	public final List<IResult> run(IGraph model) {
 		if (model != null) {
-			return GraphLayout.layout(model);
+			List<ICommand> commands = GraphLayout.layout(model);
+			List<IResult> result = new ArrayList<IResult>();
+			result.add(new ResultCommand(commands));
+			return result;
 		}
 		return null;
 	}
---
+++ tools/layout/src/fr/lip6/move/coloane/tools/layout/ResultCommand.java
@@ -0,0 +1,78 @@
+package fr.lip6.move.coloane.tools.layout;
+
+import fr.lip6.move.coloane.interfaces.model.IGraph;
+import fr.lip6.move.coloane.interfaces.model.command.ICommand;
+import fr.lip6.move.coloane.interfaces.objects.result.IResult;
+import fr.lip6.move.coloane.interfaces.objects.result.ISubResult;
+import fr.lip6.move.coloane.interfaces.objects.result.ITip;
+
+import java.util.List;
+
+/**
+ * An adapter to encapsulate a set of commands in an IResult.
+ * @author Yann TM
+ *
+ */
+public final class ResultCommand implements IResult {
+
+	
+	
+	private List<ICommand> commands;
+
+	/**
+	 * A constructor.
+	 * @param commands a list of commands to encapsulate
+	 */
+	public ResultCommand(List<ICommand> commands) {
+		this.commands = commands;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public List<ICommand> getModificationsOnCurrentGraph() {
+		return this.commands;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public IGraph getNewGraph() {
+		return null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String getRootName() {
+		return null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String getServiceName() {
+		return null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public List<ISubResult> getSubResults() {
+		return null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public List<ITip> getTipsList() {
+		return null;
+	}
+
+}
