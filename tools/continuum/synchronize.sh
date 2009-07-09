90418736c6406c6ff8e57e4d135d7d78e1f8a000
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2009-07-09 14:54:28 +0000

    Update synchronize tools to deal with local transfert
    This operation should be done again next week...

--- tools/continuum/synchronize.sh
+++ tools/continuum/synchronize.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 #
+# Synchornise le répertoire de build de continuum avec le répertoire public de apache
 # Auteur  : Jean-Baptiste Voron
 # Contact : jean-baptiste.voron@lip6.fr
 # Projet  : Coloane (http://coloane.lip6.fr)
@@ -13,19 +14,22 @@ dest=$2
 night=$source"/night-updates"
 targetnight=$dest"/night-updates"
 if [ -d $source ]; then
-	rsync -avz -r -C -S -e ssh $night/* coloane@coloane.lip6.fr:$targetnight
+#	rsync -avz -r -C -S -e ssh $night/* coloane@coloane.lip6.fr:$targetnight
+	rsync -avz -r -C -S $night/* $targetnight
 fi
 
 # For updates...
 update=$source"/updates"
 targetupdate=$dest"/updates"
 if [ -d $update ]; then
-	rsync -avz -r -C -S -e ssh $update/* coloane@coloane.lip6.fr:$targetupdate
+#	rsync -avz -r -C -S -e ssh $update/* coloane@coloane.lip6.fr:$targetupdate
+	rsync -avz -r -C -S $update/* $targetupdate
 fi
 
-# For night-updates...
+# For reports...
 reports=$source"/reports"
 targetreports=$dest"/reports"
 if [ -d $reports ]; then
-	rsync -avz -r -C -S -e ssh $reports/* coloane@coloane.lip6.fr:$targetreports
+#	rsync -avz -r -C -S -e ssh $reports/* coloane@coloane.lip6.fr:$targetreports
+	rsync -avz -r -C -S $reports/* $targetreports
 fi
