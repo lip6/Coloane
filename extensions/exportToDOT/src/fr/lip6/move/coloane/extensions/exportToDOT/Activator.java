02ced8bf70b98dc33feafda1ac7e60844a63c078
jbvoron <Jean-Baptiste.Voron@lip6.fr>
2009-08-26 21:34:22 +0000

    Adjust names

--- extensions/exportToDOT/src/fr/lip6/move/coloane/extensions/exportToDOT/Activator.java
+++ extensions/exportToDOT/src/fr/lip6/move/coloane/extensions/exportToDOT/Activator.java
@@ -1,4 +1,4 @@
-package fr.lip6.move.coloane.extension.exportToDOT;
+package fr.lip6.move.coloane.extensions.exportToDOT;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
--- extensions/exportToDOT/src/fr/lip6/move/coloane/extensions/exportToDOT/ExportToImpl.java
+++ extensions/exportToDOT/src/fr/lip6/move/coloane/extensions/exportToDOT/ExportToImpl.java
@@ -1,4 +1,4 @@
-package fr.lip6.move.coloane.extension.exportToDOT;
+package fr.lip6.move.coloane.extensions.exportToDOT;
 
 import fr.lip6.move.coloane.core.exceptions.ColoaneException;
 import fr.lip6.move.coloane.core.extensions.IExportTo;
