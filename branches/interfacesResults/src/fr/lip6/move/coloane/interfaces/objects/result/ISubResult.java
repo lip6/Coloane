cc1e27c02fd43ccd9d3b4b3974d6b94211fda2f1
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2009-08-27 18:01:26 +0000

    new try for interfacesResult

---
+++ branches/interfacesResults/src/fr/lip6/move/coloane/interfaces/objects/result/ISubResult.java
@@ -0,0 +1,48 @@
+package fr.lip6.move.coloane.interfaces.objects.result;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Définition d'un sous-résultat<br>
+ * Un sous-résultat peut contenir d'autres réstulats.
+ *
+ * @author Jean-Baptiste
+ */
+public interface ISubResult {
+
+	/**
+	 * @return Tous les sous-résultats
+	 */
+	List<ISubResult> getChildren();
+
+	/**
+	 * @return La liste des objets désignés par la plate-forme
+	 */
+	List<Integer> getObjectsDesignation();
+
+	/**
+	 * @return La liste des objets qui doivent être mis en valeur
+	 */
+	List<Integer> getObjectsOutline();
+
+	/**
+	 * @return La liste des attributs qui doivent être mis en valeur
+	 */
+	Map<Integer, List<String>> getAttributesOutline();
+
+	/**
+	 * @return La liste des résultats textuels
+	 */
+	List<String> getTextualResults();
+
+	/**
+	 * @return Le nom de l'ensemble de résultats
+	 */
+	String getName();
+
+	/**
+	 * @return Le type de l'ensemble de résultat
+	 */
+	int getType();
+}
