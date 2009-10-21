cbc10751abfeaf5f3b1bde0c6b95df56c5782b21
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2009-10-21 15:55:30 +0000

    Change rsync destination

--- tools/build-forge/synchronize.sh
+++ tools/build-forge/synchronize.sh
@@ -14,22 +14,22 @@ dest=$2
 night=$source"/night-updates"
 targetnight=$dest"/night-updates"
 if [ -d $source ]; then
-#	rsync -avz -r -C -S -e ssh $night/* coloane@coloane.lip6.fr:$targetnight
-	rsync -avz -r -C -S $night/* $targetnight
+	rsync -avz -r -C -S -e ssh $night/* coloane@coloane.lip6.fr:$targetnight
+#	rsync -avz -r -C -S $night/* $targetnight
 fi
 
 # For updates...
 update=$source"/updates"
 targetupdate=$dest"/updates"
 if [ -d $update ]; then
-#	rsync -avz -r -C -S -e ssh $update/* coloane@coloane.lip6.fr:$targetupdate
-	rsync -avz -r -C -S $update/* $targetupdate
+	rsync -avz -r -C -S -e ssh $update/* coloane@coloane.lip6.fr:$targetupdate
+#	rsync -avz -r -C -S $update/* $targetupdate
 fi
 
 # For reports...
 reports=$source"/reports"
 targetreports=$dest"/reports"
 if [ -d $reports ]; then
-#	rsync -avz -r -C -S -e ssh $reports/* coloane@coloane.lip6.fr:$targetreports
-	rsync -avz -r -C -S $reports/* $targetreports
+	rsync -avz -r -C -S -e ssh $reports/* coloane@coloane.lip6.fr:$targetreports
+#	rsync -avz -r -C -S $reports/* $targetreports
 fi
