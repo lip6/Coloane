815eeea93659044c16d628e93a67ce78ec00f4f8
ythierry <ythierry@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2009-11-09 16:09:14 +0000

    added a nice icon from the formalism

--- branches/itsFormalism/fr.lip6.move.coloane.its/src/its/typesui/TypeLabelProvider.java
+++ branches/itsFormalism/fr.lip6.move.coloane.its/src/its/typesui/TypeLabelProvider.java
@@ -1,7 +1,13 @@
 package its.typesui;
 
+import fr.lip6.move.coloane.core.main.Coloane;
+import fr.lip6.move.coloane.core.motor.formalisms.FormalismManager;
+import fr.lip6.move.coloane.interfaces.formalism.IFormalism;
+import its.CompositeTypeDeclaration;
+import its.Concept;
 import its.TypeDeclaration;
 
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.viewers.ILabelProviderListener;
 import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.jface.viewers.LabelProvider;
@@ -13,25 +19,41 @@ public class TypeLabelProvider extends LabelProvider implements
 	
 	
 	@Override
-	public Image getColumnImage(Object element, int columnIndex) {
-		// do stuff if you want nice icons in the table
+	public Image getColumnImage(Object obj, int index) {
+		if (index == 0) {
+			if (obj instanceof CompositeTypeDeclaration) {
+				IFormalism f = FormalismManager.getInstance().getFormalismById("ITS Composite");
+				return ImageDescriptor.createFromFile(Coloane.class, f.getImageName()).createImage();
+			}
+			if (obj instanceof TypeDeclaration) {
+				IFormalism f = FormalismManager.getInstance().getFormalismById("Time Petri Net");
+				return ImageDescriptor.createFromFile(Coloane.class, f.getImageName()).createImage();
+			}
+		}
 		return null;
 	}
 
 	@Override
 	public String getColumnText(Object element, int columnIndex) {
-		TypeDeclaration type = (TypeDeclaration) element;
-		switch (columnIndex) {
-		case 0 :
-			return type.getTypeName();
-		case 1 :
-			return type.getTypeType();
-		case 2 :
-			return type.getTypePath();
-		case 3 :
-			return Boolean.toString(type.isSatisfied());
-		default :
-			throw new RuntimeException("Column index out of bounds in TypeLabelProvider.");
+		if (element instanceof TypeDeclaration) {
+			TypeDeclaration type = (TypeDeclaration) element;
+			switch (columnIndex) {
+			case 0 :
+				return type.getTypeName();
+			case 1 :
+				return type.getTypeType();
+			case 2 :
+				return type.getTypePath();
+			case 3 :
+				return Boolean.toString(type.isSatisfied());
+			default :
+				throw new RuntimeException("Column index out of bounds in TypeLabelProvider.");
+			}
+		} else if (element instanceof Concept) {
+			Concept concept = (Concept) element;
+			return concept.getName();
+		} else {
+			return "Unrecognized type in TypeLabelProvider";
 		}
 	}
 
