1d86733933ce16c24c6bbefe755d9043c0631733
ythierry <ythierry@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2009-11-10 16:26:02 +0000

    added possibility to set a hint = specify which file to import (untested)

--- branches/itsFormalism/fr.lip6.move.coloane.its/src/its/actions/AddTypeAction.java
+++ branches/itsFormalism/fr.lip6.move.coloane.its/src/its/actions/AddTypeAction.java
@@ -10,22 +10,27 @@ import testits.editors.MultiPageEditor;
 public class AddTypeAction extends Action {
 	
 	private MultiPageEditor editor;
+	private AddTypeDialog atd;
 
 	public AddTypeAction(MultiPageEditor editor) {
-		this.editor = editor;
+		setEditor(editor);
 	}
 	
 	public void setEditor(MultiPageEditor editor) {
 		this.editor = editor;
+		atd = new AddTypeDialog(editor.getSite().getShell(),editor.getTypes());				
 	}
 	
 	@Override
 	public void run() {
 		TypeList types = editor.getTypes();
-		AddTypeDialog atd = new AddTypeDialog(editor.getSite().getShell(),types);
 		atd.open();
 		if (atd.getType() != null) {
 			types.addTypeDeclaration(atd.getType());
 		}
+	}
+
+	public void setHint(String hint) {
+		atd.setFileField(hint);
 	}
 }
