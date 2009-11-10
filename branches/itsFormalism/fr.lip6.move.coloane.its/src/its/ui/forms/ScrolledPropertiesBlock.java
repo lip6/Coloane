0f75f3970475b1e4df2afb45b4eeb86e1e4b4483
ythierry <ythierry@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2009-11-10 16:43:41 +0000

    * the great and incredible tree based ITS list editor, now revealed * support for double click to open editor * incomplete support for drag n drop

--- branches/itsFormalism/fr.lip6.move.coloane.its/src/its/ui/forms/ScrolledPropertiesBlock.java
+++ branches/itsFormalism/fr.lip6.move.coloane.its/src/its/ui/forms/ScrolledPropertiesBlock.java
@@ -9,34 +9,42 @@
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 package its.ui.forms;
-import fr.lip6.move.coloane.core.main.Coloane;
-import fr.lip6.move.coloane.core.motor.formalisms.FormalismManager;
-import fr.lip6.move.coloane.interfaces.formalism.IFormalism;
+
 import its.CompositeTypeDeclaration;
+import its.Concept;
 import its.TypeDeclaration;
-import its.TypeList;
-import its.typesui.TypeLabelProvider;
-import its.typesui.TypeListProvider;
+import its.actions.RemoveTypeAction;
 import its.typesui.TypeListTreeProvider;
 import its.typesui.TypeTreeLabelProvider;
 
 import org.eclipse.jface.action.Action;
-import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.util.LocalSelectionTransfer;
+import org.eclipse.jface.viewers.DoubleClickEvent;
+import org.eclipse.jface.viewers.IDoubleClickListener;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
-import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.dnd.DND;
+import org.eclipse.swt.dnd.FileTransfer;
+import org.eclipse.swt.dnd.Transfer;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.layout.*;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.Tree;
-import org.eclipse.ui.forms.*;
-import org.eclipse.ui.forms.widgets.*;
+import org.eclipse.ui.forms.DetailsPart;
+import org.eclipse.ui.forms.IManagedForm;
+import org.eclipse.ui.forms.MasterDetailsBlock;
+import org.eclipse.ui.forms.SectionPart;
+import org.eclipse.ui.forms.widgets.ExpandableComposite;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.ui.forms.widgets.ScrolledForm;
+import org.eclipse.ui.forms.widgets.Section;
 /**
  *
  */
@@ -47,14 +55,15 @@ public class ScrolledPropertiesBlock extends MasterDetailsBlock {
 		this.page = page;
 	}
 
+	@Override
 	protected void createMasterPart(final IManagedForm managedForm,
 			Composite parent) {
 		//final ScrolledForm form = managedForm.getForm();
 		FormToolkit toolkit = managedForm.getToolkit();
-		Section section = toolkit.createSection(parent, Section.DESCRIPTION|Section.TITLE_BAR);
+		Section section = toolkit.createSection(parent, Section.DESCRIPTION|ExpandableComposite.TITLE_BAR);
 		section.setText("Type Declarations"); //$NON-NLS-1$
 		section
-				.setDescription("Models imported into the ITS referential."); //$NON-NLS-1$
+		.setDescription("Models imported into the ITS referential."); //$NON-NLS-1$
 		section.marginWidth = 10;
 		section.marginHeight = 5;
 		Composite client = toolkit.createComposite(section, SWT.WRAP);
@@ -65,12 +74,12 @@ public class ScrolledPropertiesBlock extends MasterDetailsBlock {
 		client.setLayout(layout);
 		GridData gd = new GridData(GridData.FILL_BOTH);
 		gd.heightHint = 20;
-		gd.widthHint = 100;
+		gd.widthHint = 30;
 
-//		Table t = toolkit.createTable(client, SWT.NULL);
+		//		Table t = toolkit.createTable(client, SWT.NULL);
 		Tree tree = toolkit.createTree(client, SWT.NULL);
-		//t.setLayoutData(gd);
-		
+		tree.setLayoutData(gd);
+
 		toolkit.paintBordersFor(client);
 		Button b = toolkit.createButton(client, "Add a type", SWT.PUSH); //$NON-NLS-1$
 		gd = new GridData(GridData.VERTICAL_ALIGN_BEGINNING);
@@ -82,6 +91,21 @@ public class ScrolledPropertiesBlock extends MasterDetailsBlock {
 			}
 		});				
 
+		toolkit.paintBordersFor(client);
+		Button b2 = toolkit.createButton(client, "Remove a type", SWT.PUSH); //$NON-NLS-1$
+		gd = new GridData(GridData.VERTICAL_ALIGN_BEGINNING);
+		b2.setLayoutData(gd);
+		b2.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent event) {
+				try {
+					TypeDeclaration td = (TypeDeclaration) ((TreeSelection)viewer.getSelection()).getFirstElement();
+					new RemoveTypeAction(page.getMpe().getTypes(), td).run();
+				} catch (Exception e) {
+					System.err.println("Select a type");
+				}
+			}
+		});				
 
 		viewer = new TreeViewer(tree);
 		viewer.setContentProvider(new TypeListTreeProvider());
@@ -97,10 +121,32 @@ public class ScrolledPropertiesBlock extends MasterDetailsBlock {
 				managedForm.fireSelectionChanged(spart, event.getSelection());
 			}
 		});
+
+		viewer.addDoubleClickListener(new IDoubleClickListener() {
+
+			@Override
+			public void doubleClick(DoubleClickEvent event) {
+				try {
+					TreeSelection o = (TreeSelection) event.getSelection();
+					TypeDeclaration td = (TypeDeclaration)o.getFirstElement();
+					page.getMpe().openEditor(td );
+				} catch (Exception e) {
+					// a concept was double clicked
+				}
+			}
+		});
+		
+		int ops = DND.DROP_COPY | DND.DROP_DEFAULT;
+		Transfer[] transfers = new Transfer[] { LocalSelectionTransfer.getTransfer(),FileTransfer.getInstance() };
+		TypesViewDropAdapter adapter = new TypesViewDropAdapter(this);
+		viewer.addDropSupport(ops, transfers, adapter);
+
 	}
+	@Override
 	protected void createToolBarActions(IManagedForm managedForm) {
 		final ScrolledForm form = managedForm.getForm();
-		Action haction = new Action("hor", Action.AS_RADIO_BUTTON) { //$NON-NLS-1$
+		Action haction = new Action("hor", IAction.AS_RADIO_BUTTON) { //$NON-NLS-1$
+			@Override
 			public void run() {
 				sashForm.setOrientation(SWT.HORIZONTAL);
 				form.reflow(true);
@@ -111,7 +157,8 @@ public class ScrolledPropertiesBlock extends MasterDetailsBlock {
 		haction.setImageDescriptor(ITSEditorPlugin.getDefault()
 				.getImageRegistry()
 				.getDescriptor(ITSEditorPlugin.IMG_HORIZONTAL));
-		Action vaction = new Action("ver", Action.AS_RADIO_BUTTON) { //$NON-NLS-1$
+		Action vaction = new Action("ver", IAction.AS_RADIO_BUTTON) { //$NON-NLS-1$
+			@Override
 			public void run() {
 				sashForm.setOrientation(SWT.VERTICAL);
 				form.reflow(true);
@@ -124,12 +171,16 @@ public class ScrolledPropertiesBlock extends MasterDetailsBlock {
 		form.getToolBarManager().add(haction);
 		form.getToolBarManager().add(vaction);
 	}
+	@Override
 	protected void registerPages(DetailsPart detailsPart) {
-		detailsPart.registerPage(TreeNode.class, new TreeNodeDetailsPage());
-		detailsPart.registerPage(CompositeTypeDeclaration.class, new CompositeDetailsPage());
-		detailsPart.registerPage(TypeDeclaration.class, new TypeDeclarationDetailsPage());
+		detailsPart.registerPage(CompositeTypeDeclaration.class, new TypeDeclarationDetailsPage(this));
+		detailsPart.registerPage(TypeDeclaration.class, new TypeDeclarationDetailsPage(this));
+		detailsPart.registerPage(Concept.class, new ConceptDetailsPage(page.getMpe().getTypes()));
 	}
 	public void refresh() {
 		viewer.refresh();
+	}
+	public MasterDetailsPage getPage() {
+		return page;
 	}
 }
