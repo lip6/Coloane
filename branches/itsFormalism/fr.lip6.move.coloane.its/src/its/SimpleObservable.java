f4479ab110353b1d694609532442f6e709736f1b
ythierry <ythierry@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2009-11-10 16:21:28 +0000

    * let SimpleObservable implement ISimpleObs * let notify be public (was protected)

--- branches/itsFormalism/fr.lip6.move.coloane.its/src/its/SimpleObservable.java
+++ branches/itsFormalism/fr.lip6.move.coloane.its/src/its/SimpleObservable.java
@@ -2,18 +2,24 @@ package its;
 
 import java.util.ArrayList;
 
-public class SimpleObservable {
+public class SimpleObservable implements ISimpleObservable {
 	private ArrayList<ISimpleObserver> obs = new ArrayList<ISimpleObserver>(); 
 
+	/* (non-Javadoc)
+	 * @see its.ISimpleObservable#addObserver(its.ISimpleObserver)
+	 */
 	public void addObserver (ISimpleObserver o) {
 		obs.add(o);
 	}
 	
+	/* (non-Javadoc)
+	 * @see its.ISimpleObservable#deleteObserver(its.ISimpleObserver)
+	 */
 	public void deleteObserver(ISimpleObserver o) {
 		obs.remove(o);
 	}
 	
-	protected void notifyObservers() {
+	public void notifyObservers() {
 		for (ISimpleObserver o : obs) {
 			o.update();
 		}
