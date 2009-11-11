0bc174c81de56192f4daf34cd5bdbb093825369e
ythierry <ythierry@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2009-11-11 15:59:39 +0000

    A version that allows to drop one or more files from the browser into the model

--- branches/itsFormalism/fr.lip6.move.coloane.its/src/its/ui/forms/TypesViewDropAdapter.java
+++ branches/itsFormalism/fr.lip6.move.coloane.its/src/its/ui/forms/TypesViewDropAdapter.java
@@ -1,63 +1,52 @@
 package its.ui.forms;
 
 
-import org.eclipse.swt.dnd.DropTargetEvent;
-import org.eclipse.swt.dnd.DropTargetListener;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.jface.util.LocalSelectionTransfer;
+import org.eclipse.jface.viewers.TreeSelection;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerDropAdapter;
+import org.eclipse.swt.dnd.FileTransfer;
+import org.eclipse.swt.dnd.TransferData;
+import org.eclipse.ui.part.ResourceTransfer;
 
-public class TypesViewDropAdapter implements DropTargetListener {
+public class TypesViewDropAdapter extends ViewerDropAdapter {
 
-	
-	
+
+
 	private ScrolledPropertiesBlock master;
 
-
-	public TypesViewDropAdapter(ScrolledPropertiesBlock master) {
+	public TypesViewDropAdapter(Viewer viewer, ScrolledPropertiesBlock master) {
+		super(viewer);
 		this.master = master;
 	}
+	public boolean validateDrop(Object target, int op, TransferData type) {
+		boolean b= FileTransfer.getInstance().isSupportedType(type)
+		|| LocalSelectionTransfer.getTransfer().isSupportedType(type)
+		|| ResourceTransfer.getInstance().isSupportedType(type);
+		return b;
+	}
 
-
-	public void drop(DropTargetEvent event) {
-		Object data = event.data;
+	@Override
+	public boolean performDrop(Object data) {
 		if (data instanceof String[]) {
 			final String[] strings = (String[]) data;
 			for (int i = 0; i < strings.length; i++) {
 				master.getPage().getMpe().getAddAction().setHint(strings[i]);
 				master.getPage().getMpe().getAddAction().run();
 			}
+			return true;
+		} else if (data instanceof TreeSelection) {
+			TreeSelection tsel = (TreeSelection) data;
+			for (Object element : tsel.toArray())
+				if (element instanceof IFile) {
+					IFile file = (IFile) element;
+					master.getPage().getMpe().getAddAction().setHint(file.getLocation().toString());
+					master.getPage().getMpe().getAddAction().run();				
+				}
 		}
+		return false;
 	}
 
-
-	@Override
-	public void dragEnter(DropTargetEvent event) {
-		
-	}
-
-
-	@Override
-	public void dragLeave(DropTargetEvent event) {
-		
-	}
-
-
-	@Override
-	public void dragOperationChanged(DropTargetEvent event) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-	@Override
-	public void dragOver(DropTargetEvent event) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-	@Override
-	public void dropAccept(DropTargetEvent event) {
-		// TODO Auto-generated method stub
-		
-	}
 
 }
