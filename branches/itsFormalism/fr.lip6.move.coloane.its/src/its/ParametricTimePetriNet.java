63082fd13cf96a5b99747c309d0952ed0fd06179
ythierry <ythierry@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2009-11-13 17:02:13 +0000

    REFACTORING: extract code into EvaluationContext and TypeDeclaration. This class is now obsolete.

--- branches/itsFormalism/fr.lip6.move.coloane.its/src/its/ParametricTimePetriNet.java
+++ branches/itsFormalism/fr.lip6.move.coloane.its/src/its/ParametricTimePetriNet.java
@@ -1,13 +1,14 @@
 package its;
 
+import its.expression.Constant;
 import its.expression.EvaluationContext;
+import its.expression.IEvaluationContext;
 import its.expression.IVariable;
 import its.expression.IntegerExpression;
 import its.expression.parser.IntegerExpressionParserLexer;
 import its.expression.parser.IntegerExpressionParserParser;
+import its.obs.SimpleObservable;
 
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -16,61 +17,65 @@ import org.antlr.runtime.CommonTokenStream;
 import org.antlr.runtime.RecognitionException;
 
 import fr.lip6.move.coloane.core.exceptions.ColoaneException;
+import fr.lip6.move.coloane.interfaces.model.IArc;
 import fr.lip6.move.coloane.interfaces.model.IAttribute;
 import fr.lip6.move.coloane.interfaces.model.IGraph;
 import fr.lip6.move.coloane.interfaces.model.INode;
 
-public class ParametricTimePetriNet extends SimpleObservable implements EvaluationContext {
+public class ParametricTimePetriNet extends SimpleObservable {
 
 	private IGraph  graph;
-	private Set<IVariable> variables=null;
 	private Map<IAttribute,IntegerExpression> attribs;
-	private Map<IVariable,Integer> values;
-
+	private IEvaluationContext context;
+	
 	public ParametricTimePetriNet(IGraph graph) {
 		this.graph = graph;
 	}
 	
-	public Set<IVariable> getVariables () throws ColoaneException {
-		if (variables == null) {
-			variables = new HashSet<IVariable>();
-			values = new HashMap<IVariable, Integer>();
+	public IEvaluationContext computeParameters () throws ColoaneException {
+		if (context == null) {
+			context = new EvaluationContext();
 			for (INode node : graph.getNodes()) {
 				if ("place".equals(node.getNodeFormalism().getName())) {
 					IAttribute attrib = node.getAttribute("marking");
-					String mark = attrib.getValue();
-					if (mark != null && ! "".equals(mark)) {
-
-						IntegerExpressionParserLexer lexer;
-						lexer = new IntegerExpressionParserLexer (new ANTLRStringStream(mark));
-
-						CommonTokenStream tokens = new CommonTokenStream(lexer);
-
-						IntegerExpressionParserParser parser = new IntegerExpressionParserParser(tokens);
-						IntegerExpression expr;
-						try {
-							expr = parser.prog();
-						} catch (RecognitionException e) {
-							throw new ColoaneException("Error parsing Marking "+e.getMessage());
-						}
-						attribs.put(attrib,expr);
-						variables.addAll(expr.supportingVariables());
-					}
+					parseIntExpression(attrib);
 				}
 			}
+			for (IArc arc : graph.getArcs()) {
+				// supports null attribute passing: some arcs have no valuation
+				parseIntExpression(arc.getAttribute("valuation"));
+			}
 		}		
-		return variables;
+		return context;
 	}
 
-	@Override
-	public Integer getVariableValue(IVariable var) {
-		return values.get(var);
+	private void parseIntExpression(IAttribute attrib) throws ColoaneException {
+		if (attrib == null)
+			return;
+		String mark = attrib.getValue();
+		if (mark != null && ! "".equals(mark)) {
+
+			IntegerExpressionParserLexer lexer;
+			lexer = new IntegerExpressionParserLexer (new ANTLRStringStream(mark));
+
+			CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+			IntegerExpressionParserParser parser = new IntegerExpressionParserParser(tokens);
+			IntegerExpression expr;
+			try {
+				expr = parser.prog();
+			} catch (RecognitionException e) {
+				throw new ColoaneException("Error parsing Marking "+e.getMessage());
+			}
+			if (! (expr instanceof Constant)) {
+				// dont store the mapping for trivial integers
+				attribs.put(attrib,expr);
+				// could be empty for simple expressions, eg 3+2
+				for (IVariable var : expr.supportingVariables())
+					context.declareVariable(var);
+			}
+		}
 	}
 
-	@Override
-	public void setVariableValue(IVariable var, int value) {
-		values.put(var,value);
-		notifyObservers();
-	}
 	
 }
