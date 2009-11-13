439f7f2878e31f9935a8ba88b490f019ccfa481a
ythierry <ythierry@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2009-11-13 16:42:43 +0000

    added variable bindings to input objects

--- branches/itsFormalism/fr.lip6.move.coloane.its/src/its/typesui/TypeTreeLabelProvider.java
+++ branches/itsFormalism/fr.lip6.move.coloane.its/src/its/typesui/TypeTreeLabelProvider.java
@@ -6,11 +6,13 @@ import fr.lip6.move.coloane.interfaces.formalism.IFormalism;
 import its.CompositeTypeDeclaration;
 import its.Concept;
 import its.TypeDeclaration;
+import its.expression.IVariableBinding;
 
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.viewers.ILabelProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
 
 public class TypeTreeLabelProvider extends LabelProvider implements
 ILabelProvider {
@@ -31,6 +33,13 @@ ILabelProvider {
 				return concept.getName()+": "+concept.getEffective().getTypeName();
 			else
 				return "!!!  " + concept.getName()+": ? ";
+		} else if (element instanceof IVariableBinding) {
+			IVariableBinding vb = (IVariableBinding) element;
+			Integer val = vb.getVariableValue();
+			if (val != null) 
+				return vb.getVariableName()+":= "+val;
+			else
+				return "!!!  " + vb.getVariableName()+":= ?";				
 		}
 		return "Unrecognized type in TypeLabelProvider";
 	}
@@ -48,6 +57,10 @@ ILabelProvider {
 			IFormalism f = FormalismManager.getInstance().getFormalismById("ITS Composite");
 			String img = f.getMasterGraph().getElementFormalism("instance").getGraphicalDescription().getIcon24px();
 			return ImageDescriptor.createFromFile(Coloane.class, img).createImage();
+		} else if (element instanceof IVariableBinding) {
+			IVariableBinding vb = (IVariableBinding) element;
+			// TODO : discriminate icon depending on variable set or not
+			return AbstractUIPlugin.imageDescriptorFromPlugin("org.eclipse.ui", "$nl$/icons/full/elcl16/progress_rem.gif").createImage(); //$NON-NLS-1$ //$NON-NLS-2$
 		}
 		return null;
 	}
