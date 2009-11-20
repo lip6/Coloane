e6b591aa8028434c70897194a641301c64818b0e
ythierry <ythierry@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2009-11-20 10:49:31 +0000

    a nicer report, with one result per element (node) and one subresult per incorrect attribute

--- branches/itsFormalism/fr.lip6.move.coloane.its/src/fr/lip6/move/coloane/its/syntax/tool/SyntaxCheckReport.java
+++ branches/itsFormalism/fr.lip6.move.coloane.its/src/fr/lip6/move/coloane/its/syntax/tool/SyntaxCheckReport.java
@@ -3,13 +3,15 @@ package fr.lip6.move.coloane.its.syntax.tool;
 import fr.lip6.move.coloane.core.motor.session.SessionManager;
 import fr.lip6.move.coloane.core.results.ResultTreeImpl;
 import fr.lip6.move.coloane.core.results.reports.IReport;
-import fr.lip6.move.coloane.core.results.reports.Messages;
 import fr.lip6.move.coloane.interfaces.model.IArc;
 import fr.lip6.move.coloane.interfaces.model.IElement;
 import fr.lip6.move.coloane.interfaces.model.INode;
 import fr.lip6.move.coloane.interfaces.objects.result.IResult;
 import fr.lip6.move.coloane.interfaces.objects.result.ISubResult;
 import fr.lip6.move.coloane.its.syntax.Result;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class SyntaxCheckReport implements IReport {
@@ -21,42 +23,46 @@ public class SyntaxCheckReport implements IReport {
 		// 2. Attach the session Manager to the root
 		root.setSessionManager(SessionManager.getInstance());			
 
+		Map<Integer,ResultTreeImpl> resPerObject = new HashMap<Integer, ResultTreeImpl>();
 		if (res instanceof Result) {
-			Result result = (Result) res;
 			if (res.getSubResults().isEmpty()) {
 				return root;
 			}
-			ISubResult sub = res.getSubResults().get(0);
-			// Create a node result
-			int id = sub.getObjectsOutline().get(0);
-			IElement element = root.getSessionManager().getCurrentSession().getGraph().getObject(id);
-			ResultTreeImpl node;
-			if ((element != null) && (element instanceof INode)) {
-				INode inode = (INode) element;
-				String name;
-				if (element.getAttribute("name") != null) {
-					name = element.getAttribute("name").getValue();
-				} else if (element.getAttribute("label") != null) {
-					name = element.getAttribute("label").getValue();
+			for (ISubResult sub : res.getSubResults()) {
+				// Create a node result
+				int id = sub.getObjectsOutline().get(0);
+				ResultTreeImpl node;
+				if (resPerObject.containsKey(id)) {
+					node = resPerObject.get(id);
 				} else {
-					name = "";
+					IElement element = root.getSessionManager().getCurrentSession().getGraph().getObject(id);
+					if ((element != null) && (element instanceof INode)) {
+						INode inode = (INode) element;
+						String name;
+						if (element.getAttribute("name") != null) {
+							name = element.getAttribute("name").getValue();
+						} else if (element.getAttribute("label") != null) {
+							name = element.getAttribute("label").getValue();
+						} else {
+							name = "";
+						}
+					node = new ResultTreeImpl(sub.getObjectsOutline(), inode.getNodeFormalism().getName(), name);
+					} else if ((element != null) && (element instanceof IArc)) {
+						IArc ia = (IArc) element;
+						node = new ResultTreeImpl(sub.getObjectsOutline(),
+								ia.getArcFormalism().getName(),
+								ia.getSource().getAttribute("name").getValue(),
+								"->",
+								ia.getTarget().getAttribute("name").getValue());				
+					} else {
+						node = new ResultTreeImpl(sub.getObjectsOutline());
+					}
+					resPerObject.put(id,node);
+					root.addChild(node);
 				}
-				node = new ResultTreeImpl(sub.getObjectsOutline(), inode.getNodeFormalism().getName(), name);
-			} else if ((element != null) && (element instanceof IArc)) {
-				IArc ia = (IArc) element;
-				node = new ResultTreeImpl(sub.getObjectsOutline(),
-						ia.getArcFormalism().getName(),
-						ia.getSource().getAttribute("name").getValue(),
-						"->",
-						ia.getTarget().getAttribute("name").getValue());				
-			} else {
-				node = new ResultTreeImpl(sub.getObjectsOutline());
-			}
-			for (ISubResult subres : result.getSubResults()) {
-				String attribute = subres.getAttributesOutline().entrySet().iterator().next().getValue().get(0);
-				node.addChild(new ResultTreeImpl(sub.getObjectsOutline(),attribute,subres.getName(),subres.getTextualResults().get(0)));
+				String attribute = sub.getAttributesOutline().entrySet().iterator().next().getValue().get(0);
+				node.addChild(new ResultTreeImpl(sub.getObjectsOutline(),attribute,sub.getName(),sub.getTextualResults().get(0)));
 			}		
-			root.addChild(node);
 		}
 		return root;
 	}
