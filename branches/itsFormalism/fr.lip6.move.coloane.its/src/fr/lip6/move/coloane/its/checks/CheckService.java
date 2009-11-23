105f4e30727ce3f1ff819bcf21d0f69f24e55b8b
ythierry <ythierry@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2009-11-23 17:54:26 +0000

    more body: add a run service and a folder and observable

--- branches/itsFormalism/fr.lip6.move.coloane.its/src/fr/lip6/move/coloane/its/checks/CheckService.java
+++ branches/itsFormalism/fr.lip6.move.coloane.its/src/fr/lip6/move/coloane/its/checks/CheckService.java
@@ -1,14 +1,59 @@
 package fr.lip6.move.coloane.its.checks;
 
+import fr.lip6.move.coloane.its.io.ITSModelWriter;
 import fr.lip6.move.coloane.its.obs.SimpleObservable;
 
-public class CheckService extends SimpleObservable {
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CheckService extends SimpleObservable implements Iterable<ServiceResult> {
 
 	private String name="ITS reachability";
-
+	private String workdir;
+	private CheckList parent;
+	private List<ServiceResult> results = new LinkedList<ServiceResult>();
+	
+	public CheckService(CheckList parent) {
+		this.parent = parent;
+	}
+	
 	public String getName() {
 		return name;
 	}
 
+	public void setWorkdir(String workdir) {
+		this.workdir = workdir;
+		notifyObservers();
+	}
+	
+	public String getWorkDir() {
+		return workdir;
+	}
+
+	public String run() {
+		ITSModelWriter mw = new ITSModelWriter();
+		String report;
+		boolean success = true;
+		try {
+			mw.exportITSModel(parent.getTypes(), parent.getType(), workdir);
+			report = "Run successful in folder "+workdir;
+		} catch (Exception e) {
+			success = false;
+			report = "An error occurred during service invocation :" + e + e.getMessage();
+		}
+		addResult (new ServiceResult(success,report,this));
+		return report;
+	}
+
+	private void addResult(ServiceResult serviceResult) {
+		results.add(serviceResult);
+		notifyObservers();
+	}
+
+	@Override
+	public Iterator<ServiceResult> iterator() {
+		return results.iterator();
+	}
 	
 }
