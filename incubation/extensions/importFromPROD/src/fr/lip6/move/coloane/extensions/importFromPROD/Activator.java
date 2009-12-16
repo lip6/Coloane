2d8e2cb5a9a12dd8e48722acbc5d2d895fdf3927
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2009-12-16 15:58:48 +0000

    extensionS package

--- incubation/extensions/importFromPROD/src/fr/lip6/move/coloane/extensions/importFromPROD/Activator.java
+++ incubation/extensions/importFromPROD/src/fr/lip6/move/coloane/extensions/importFromPROD/Activator.java
@@ -1,4 +1,4 @@
-package fr.lip6.move.coloane.extension.importFromPROD;
+package fr.lip6.move.coloane.extensions.importFromPROD;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
