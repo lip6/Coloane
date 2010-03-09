e30c39dd7ae83376b9f89fb3a96792e02981b86b
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2010-03-09 15:01:32 +0000

    You can specify in a formalism description if an attribute should be displayed even if its value matches the default one. See drawable_default properties in attribute description.

--- trunk/application/interfaces/src/fr/lip6/move/coloane/interfaces/model/IAttributeGraphicInfo.java
+++ trunk/application/interfaces/src/fr/lip6/move/coloane/interfaces/model/IAttributeGraphicInfo.java
@@ -4,38 +4,40 @@ import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.swt.graphics.Color;
 
 /**
- * Définition des propriétés et comportements graphique d'un objet attribut
+ * Graphical properties and behaviors of an attribute
  * @see IAttribute
+ * 
+ * @author Jean-Baptiste Voron
  */
 public interface IAttributeGraphicInfo extends ILocationInfo {
 
 	/**
-	 * @return couleur de fond
+	 * @return background color
 	 */
 	Color getBackground();
 
 	/**
-	 * @param background La couleur d'arrière plan
+	 * @param background color
 	 */
 	void setBackground(Color background);
 
 	/**
-	 * @return couleur du noeud
+	 * @return foreground color
 	 */
 	Color getForeground();
 
 	/**
-	 * @param foreground La couleur d'avant plan
+	 * @param foreground color
 	 */
 	void setForeground(Color foreground);
 
 	/**
-	 * @return <code>true</code> if the attribut is displayed
+	 * @return <code>true</code> if the attribute has to be displayed
 	 */
 	boolean isVisible();
 
 	/**
-	 * @return size of the attribut
+	 * @return size of the attribute
 	 */
 	Dimension getSize();
 }
