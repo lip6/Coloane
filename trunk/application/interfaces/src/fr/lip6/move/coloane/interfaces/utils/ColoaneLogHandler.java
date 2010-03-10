a20b12d42ff6d899696ac3992140b3039172d817
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2010-03-10 00:18:54 +0000

    log handler are now clean and documented in english

--- trunk/application/interfaces/src/fr/lip6/move/coloane/interfaces/utils/ColoaneLogHandler.java
+++ trunk/application/interfaces/src/fr/lip6/move/coloane/interfaces/utils/ColoaneLogHandler.java
@@ -4,26 +4,21 @@ import java.io.IOException;
 import java.util.logging.FileHandler;
 
 /**
- * Classe en charge de la gestion des messages de log<br>
- * Ici, elle permet l'affichage de tous les message sur la console.
+ * Log handler dedicated to file export<br>
+ * All messages are dumped in a file (in the default temporary folder)
  */
 public final class ColoaneLogHandler extends FileHandler {
-
 	private static ColoaneLogHandler instance = null;
 
 	/**
-	 * Constructeur
-	 * @throws IOException Problème d'écriture sur le fichier de log
-	 * @throws SecurityException Problème de droits sur le fichier de logs
+	 * Build the handle (inside the <b>default temporary folder</b>)
 	 */
 	private ColoaneLogHandler() throws IOException, SecurityException {
 		super("%t/coloane.log", true); //$NON-NLS-1$
 	}
 
 	/**
-	 * @return l'instance du handler
-	 * @throws IOException Problème d'écriture sur le fichier de log
-	 * @throws SecurityException Problème de droits sur le fichier de logs
+	 * @return the handler
 	 */
 	public static ColoaneLogHandler getInstance() throws IOException, SecurityException {
 		if (instance == null) { instance = new ColoaneLogHandler(); }
