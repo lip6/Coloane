f4487a4fce3fbc6303f124d74140db7e19e80805
ythierry <ythierry@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2010-03-30 17:22:27 +0000

    support for running on a composite model added

--- incubation/projects/itsModeler/src/fr/lip6/move/coloane/projects/its/checks/OrderingService.java
+++ incubation/projects/itsModeler/src/fr/lip6/move/coloane/projects/its/checks/OrderingService.java
@@ -7,8 +7,13 @@ import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
 
+import fr.lip6.move.coloane.extension.importExportRomeo.exportToRomeo.ExportToRomeo;
 import fr.lip6.move.coloane.extensions.importExportCAMI.exportToCAMI.ExportToImpl;
+import fr.lip6.move.coloane.interfaces.exceptions.ModelException;
+import fr.lip6.move.coloane.interfaces.model.IGraph;
+import fr.lip6.move.coloane.projects.its.CompositeTypeDeclaration;
 import fr.lip6.move.coloane.projects.its.TypeDeclaration;
+import fr.lip6.move.coloane.projects.its.flatten.ModelFlattener;
 import fr.lip6.move.coloane.projects.its.ui.forms.ITSEditorPlugin;
 
 public class OrderingService extends CheckService {
@@ -17,6 +22,7 @@ public class OrderingService extends CheckService {
 	private static final String ORDER_HEURISTIC_PARAM = "Heuristic chosen";
 	private static final String ORDER_FILE_NAME = "model.json";
 	private static final String CAMI_FILE_NAME = "model.cami";
+	private static final String ROMEO_FILE_NAME = "model.xml";
 	
 	public OrderingService(CheckList parent) {
 		super(parent, ORDER_NAME);
@@ -46,20 +52,45 @@ public class OrderingService extends CheckService {
 	public String run() {
 		boolean success = true;
 		String report = "";
-		
+		IGraph graph;
 		// Step 1 : flatten the model if necessary
 		TypeDeclaration td = getParent().getType();
-		//just suppose its ok for now
-		
+		if (td instanceof CompositeTypeDeclaration) {
+			ModelFlattener mf = new ModelFlattener();
+			mf.setInstantiateOption(true);
+			try {
+				mf.doFlatten((CompositeTypeDeclaration) td);
+			} catch (ModelException e) {
+				success  = false;
+				report = "An error occurred during flatten model phase :" + e + e.getMessage();
+				addResult (new ServiceResult(success,report,this));
+				return report;
+			}
+			graph = mf.getFlatModel();
+		} else {
+			graph = td.getInstantiatedGraph();
+		}
+			
 		// Step 2 : export to CAMI to build input for Silien's ordering tool
 		try {
-			new ExportToImpl().export(td.getInstantiatedGraph(),getWorkDir()+ "/" + CAMI_FILE_NAME, new NullProgressMonitor());
+			new ExportToImpl().export(graph,getWorkDir()+ "/" + CAMI_FILE_NAME, new NullProgressMonitor());
 		} catch (Exception e) {
 			success  = false;
 			report = "An error occurred during export to CAMI phase :" + e + e.getMessage();
 			addResult (new ServiceResult(success,report,this));
 			return report;
 		}
+		try {
+			// export the TPN to Romeo xml format.
+			new ExportToRomeo().export(graph,getWorkDir()+ "/" + ROMEO_FILE_NAME, new NullProgressMonitor());
+		} catch (Exception e) {
+			success  = false;
+			report = "An error occurred during export to Romeo phase :" + e + e.getMessage();
+			addResult (new ServiceResult(success,report,this));
+			return report;
+		}
+		
+		
 		
 		// Step 3 : invoke Silien's tool
 		// RUN THE SERVICE
