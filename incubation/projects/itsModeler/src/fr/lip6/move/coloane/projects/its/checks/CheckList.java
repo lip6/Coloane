83208234b1fb6bff7051ca82b42fad5144c0dace
ythierry <ythierry@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2010-06-01 13:50:26 +0000

    adding ordering to checklist

--- incubation/projects/itsModeler/src/fr/lip6/move/coloane/projects/its/checks/CheckList.java
+++ incubation/projects/itsModeler/src/fr/lip6/move/coloane/projects/its/checks/CheckList.java
@@ -5,6 +5,7 @@ import fr.lip6.move.coloane.projects.its.TypeDeclaration;
 import fr.lip6.move.coloane.projects.its.TypeList;
 import fr.lip6.move.coloane.projects.its.obs.ISimpleObserver;
 import fr.lip6.move.coloane.projects.its.obs.SimpleObservable;
+import fr.lip6.move.coloane.projects.its.order.Orders;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -15,11 +16,14 @@ public class CheckList extends SimpleObservable implements Iterable<CheckService
 	private TypeDeclaration type;
 	private List<CheckService> services;
 	private ITypeListProvider typeP;
-
+	private Orders orders;
+	
 	public CheckList(TypeDeclaration td, ITypeListProvider typeP) {
 		this.typeP = typeP;
 		type = td;
 		services = new ArrayList<CheckService>();
+		orders = new Orders();
+		orders.addObserver(this);
 	}
 
 	public TypeDeclaration getType() {
@@ -42,5 +46,9 @@ public class CheckList extends SimpleObservable implements Iterable<CheckService
 
 	public TypeList getTypes() {
 		return typeP.getTypes();
+	}
+	
+	public Orders getOrders() {
+		return orders;
 	}
 }
