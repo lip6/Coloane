4f4d738340b3776801941dc7883a2fedb0267c71
ythierry <ythierry@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2010-06-02 12:57:59 +0000

    add an order parameter

--- incubation/projects/itsModeler/src/fr/lip6/move/coloane/projects/its/checks/ITSCheckService.java
+++ incubation/projects/itsModeler/src/fr/lip6/move/coloane/projects/its/checks/ITSCheckService.java
@@ -5,14 +5,19 @@ import java.util.List;
 
 import org.eclipse.core.runtime.IStatus;
 
+import fr.lip6.move.coloane.core.exceptions.ColoaneException;
 import fr.lip6.move.coloane.projects.its.io.ITSModelWriter;
+import fr.lip6.move.coloane.projects.its.order.ITSOrderWriter;
+import fr.lip6.move.coloane.projects.its.order.Ordering;
 
 public abstract class ITSCheckService extends AbstractCheckService {
 
-
+	private static final String ORDER_FILE_NAME = "model.ord";
+	private OrderParameter order;
 
 	public ITSCheckService(CheckList parent, String serviceName) {
 		super(parent,serviceName);
+		order = new OrderParameter(parent.getOrders());
 	}
 
 	@Override
@@ -20,10 +25,37 @@ public abstract class ITSCheckService extends AbstractCheckService {
 		ArrayList<String> cmd = new ArrayList<String>();
 		cmd.add(getToolPath().toOSString());
 		//		cmd.add("--quiet");
-		//		cmd.add("-T" + format);
 		cmd.add("-xml");
 		cmd.add("modelMain.xml");
+		
+		// Handle order input
+		if (buildOrderFile()) {
+			cmd.add("--load-order");
+			cmd.add(getOrderFileName());
+		}
+		
 		return cmd;
+	}
+
+	private boolean buildOrderFile() {
+		Ordering o = order.getSelection();
+		if (o == null)
+			return false;
+		
+		// build the file
+		ITSOrderWriter ow = new ITSOrderWriter();
+		try {
+			ow.writeOrder(getOrderFileName(), getParent().getType(), o);
+		} catch (ColoaneException e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+		return true;
+	}
+
+	private String getOrderFileName() {
+		return getWorkDir()+"/"+ORDER_FILE_NAME;
 	}
 
 	public String run() {
@@ -35,14 +67,14 @@ public abstract class ITSCheckService extends AbstractCheckService {
 			report = "Run successful in folder "+getWorkDir();
 		} catch (Exception e) {
 			success  = false;
-			report = "An error occurred during service invocation :" + e + e.getMessage();
+			report = "An error occurred during Export phase of service invocation :" + e + e.getMessage();
 		}
 	
 		// RUN THE SERVICE
 		IStatus status = runTool(getWorkDirPath());
 		if (! status.isOK()) {
 			success  = false;
-			report = "An error occurred during service invocation :" + status.getMessage();
+			report += "An error occurred during ITS service invocation :" + status.getMessage();
 		} else {
 			report = getReportText();
 		}
@@ -50,5 +82,7 @@ public abstract class ITSCheckService extends AbstractCheckService {
 		return report;
 	}
 
-	
+	public OrderParameter getOrder() {
+		return order;
+	}
 }
