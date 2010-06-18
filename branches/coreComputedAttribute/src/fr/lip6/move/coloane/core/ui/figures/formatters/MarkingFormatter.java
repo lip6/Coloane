36a6d00738f3fc7264769fdd8aeb4310ec9c7859
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2010-06-18 17:44:22 +0000

    Declare the new element ComputedAttribute :   * XMLFormalism OK   * FormalismElements : OK
    New object : Formatter (class that has to be contributed by the user to format the attribute value)
    
    Todo : EditPart + ModelElement

---
+++ branches/coreComputedAttribute/src/fr/lip6/move/coloane/core/ui/figures/formatters/MarkingFormatter.java
@@ -0,0 +1,12 @@
+package fr.lip6.move.coloane.core.ui.figures.formatters;
+
+import fr.lip6.move.coloane.interfaces.model.IAttributeFormatter;
+import fr.lip6.move.coloane.interfaces.model.IElement;
+
+public class MarkingFormatter implements IAttributeFormatter {
+	
+	public String ApplyFormat(IElement parentElement) {
+		return "toto"; //$NON-NLS-1$
+	}
+
+}
