b8a13c10f547de66c6f9bebb1c84c7729ef6bd51
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2010-06-21 16:46:30 +0000

    Add computed attributes elements to the list of displayed children
    Change constructors in order to exploit new information from formalism definition

--- branches/interfacesComputedAttribute/src/fr/lip6/move/coloane/interfaces/formalism/IComputedAttributeFormalism.java
+++ branches/interfacesComputedAttribute/src/fr/lip6/move/coloane/interfaces/formalism/IComputedAttributeFormalism.java
@@ -2,50 +2,20 @@ package fr.lip6.move.coloane.interfaces.formalism;
 
 import fr.lip6.move.coloane.interfaces.model.IAttributeFormatter;
 
-import org.eclipse.draw2d.geometry.Point;
-
 /**
- * This class represents all the formalism attribute characteristics.<br>
+ * This class represents all the formalism computed attribute characteristics.<br>
  * An attribute is like a property of a base element.<br>
  * Each base element has to maintain its own list of attributes.
+ * <br>
+ * Computed Attributes share some properties with Standard Attributes.<br>
+ * Those common properties are detailed in Global Attributes definition.
  * 
+ * @see IAttributeFormalism
+ * @see IGlobalAttributeFormalism
+ *
  * @author Jean-Baptiste Voron
  */
-public interface IComputedAttributeFormalism {
-
-	/** @return Attribute name */
-	String getName();
-
-	/**
-	 * @return the default value
-	 */
-	String getDefaultValue();
-
-	/**
-	 * @return <code>true</code> if the attribute has to be displayed with a bold font
-	 */
-	boolean isBold();
-
-	/**
-	 * @return <code>true</code> if the attribute has to be displayed with an italic font 
-	 */
-	boolean isItalic();
-
-	/**
-	 * @return the font size
-	 */
-	Integer getFontSize();
-	
-	/**
-	 * @return <code>true</code> if the attribute has to be displayed even if its valued is the default one
-	 */
-	boolean isDefaultValueDrawable();
-	
-	/** 
-	 * <b>Delta Location</b> is used to specify the relative position of the attribute according to the parent element.<br>
-	 * @return the delta location
-	 */
-	Point getDeltaLocation();
+public interface IComputedAttributeFormalism extends IGlobalAttributeFormalism {
 	
 	/** 
 	 * @return The class that is supposed to format the attribute value
