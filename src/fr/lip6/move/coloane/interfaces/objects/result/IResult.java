303f2bf0679e7943b904466a46d717b7762e681e
jbvoron <Jean-Baptiste.Voron@lip6.fr>
2010-06-25 13:35:36 +0000

    interfaces are ready for new result presentation and checker skills

--- src/fr/lip6/move/coloane/interfaces/objects/result/IResult.java
+++ src/fr/lip6/move/coloane/interfaces/objects/result/IResult.java
@@ -4,39 +4,65 @@ import fr.lip6.move.coloane.interfaces.model.IGraph;
 import fr.lip6.move.coloane.interfaces.model.command.ICommand;
 
 import java.util.List;
+import java.util.Map;
 
 /**
- * Cette interface décrit un résultat renvoyé par la plate-forme
+ * This interface describes a <b>result</b> sent by a tool.
+ * A result is basically composed of :
+ * <ul>
+ * 	<li>Sub-Results</li>
+ * 	<li>Textual Results</li>
+ * 	<li>Special Information: Tips</li>
+ * 	<li>A new model</li>
+ * 	<li>Some modifications to perform on the current model</li>
+ * </ul>
+ *
+ * @author Jean-Baptiste Voron
+ * @author Florian David
  */
 public interface IResult {
 
 	/**
-	 * @return tous les sous-résultats
+	 * Return the name of the result (name of the called tool is preferred).
+	 * @return the result name.
 	 */
-	List<ISubResult> getSubResults();
+	String getResultName();
 
 	/**
-	 * @return le nom du menu racine qui contient le service qui fournit ces résultats
+	 * @return the name of the root menu from which the service has been invoked
+	 * @deprecated Use {@link IResult#getResultName()} instead.
 	 */
 	String getRootName();
 
 	/**
-	 * @return le nom du service qui a été invoqué
+	 * @return the invoked service name 
+	 * @deprecated Use {@link IResult#getResultName()} instead.
 	 */
 	String getServiceName();
-
+	
 	/**
-	 * @return la liste d'informations associées au résultat
+	 * @return The list of sub-results contained in the result.
 	 */
-	List<ITip> getTipsList();
+	List<ISubResult> getSubResults();
 
 	/**
-	 * @return le nouveau graphe qui doit être affiché dans une nouvelle fenêtre
+	 * @return The list of textual results contained in the result.
+	 */
+	List<List<String>> getTextualResults();
+
+	/**
+	 * @see ITip
+	 * @return The list of special information associated to the result.
+	 */
+	Map<Integer, List<ITip>> getTips();
+
+	/**
+	 * @return A new graph provided by the tool (or <code>null</code> if there is no new graph to display)
 	 */
 	IGraph getNewGraph();
 
 	/**
-	 * @return les modifications à faire sur le graphe courant
+	 * @return A list of modification to perform on the current model
 	 */
 	List<ICommand> getModificationsOnCurrentGraph();
 }
