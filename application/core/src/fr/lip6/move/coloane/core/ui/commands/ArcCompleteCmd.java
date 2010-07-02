11a6df4297aa2936f233bc415c55321abe109083
jbvoron <Jean-Baptiste.Voron@lip6.fr>
2010-07-02 12:17:12 +0000

    FEATURE Handle the case where the source and the target of an arc are the same object

--- application/core/src/fr/lip6/move/coloane/core/ui/commands/ArcCompleteCmd.java
+++ application/core/src/fr/lip6/move/coloane/core/ui/commands/ArcCompleteCmd.java
@@ -8,63 +8,70 @@ import fr.lip6.move.coloane.interfaces.model.INode;
 
 import java.util.logging.Logger;
 
+import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.gef.commands.Command;
 
 /**
- * Deuxieme étape de la création d'un lien entre deux noeuds !<br>
- * Cette commande est créée lors du second clic (donc sur l'element d'arrivee).
+ * Second step for the link creation between two nodes !<br>
+ * This command is created when the user has designated the <b>target object</b>.
+ * The special case source = target must be handled here !
  * @see ArcCreateCmd
  */
 public class ArcCompleteCmd extends Command {
-	/** Le logger */
+	/** Log factory */
 	private static final Logger LOGGER = Logger.getLogger("fr.lip6.move.coloane.core"); //$NON-NLS-1$
 
-	/** Graphe */
+	/** The current graph */
 	private IGraph graph;
 
-	/** Noeud source */
+	/** Arc Source */
 	private final INode source;
 
-	/** Noeud cible */
+	/** Arc Target */
 	private final INode target;
 
-	/** Element de base du formalisme (arc) */
+	/** Arc description (formalism) */
 	private final IArcFormalism arcFormalism;
 
-	/** L'arc */
+	/** The arc */
 	private IArc arc;
 
 	/**
-	 * Connexion de l'arc
-	 * @param arcSource Noeud source de l'arc
-	 * @param arcTarget Noeud cible de l'arc
-	 * @param arcFormalism Le formalisme de l'arc
+	 * Connect the arc
+	 * @param source Source
+	 * @param target Target
+	 * @param formalism Formalism
 	 */
-	public ArcCompleteCmd(INode arcSource, INode arcTarget, IArcFormalism arcFormalism) {
-		this.graph = (IGraph) arcSource.getParent();
-		this.source = arcSource;
-		this.target = arcTarget;
-		this.arcFormalism = arcFormalism;
+	public ArcCompleteCmd(INode source, INode target, IArcFormalism formalism) {
+		this.graph = (IGraph) source.getParent();
+		this.source = source;
+		this.target = target;
+		this.arcFormalism = formalism;
 	}
 
 	/** {@inheritDoc} */
 	@Override
 	public final boolean canExecute() {
-
-		// La connexion est-elle autorisee par le formalisme ?
-		if (!arcFormalism.getFormalism().isLinkAllowed(this.source, this.target, this.arcFormalism)) {
-			return false;
-		}
-
-		return true;
+		// Is the connection correct according to the formalism ?
+		return arcFormalism.getFormalism().isLinkAllowed(this.source, this.target, this.arcFormalism);
 	}
 
 	/** {@inheritDoc} */
 	@Override
 	public final void execute() {
-		// Construction de l'arc
+		// Build the arc
 		try {
+			// The arc has to be created
 			arc = graph.createArc(arcFormalism.getName(), source, target);
+			
+			// Handle the special case where the source and the target node of the arc are the same
+			if (source.equals(target)) {
+				// In that case, two inflex points are created
+				int x = source.getGraphicInfo().getLocation().x + (source.getGraphicInfo().getSize().width)/2;
+				int y = source.getGraphicInfo().getLocation().y;
+				arc.addInflexPoint(new Point(x-20,y-20));
+				arc.addInflexPoint(new Point(x+20,y-20));
+			}
 		} catch (ModelException e) {
 			LOGGER.warning("Impossible de construire l'arc: " + e.toString()); //$NON-NLS-1$
 			e.printStackTrace();
