0efa67447ef86d6ec18cc1a8d9f9d0f199016aa0
jbvoron <Jean-Baptiste.Voron@lip6.fr>
2010-07-06 18:20:51 +0000

    Clean up on commands

--- src/fr/lip6/move/coloane/core/ui/commands/InflexDeleteCmd.java
+++ src/fr/lip6/move/coloane/core/ui/commands/InflexDeleteCmd.java
@@ -6,23 +6,28 @@ import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.gef.commands.Command;
 
 /**
- * Commande de suppression d'un point d'inflexion
+ * Delete an inflex point from an arc
+ * 
+ * @author Jean-Baptiste Voron
  */
 public class InflexDeleteCmd extends Command {
 
+	/** The arc from which the inflex point will be removed */
 	private IArc arc;
-	private Point position;
+	/** The index of the point inside the list of inflex points */
 	private int index;
+	/** The location of the inflex point that will be removed */
+	private Point position;
 
 	/**
-	 * Constructeur
-	 * @param arcModel L'arc concerne par le point d'inflexion
-	 * @param i L'index
+	 * Constructor
+	 * @param arc The arc from which the inflex point will be removed
+	 * @param index the index of the inflex point in the list of arc inflex points
 	 */
-	public InflexDeleteCmd(IArc arcModel, int i) {
+	public InflexDeleteCmd(IArc arc, int index) {
 		super(Messages.InflexDeleteCmd_0);
-		this.arc = arcModel;
-		this.index = i;
+		this.arc = arc;
+		this.index = index;
 	}
 
 	/** {@inheritDoc} */
@@ -43,6 +48,4 @@ public class InflexDeleteCmd extends Command {
 	public final void redo() {
 		this.arc.removeInflexPoint(this.index);
 	}
-
-
 }
