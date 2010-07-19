2e06fe05e122d46da00d564f7a766d9b3f0b8dee
jbvoron <Jean-Baptiste.Voron@lip6.fr>
2010-07-19 14:00:49 +0000

    Ease the way to connect GraphFormalism and ElementFormalism
    Javadoc

--- src/fr/lip6/move/coloane/core/motor/formalisms/elements/GraphFormalism.java
+++ src/fr/lip6/move/coloane/core/motor/formalisms/elements/GraphFormalism.java
@@ -4,49 +4,50 @@ import fr.lip6.move.coloane.interfaces.formalism.IElementFormalism;
 import fr.lip6.move.coloane.interfaces.formalism.IFormalism;
 import fr.lip6.move.coloane.interfaces.formalism.IGraphFormalism;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
 
 /**
- * Cette classe décrit un conteneur de noeuds, d'arcs et d'attributs d'un formalisme.<br>
- * Chaque instance de formalisme contient forcément au moins un élément de ce type.<br>
- * Dans le cas de la hiérachie, ce conteneur peut être contenu dans un noeud de plus haut niveau.
+ * Describe all properties for a graph.<br>
+ * Basically, it contains nodes, arcs and attributes.<br>
+ * Every formalism instances have, at least, one instance of this element. 
  */
 public class GraphFormalism extends ElementFormalism implements IGraphFormalism {
+	/** Logger */
+	private static final Logger LOGGER = Logger.getLogger("fr.lip6.move.coloane.core"); //$NON-NLS-1$
 
-	/** Liste des éléments de formalisme défini dans ce graphe */
-	private List<IElementFormalism> children = new ArrayList<IElementFormalism>();
+	/** All elements that can be contained inside such a graph */
+	private Map<String,IElementFormalism> children = new HashMap<String,IElementFormalism>();
 
 	/**
-	 * Constructeur
-	 * @param name Le nom du graphe
-	 * @param formalism Le formalisme qui définit ce graphe
+	 * Constructor
+	 * @param name Graph name
+	 * @param formalism The formalism (i.e. constraints and properties)
+	 * @see IFormalism
 	 */
 	public GraphFormalism(String name, IFormalism formalism) {
 		super(name, formalism);
 	}
 
 	/**
-	 * Ajout d'un element de base a la definition d'un graphe. Notion de hierarchie
-	 * @param element {@link ElementFormalism} de base a ajouter aux enfants possibles du graphe.
+	 * Add an element to the graph definition
+	 * @param element The element to add to the graph
 	 */
 	public final void addElement(ElementFormalism element) {
 		if (element == null) { return; }
-		this.children.add(element);
+		LOGGER.finer("Add an element to the graph formalism : " + element.getName()); //$NON-NLS-1$
+		this.children.put(element.getName(), element);
 	}
 
 	/** {@inheritDoc} */
-	public final List<IElementFormalism> getAllElementFormalism() {
-		return this.children;
+	public final Collection<IElementFormalism> getAllElementFormalism() {
+		return this.children.values();
 	}
 
 	/** {@inheritDoc} */
 	public final IElementFormalism getElementFormalism(String name) {
-		for (IElementFormalism element : this.children) {
-			if (name.equals(element.getName())) {
-				return element;
-			}
-		}
-		return null;
+		return children.get(name);
 	}
 }
