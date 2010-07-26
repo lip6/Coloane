95660f734cbf7d694d7b1909923ec66bb145d41b
jbvoron <Jean-Baptiste.Voron@lip6.fr>
2010-07-26 13:57:43 +0000

    First part of result manager Now local action are performed inside a job that locks the current workspace

--- src/fr/lip6/move/coloane/core/extensions/IColoaneAction.java
+++ src/fr/lip6/move/coloane/core/extensions/IColoaneAction.java
@@ -5,6 +5,8 @@ import fr.lip6.move.coloane.interfaces.objects.result.IResult;
 
 import java.util.List;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+
 /**
  * Interface that must be implemented by tools provider.<br>
  * For the moment, tools can only return a set of commands.
@@ -19,7 +21,8 @@ public interface IColoaneAction {
 	
 	/**
 	 * @param model The current session graph model
+	 * @param monitor The progress monitor used to visualize the job progress
 	 * @return A set of commands that will be executed by the core
 	 */
-	List<IResult> run(IGraph model);
+	List<IResult> run(IGraph model, IProgressMonitor monitor);
 }
---
+++ src/fr/lip6/move/coloane/core/ui/ColoaneJob.java
@@ -0,0 +1,69 @@
+package fr.lip6.move.coloane.core.ui;
+
+import fr.lip6.move.coloane.core.extensions.IColoaneAction;
+import fr.lip6.move.coloane.core.ui.actions.LocalAction;
+import fr.lip6.move.coloane.interfaces.model.IGraph;
+import fr.lip6.move.coloane.interfaces.objects.result.IResult;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+
+/**
+ * Jobs are action containers.<br>
+ * To use with {@link LocalAction}
+ * 
+ * @author Jean-Baptiste Voron
+ */
+public class ColoaneJob extends Job {
+	/** Logger */
+	private static final Logger LOGGER = Logger.getLogger("fr.lip6.move.coloane.core"); //$NON-NLS-1$
+	
+	/** The set of results */
+	List<IResult> results;
+	
+	/** The action */
+	IColoaneAction action;
+	
+	/** The graph on which the action has to be performed */
+	IGraph graph;
+
+	/**
+	 * Constructor
+	 * @param name The name of the action
+	 * @param graph The graph on which the action will be performed
+	 * @param actionToPerform The action to run
+	 * @see Job
+	 */
+	public ColoaneJob(String name, IGraph graph, IColoaneAction actionToPerform) {
+		super(name);
+		setSystem(false);
+		this.results = new ArrayList<IResult>();
+		this.action = actionToPerform;
+		this.graph = graph;
+	}
+	
+	public List<IResult> getResults() {
+		return this.results;
+	}
+	
+	@Override
+	protected IStatus run(IProgressMonitor monitor) {
+		this.results = this.action.run(this.graph,monitor);
+		monitor.done();
+		return Status.OK_STATUS;
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	protected final void canceling() {
+		LOGGER.warning("The job " + this.getName() + " has received an INTERRUPT request : "); //$NON-NLS-1$//$NON-NLS-2$
+		getThread().interrupt();
+	}
+
+}
