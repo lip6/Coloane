063dc8ba9a2a5b3953ad4f57c005bafae4e8155c
jbvoron <Jean-Baptiste.Voron@lip6.fr>
2010-07-26 12:37:35 +0000

    Refectoring

--- src/fr/lip6/move/coloane/core/formalisms/constraints/Messages.java
+++ src/fr/lip6/move/coloane/core/formalisms/constraints/Messages.java
@@ -3,7 +3,7 @@ package fr.lip6.move.coloane.core.formalisms.constraints;
 import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
-	private static final String BUNDLE_NAME = "fr.lip6.move.coloane.core.motor.formalisms.constraints.messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "fr.lip6.move.coloane.core.formalisms.constraints.messages"; //$NON-NLS-1$
 	public static String CapacityConstraint_0;
 	public static String CardinalityConstraint_0;
 	public static String ConnectionConstraint_0;
--- src/fr/lip6/move/coloane/core/session/Messages.java
+++ src/fr/lip6/move/coloane/core/session/Messages.java
@@ -3,7 +3,7 @@ package fr.lip6.move.coloane.core.session;
 import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
-	private static final String BUNDLE_NAME = "fr.lip6.move.coloane.core.motor.session.messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "fr.lip6.move.coloane.core.session.messages"; //$NON-NLS-1$
 	public static String Session_0;
 	public static String Session_1;
 	public static String Session_2;
