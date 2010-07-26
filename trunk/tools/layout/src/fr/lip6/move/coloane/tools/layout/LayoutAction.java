d14d1bff352c52a39aa674b29857e554963c4984
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2010-07-26 16:43:17 +0000

    Adapt stats tools for the new result object

--- trunk/tools/layout/src/fr/lip6/move/coloane/tools/layout/LayoutAction.java
+++ trunk/tools/layout/src/fr/lip6/move/coloane/tools/layout/LayoutAction.java
@@ -1,10 +1,10 @@
 package fr.lip6.move.coloane.tools.layout;
 
 import fr.lip6.move.coloane.core.extensions.IColoaneAction;
-import fr.lip6.move.coloane.core.results.Result;
 import fr.lip6.move.coloane.interfaces.model.IGraph;
 import fr.lip6.move.coloane.interfaces.model.requests.IRequest;
 import fr.lip6.move.coloane.interfaces.objects.result.IResult;
+import fr.lip6.move.coloane.interfaces.objects.result.Result;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,10 +20,13 @@ public class LayoutAction implements IColoaneAction {
 	 */
 	public final List<IResult> run(IGraph model, IProgressMonitor monitor) {
 		if (model != null) {
-			List<IRequest> commands = GraphLayout.layout(model, monitor);
-			IResult result = new Result("Dot Layout", model);
+			//List<IRequest> requests = GraphLayout.layout(model, monitor);
+			List<IRequest> requests = new ArrayList<IRequest>();
 			List<IResult> results = new ArrayList<IResult>();
-			results.add(result);
+
+			IResult result = new Result("Dot Layout");
+			result.addDeltaRequests(requests);
+
 			return results;
 		}
 		return null;
