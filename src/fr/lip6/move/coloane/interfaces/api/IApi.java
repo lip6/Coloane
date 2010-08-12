8426b5328ad9aaa126adcbfab73290a247a5f135
jbvoron <Jean-Baptiste.Voron@lip6.fr>
2010-08-12 12:59:04 +0000

    Work on new API integration

--- src/fr/lip6/move/coloane/interfaces/api/IApi.java
+++ src/fr/lip6/move/coloane/interfaces/api/IApi.java
@@ -3,19 +3,13 @@ package fr.lip6.move.coloane.interfaces.api;
 import fr.lip6.move.coloane.interfaces.objects.menu.ISubMenu;
 
 import java.util.List;
-import java.util.Observer;
 
-public interface IApi {
+public interface IApi extends IObservable {
 
 		public final int MENU_OBSERVER = 0;
 		
 		/**
-		 * Set an observer to be able to be warned when something happens to the API
-		 * @param observer The observer
-		 * @param observerType The kind event to observe
+		 * @return The list of menus associated with the API
 		 */
-		void setObserver(Observer observer, int observerType);
-		
-		List<ISubMenu> initApi();
-
+		List<ISubMenu> getApiMenus();
 }
