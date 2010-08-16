9298daed8e319532c04cad1dda5633790d1952d1
jbvoron <jbvoron@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2010-08-16 09:12:50 +0000

    Constraints have been moved to interface packages

---
+++ branches/interfacesCheckResults/src/fr/lip6/move/coloane/interfaces/formalism/constraints/IConstraint.java
@@ -0,0 +1,19 @@
+package fr.lip6.move.coloane.interfaces.formalism.constraints;
+
+/**
+ * Constraint.<br>
+ * There exist two kind of constraints:
+ * <ul>
+ * 	<li>{@link IConstraintLink}</li>
+ * 	<li>{@link IConstraintNode}</li>
+ * </ul>
+ * 
+ * @author Jean-Baptiste Voron
+ */
+public interface IConstraint {
+
+	String PARAMETER_ID = "parameter"; //$NON-NLS-1$
+	String PARAMETER_NAME = "name"; //$NON-NLS-1$
+	String PARAMETER_VALUE = "value"; //$NON-NLS-1$
+
+}
---
+++ branches/interfacesCheckResults/src/fr/lip6/move/coloane/interfaces/formalism/constraints/IConstraintNode.java
@@ -0,0 +1,17 @@
+package fr.lip6.move.coloane.interfaces.formalism.constraints;
+
+import fr.lip6.move.coloane.interfaces.model.INode;
+
+/**
+ * Node constraint
+ * @author Jean-Baptiste Voron
+ */
+public interface IConstraintNode {
+
+	/**
+	 * Is the node constraint verified ?
+	 * @param node The node on which the constraint is applied
+	 * @return <code>true</code> if the constraint is verified. <code>false</code> otherwise.
+	 */
+	boolean isSatisfied(INode node);
+}
