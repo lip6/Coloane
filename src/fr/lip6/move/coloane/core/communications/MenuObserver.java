84a739f2db1f7e431551f31be906d081dfbf5936
jbvoron <Jean-Baptiste.Voron@lip6.fr>
2010-08-16 17:34:52 +0000

    Service an Option can be directly children of the rootMenu
    SubMenu, Service and Option can declare an icon

--- src/fr/lip6/move/coloane/core/communications/MenuObserver.java
+++ src/fr/lip6/move/coloane/core/communications/MenuObserver.java
@@ -1,8 +1,9 @@
 package fr.lip6.move.coloane.core.communications;
 
 import fr.lip6.move.coloane.core.ui.menus.ColoaneAPIRootMenu;
+import fr.lip6.move.coloane.core.ui.menus.ColoaneMenuManager;
 import fr.lip6.move.coloane.core.ui.menus.MenuManipulation;
-import fr.lip6.move.coloane.interfaces.objects.menu.ISubMenu;
+import fr.lip6.move.coloane.interfaces.objects.menu.IItemMenu;
 
 import java.util.List;
 import java.util.Observable;
@@ -37,9 +38,12 @@ public class MenuObserver implements Observer {
 
 		// Build sub-menus
 		LOGGER.finer("Fetching new sub-menus"); //$NON-NLS-1$
-		List<ISubMenu> submenus = (List<ISubMenu>) newMenus; 
-		for (ISubMenu submenu : submenus) {
-			this.rootMenu.add(MenuManipulation.buildSubMenu(submenu));
+		List<IItemMenu> submenus = (List<IItemMenu>) newMenus; 
+		for (IItemMenu submenu : submenus) {
+			ColoaneMenuManager newMenu = MenuManipulation.buildSubMenu(this.rootMenu, submenu);
+			if (newMenu != null) {
+				this.rootMenu.add(newMenu);
+			}
 		}
 	}
 }
