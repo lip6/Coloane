9828fa726507373a0107f8b4a36d8039947ce6be
cdemoulins <cdemoulins@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2010-11-30 11:15:50 +0000

    Migration of formalism its to new apis

--- incubation/fragments/formalisms/its/src/fr/lip6/move/coloane/core/formalisms/its/CircularEmptyModel.java
+++ incubation/fragments/formalisms/its/src/fr/lip6/move/coloane/core/formalisms/its/CircularEmptyModel.java
@@ -1,24 +1,31 @@
 package fr.lip6.move.coloane.core.formalisms.its;
 
-import fr.lip6.move.coloane.core.extensions.IExample;
-import fr.lip6.move.coloane.core.model.GraphModelFactory;
+import org.eclipse.draw2d.geometry.Point;
+
+import fr.lip6.move.coloane.core.model.factory.GraphModelFactory;
 import fr.lip6.move.coloane.interfaces.exceptions.ModelException;
+import fr.lip6.move.coloane.interfaces.exceptions.PluginException;
+import fr.lip6.move.coloane.interfaces.extensions.IExample;
+import fr.lip6.move.coloane.interfaces.formalism.IFormalism;
+import fr.lip6.move.coloane.interfaces.formalism.INodeFormalism;
 import fr.lip6.move.coloane.interfaces.model.IGraph;
 import fr.lip6.move.coloane.interfaces.model.INode;
 
 public class CircularEmptyModel implements IExample {
 
-	public IGraph buildModel() {
-		IGraph graph = new GraphModelFactory().createGraph("CircularComposite");
+	public IGraph buildModel(IFormalism formalism) throws PluginException {
+		IGraph graph = new GraphModelFactory().createGraph(formalism);
 		try {
-			INode node = graph.createNode("instance");
+			INode node = graph.createNode((INodeFormalism) formalism.getRootGraph().getElementFormalism("instance"));
+			node.getGraphicInfo().setLocation(new Point(100, 100));
 			node.getAttribute("name").setValue("current");
 			node.getAttribute("type").setValue("Type");
 			
-			node = graph.createNode("instance");
+			node = graph.createNode((INodeFormalism) formalism.getRootGraph().getElementFormalism("instance"));
+			node.getGraphicInfo().setLocation(new Point(200, 100));
 			node.getAttribute("name").setValue("successor");
 			node.getAttribute("type").setValue("Type");
-						
+			
 		} catch (ModelException e) {
 			e.printStackTrace();
 		}
