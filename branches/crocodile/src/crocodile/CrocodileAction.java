be88f61ab061eb52288cb7931e6bd963c6829c5e
mcolange <mcolange@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2011-01-19 16:17:07 +0000

    Adding Crocodile plugin.

---
+++ branches/crocodile/src/crocodile/CrocodileAction.java
@@ -0,0 +1,25 @@
+package crocodile;
+
+import java.util.List;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+
+import fr.lip6.move.coloane.interfaces.exceptions.ServiceException;
+import fr.lip6.move.coloane.interfaces.model.IGraph;
+import fr.lip6.move.coloane.interfaces.objects.result.IResult;
+import fr.lip6.move.coloane.interfaces.objects.services.IService;
+
+public class CrocodileAction implements IService {
+
+	public CrocodileAction() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public List<IResult> run(IGraph model, IProgressMonitor monitor)
+			throws ServiceException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
