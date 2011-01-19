a5b34efd1d427a6c3c91664cdd1edf83ee3b0a01
mcolange <mcolange@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2011-01-19 16:50:03 +0000

    Beginning the integration of Crocodile into a Coloane plugin.

--- branches/crocodile/src/crocodile/CrocodileAction.java
+++ branches/crocodile/src/crocodile/CrocodileAction.java
@@ -1,9 +1,13 @@
 package crocodile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 
+import fr.lip6.move.coloane.extensions.exporttogml.ExportToGML;
+import fr.lip6.move.coloane.interfaces.exceptions.ExtensionException;
 import fr.lip6.move.coloane.interfaces.exceptions.ServiceException;
 import fr.lip6.move.coloane.interfaces.model.IGraph;
 import fr.lip6.move.coloane.interfaces.objects.result.IResult;
@@ -18,7 +22,20 @@ public class CrocodileAction implements IService {
 	@Override
 	public List<IResult> run(IGraph model, IProgressMonitor monitor)
 			throws ServiceException {
-		// TODO Auto-generated method stub
+		try {
+			File tmpFile = File.createTempFile("tmp", ".gml");
+			tmpFile.deleteOnExit();
+			
+			ExportToGML exporter = new ExportToGML();
+			// TODO use a sub-monitor instead of monitor
+			exporter.export(model, tmpFile.getAbsolutePath(), monitor);
+			
+		} catch (IOException e) {
+			throw new ServiceException(e.getMessage());
+		} catch (ExtensionException e) {
+			throw new ServiceException(e.getMessage());
+		}
+		
 		return null;
 	}
 
