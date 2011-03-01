79fe59dc0a222820a5a4428ab487bc5ccf86c5fb
ythierry <ythierry@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2011-03-01 23:52:47 +0000

    to make checkstyle a bit happier

--- incubation/fragments/formalisms/tpn/src/fr/lip6/move/coloane/core/ui/figures/nodes/TPNTransition.java
+++ incubation/fragments/formalisms/tpn/src/fr/lip6/move/coloane/core/ui/figures/nodes/TPNTransition.java
@@ -23,9 +23,10 @@ import org.eclipse.draw2d.geometry.Rectangle;
 
 import fr.lip6.move.coloane.core.ui.figures.AbstractNodeFigure;
 
-
 /**
- * A figure that changes its aspect depending on the value of the visibility attribute.
+ * A figure that changes its aspect depending on the value of the visibility
+ * attribute.
+ *
  * @author Yann based on RectangleNode
  *
  */
@@ -43,23 +44,24 @@ public class TPNTransition extends AbstractNodeFigure {
 	 * {@inheritDoc}
 	 */
 	@Override
-	protected final void outlineShape(Graphics graphics) {
+	protected final void outlineShape(final Graphics graphics) {
 		Rectangle r = getBounds();
 
-		boolean isPublic = getModel().getAttribute("visibility").getValue().equals("public");
-		
+		boolean isPublic = getModel().getAttribute("visibility").getValue()
+				.equals("public");
+
 		int x = r.x + lineWidth / 2;
 		int y = r.y + lineWidth / 2;
 		int w = r.width - Math.max(1, lineWidth);
 		int h = r.height - Math.max(1, lineWidth);
 		graphics.drawRectangle(x, y, w, h);
-		
+
 		// double the line for public transition : gives a "bold" effect.
 		if (isPublic) {
 			int ix = x + 2;
-			int iy = y + 2; 
-			int iw = Math.max(1,w - 4); 
-			int ih = Math.max(1,h - 4); 
+			int iy = y + 2;
+			int iw = Math.max(1, w - 4);
+			int ih = Math.max(1, h - 4);
 			graphics.drawRectangle(ix, iy, iw, ih);
 		}
 
@@ -71,5 +73,5 @@ public class TPNTransition extends AbstractNodeFigure {
 	public final ConnectionAnchor getConnectionAnchor() {
 		return new ChopboxAnchor(this);
 	}
-	
+
 }
