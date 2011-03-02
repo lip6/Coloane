d7468104f7e4f8038f51d73f07783dda6c9c054f
ythierry <ythierry@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2011-03-02 19:09:58 +0000

    javadoc

--- incubation/projects/itsModeler/src/fr/lip6/move/coloane/projects/its/actions/FlattenModelAction.java
+++ incubation/projects/itsModeler/src/fr/lip6/move/coloane/projects/its/actions/FlattenModelAction.java
@@ -16,45 +16,48 @@
  */
 package fr.lip6.move.coloane.projects.its.actions;
 
+import fr.lip6.move.coloane.projects.its.TypeDeclaration;
+import fr.lip6.move.coloane.projects.its.plugin.wizards.FlattenNewModelWizard;
+
+
 import org.eclipse.jface.action.Action;
-import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.ui.PlatformUI;
 
-import fr.lip6.move.coloane.projects.its.TypeDeclaration;
-import fr.lip6.move.coloane.projects.its.plugin.wizards.FlattenNewModelWizard;
 
 /**
  * Class associated with buttons to handle model flattening.
  */
-public class FlattenModelAction extends Action 
-{
+public final class FlattenModelAction extends Action {
 
 	private TypeDeclaration td;
 
+	/**
+	 * Position the TypeDeclaration we are working with.
+	 * @param td the TypeDeclaration to set
+	 */
 	public void setTypeDeclaration(TypeDeclaration td) {
 		this.td = td;
 	}
-	
+
 	/**
-	 * @see IActionDelegate#run(IAction)
-	 * Instantiates the wizard and opens it in the wizard container
+	 * @see IActionDelegate#run(IAction) Instantiates the wizard and opens it in
+	 *      the wizard container
+	 * {@inheritDoc}
 	 */
 	public void run() {
-		
+
 		// Instantiates and initializes the wizard
 		FlattenNewModelWizard wizard = new FlattenNewModelWizard(td);
-		
-		wizard.init(PlatformUI.getWorkbench(), 
-			(IStructuredSelection)null);
-			
+
+		wizard.init(PlatformUI.getWorkbench(), (IStructuredSelection) null);
+
 		// Instantiates the wizard container with the wizard and opens it
-		WizardDialog dialog = new WizardDialog( PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), wizard);
+		WizardDialog dialog = new WizardDialog(PlatformUI.getWorkbench()
+				.getActiveWorkbenchWindow().getShell(), wizard);
 		dialog.create();
 		dialog.open();
 	}
-
-
 
 }
