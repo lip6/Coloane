a88371af30d47006a46f396f512a99d034f108b2
ythierry <ythierry@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2011-03-07 21:58:43 +0000

    format

--- incubation/projects/itsModeler/src/fr/lip6/move/coloane/projects/its/checks/AbstractCheckService.java
+++ incubation/projects/itsModeler/src/fr/lip6/move/coloane/projects/its/checks/AbstractCheckService.java
@@ -16,6 +16,11 @@
  */
 package fr.lip6.move.coloane.projects.its.checks;
 
+import fr.lip6.move.coloane.interfaces.utils.ProcessController;
+import fr.lip6.move.coloane.interfaces.utils.ProcessController.TimeOutException;
+import fr.lip6.move.coloane.projects.its.obs.SimpleObservable;
+import fr.lip6.move.coloane.projects.its.ui.forms.ITSEditorPlugin;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Iterator;
@@ -31,38 +36,17 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Status;
 
-import fr.lip6.move.coloane.interfaces.utils.ProcessController;
-import fr.lip6.move.coloane.interfaces.utils.ProcessController.TimeOutException;
-import fr.lip6.move.coloane.projects.its.obs.SimpleObservable;
-import fr.lip6.move.coloane.projects.its.ui.forms.ITSEditorPlugin;
 
-public abstract class AbstractCheckService extends SimpleObservable implements IServiceResultProvider {
+public abstract class AbstractCheckService extends SimpleObservable implements
+		IServiceResultProvider {
 
 	private static final int DEFAULT_TIMEOUT = 60;
 	private static final String TIMEOUT_DURATION = "Maximum execution time";
-	protected String name ;
-
-	protected abstract IPath getToolPath();
-
-	protected abstract List<String> buildCommandArguments();
-
-	private String workdir;
-	protected CheckList parent;
-	private List<ServiceResult> results = new LinkedList<ServiceResult>();
-	private String reportText;
-	private ParameterList parameters = new ParameterList();
-
-	
-	public AbstractCheckService(CheckList parent, String serviceName) {
-		this.parent = parent;
-		this.name = serviceName;
-		parameters.addParameter(TIMEOUT_DURATION);
-		parameters.setParameterValue(TIMEOUT_DURATION,Integer.toString(DEFAULT_TIMEOUT));
-	}
-	
 	/**
 	 * format error message
-	 * @param errorOutput the message
+	 * 
+	 * @param errorOutput
+	 *            the message
 	 * @return the formatted message
 	 */
 	private static String createContentMessage(ByteArrayOutputStream errorOutput) {
@@ -72,46 +56,87 @@ public abstract class AbstractCheckService extends SimpleObservable implements I
 		return " Process produced the following error output: \n" + errorOutput;
 	}
 
-	public String getName() {
-		return name;
+	protected String name;
+
+	private ParameterList parameters = new ParameterList();
+
+	protected CheckList parent;
+	private String reportText;
+	private List<ServiceResult> results = new LinkedList<ServiceResult>();
+	private String workdir;
+	public AbstractCheckService(CheckList parent, String serviceName) {
+		this.parent = parent;
+		this.name = serviceName;
+		parameters.addParameter(TIMEOUT_DURATION);
+		parameters.setParameterValue(TIMEOUT_DURATION,
+				Integer.toString(DEFAULT_TIMEOUT));
 	}
 
-	public void setWorkdir(String workdir) {
-		if (! workdir.equals(this.workdir)) {
-			this.workdir = workdir;
-			notifyObservers();
+	@Override
+	public void addResult(ServiceResult serviceResult) {
+		results.add(serviceResult);
+		notifyObservers();
+	}
+
+	protected abstract List<String> buildCommandArguments();
+
+	public String getDefaultWorkDir(IFile file) {
+		IProject proj = file.getProject();
+		IFolder folder = proj.getFolder(getParent().getType().getTypeName()
+				+ "_" + getName());
+		try {
+			folder.create(true, true, null);
+		} catch (CoreException e) {
+			// folder exists, it's OK.
 		}
+		return folder.getLocation().toOSString();
+	}
+
+	public String getName() {
+		return name;
 	}
 
 	public ParameterList getParameters() {
 		return parameters;
 	}
 
-	public String getWorkDir (IFile position) {
+	public CheckList getParent() {
+		return parent;
+	}
+
+	public String getReportText() {
+		return reportText;
+	}
+
+	protected abstract IPath getToolPath();
+
+	public String getWorkDir() {
+		return workdir;
+	}
+
+	public String getWorkDir(IFile position) {
 		if (workdir == null) {
 			workdir = getDefaultWorkDir(position);
 		}
 		return getWorkDir();
-	}
-	
-	public String getWorkDir() {
-		return workdir;
 	}
 
 	public IPath getWorkDirPath() {
 		return new Path(workdir);
 	}
 
+	@Override
+	public Iterator<ServiceResult> iterator() {
+		return results.iterator();
+	}
 
 	public abstract String run();
-	
+
 	/**
 	 * Bare bones API for launching dot. Command line options are passed to
 	 * Graphviz as specified in the options parameter. The location for dot is
 	 * obtained from the user preferences.
 	 * 
-	 * @param options
-	 *            command line options for dot
 	 * @return a non-zero integer if errors happened
 	 * @throws IOException
 	 */
@@ -121,83 +146,63 @@ public abstract class AbstractCheckService extends SimpleObservable implements I
 			return new Status(
 					IStatus.ERROR,
 					ITSEditorPlugin.getID(),
-			"Please specify the absolute path to the tool in the preferences page Coloane->ITS Path.");
+					"Please specify the absolute path to the tool in the preferences page Coloane->ITS Path.");
 		}
 		if (!toolFullPath.toFile().isFile()) {
-			return new Status(IStatus.ERROR, ITSEditorPlugin.getID(), "Could not find ITS tool at \"" + toolFullPath
-					+ "\"");
+			return new Status(IStatus.ERROR, ITSEditorPlugin.getID(),
+					"Could not find ITS tool at \"" + toolFullPath + "\"");
 		}
 		List<String> cmd = buildCommandArguments();
 		ByteArrayOutputStream errorOutput = new ByteArrayOutputStream();
 		ByteArrayOutputStream stdOutput = new ByteArrayOutputStream();
-	
+
 		int timeout = DEFAULT_TIMEOUT;
 		try {
-			timeout = Integer.parseInt(parameters.getParameterValue(TIMEOUT_DURATION));
+			timeout = Integer.parseInt(parameters
+					.getParameterValue(TIMEOUT_DURATION));
 		} catch (NumberFormatException e) {
 			// NOP, stay at 60 secs.
-		}		
-			
+		}
+
 		try {
-			final ProcessController controller =
-				new ProcessController(timeout * 1000, cmd.toArray(new String[cmd.size()]), null, workdir.toFile());
+			final ProcessController controller = new ProcessController(
+					timeout * 1000, cmd.toArray(new String[cmd.size()]), null,
+					workdir.toFile());
 			controller.forwardErrorOutput(errorOutput);
 			controller.forwardOutput(stdOutput);
 			int exitCode = controller.execute();
 			if (exitCode != 0) {
-				//				return new Status(IStatus.WARNING, ITSEditorPlugin.getID(), "ITS exit code: " + exitCode + "."
-				//						+ createContentMessage(errorOutput));
-				//			}
+				// return new Status(IStatus.WARNING, ITSEditorPlugin.getID(),
+				// "ITS exit code: " + exitCode + "."
+				// + createContentMessage(errorOutput));
+				// }
 				if (errorOutput.size() > 0) {
-					return new Status(IStatus.WARNING, ITSEditorPlugin.getID(), createContentMessage(errorOutput));
+					return new Status(IStatus.WARNING, ITSEditorPlugin.getID(),
+							createContentMessage(errorOutput));
 				}
 			}
 			reportText = stdOutput.toString();
 			return Status.OK_STATUS;
 		} catch (TimeOutException e) {
-			return new Status(IStatus.ERROR, ITSEditorPlugin.getID(), "Check Service process did not finish in a timely way."
-					+ createContentMessage(errorOutput));
+			return new Status(IStatus.ERROR, ITSEditorPlugin.getID(),
+					"Check Service process did not finish in a timely way."
+							+ createContentMessage(errorOutput));
 		} catch (IOException e) {
-			return new Status(IStatus.ERROR, ITSEditorPlugin.getID(), "Unexpected exception executing service."
-					+ createContentMessage(errorOutput), e);
+			return new Status(IStatus.ERROR, ITSEditorPlugin.getID(),
+					"Unexpected exception executing service."
+							+ createContentMessage(errorOutput), e);
 		}
-	}
-
-
-
-
-
-	public void addResult(ServiceResult serviceResult) {
-		results.add(serviceResult);
-		notifyObservers();
-	}
-
-	public Iterator<ServiceResult> iterator() {
-		return results.iterator();
-	}
-
-	public CheckList getParent() {
-		return parent;
 	}
 
 	protected void setReport(String report) {
 		this.reportText = report;
 	}
 
-	public String getReportText() {
-		return reportText;
-	}
-
-	
-	public String getDefaultWorkDir (IFile file) {
-		IProject proj = file.getProject();		
-		IFolder folder = proj.getFolder(getParent().getType().getTypeName() + "_" + getName());
-		try {
-			folder.create(true, true, null);
-		} catch (CoreException e) {
-			// folder exists, it's OK.
+	public void setWorkdir(String workdir) {
+		if (!workdir.equals(this.workdir)) {
+			this.workdir = workdir;
+			notifyObservers();
 		}
-		return folder.getLocation().toOSString();
 	}
 
 }
