b3dcdb309737fa1df28a91bdfc8afd6b1cd952d5
ythierry <ythierry@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2011-05-27 13:53:31 +0000

    Enhanced version that Observes its model element.

--- incubation/fragments/formalisms/its/src/fr/lip6/move/coloane/core/ui/figures/nodes/InstanceNode.java
+++ incubation/fragments/formalisms/its/src/fr/lip6/move/coloane/core/ui/figures/nodes/InstanceNode.java
@@ -17,6 +17,10 @@
 package fr.lip6.move.coloane.core.ui.figures.nodes;
 
 import fr.lip6.move.coloane.core.ui.figures.AbstractNodeFigure;
+import fr.lip6.move.coloane.interfaces.model.INode;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 import org.eclipse.draw2d.ChopboxAnchor;
 import org.eclipse.draw2d.ConnectionAnchor;
@@ -30,7 +34,9 @@ import org.eclipse.draw2d.geometry.Dimension;
  *
  * @author Y. Thierry-Mieg, based on C. Demoulins RectangleNode class
  */
-public class InstanceNode extends AbstractNodeFigure {
+public class InstanceNode extends AbstractNodeFigure implements PropertyChangeListener {
+	private String toshow = ":";
+
 	/**
 	 * {@inheritDoc}
 	 */
@@ -43,13 +49,21 @@ public class InstanceNode extends AbstractNodeFigure {
 	 * {@inheritDoc}
 	 */
 	@Override
+	protected void modelElementChanged(INode oldModelElement,
+			INode newModelElement) {
+		if (oldModelElement != null) {
+			oldModelElement.removePropertyChangeListener(this);
+		}
+		newModelElement.addPropertyChangeListener(this);
+		// trigger an update.
+		propertyChange(null);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
 	protected final void outlineShape(Graphics graphics) {
-		String name = getModel().getAttribute("name").getValue();
-		String type = getModel().getAttribute("type").getValue();
-		String toshow = name + " : " + type;
-		Dimension textsize = FigureUtilities.getStringExtents(toshow, getFont());
-		getModel().getGraphicInfo().setSize(textsize.expand(10,15));		
-		
 		graphics.drawString(toshow, getBounds().getTopLeft().translate(5, 3));
 		setBorder(new LineBorder(2));
 	}
@@ -59,5 +73,22 @@ public class InstanceNode extends AbstractNodeFigure {
 	 */
 	public final ConnectionAnchor getConnectionAnchor() {
 		return new ChopboxAnchor(this);
+	}
+
+	/**
+	 * We update the String toshow and the size here if necessary to reflect model changes.
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void propertyChange(PropertyChangeEvent event) {
+		
+		String name = getModel().getAttribute("name").getValue();
+		String type = getModel().getAttribute("type").getValue();
+		String toshow2 = name + " : " + type;
+		if (! toshow2.equals(toshow) ) {
+			toshow = toshow2;
+			Dimension textsize = FigureUtilities.getStringExtents(toshow, getFont());
+			getModel().getGraphicInfo().setSize(textsize.expand(10,15));
+		}
 	}
 }
