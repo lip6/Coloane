8acc27409814ef511cd843de7e978a3b5e1c0cf3
cdemoulins <cdemoulins@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2011-06-10 08:36:23 +0000

    Configuration of the plugin

---
+++ branches/importFromGML/src/fr/lip6/move/coloane/extensions/importfromgml/ImportFromGML.java
@@ -0,0 +1,22 @@
+package fr.lip6.move.coloane.extensions.importfromgml;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+
+import fr.lip6.move.coloane.interfaces.exceptions.ExtensionException;
+import fr.lip6.move.coloane.interfaces.extensions.IImportFrom;
+import fr.lip6.move.coloane.interfaces.formalism.IFormalism;
+import fr.lip6.move.coloane.interfaces.model.IGraph;
+
+public class ImportFromGML implements IImportFrom {
+
+	public ImportFromGML() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public IGraph importFrom(String filePath, IFormalism formalism,
+			IProgressMonitor monitor) throws ExtensionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
