bea1f00f0a978f814d39c1dd591e7fd86dd54e1d
elodie <elodie@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2011-07-22 09:55:08 +0000

    Formalism Archetype project -- first commited version.

---
+++ branches/formalismArchetype/src/main/resources/archetype-resources/src/attribute/LanguageQuickfixProvider.java
@@ -0,0 +1,20 @@
+
+package attribute.ui.quickfix;
+
+import fr.lip6.move.coloane.core.ui.properties.editor.EmbeddedIssueResolutionAcceptor;
+import fr.lip6.move.coloane.core.ui.properties.editor.EmbeddedQuickfixProvider;
+
+public class LanguageQuickfixProvider extends EmbeddedQuickfixProvider {
+
+//	@Fix(MyJavaValidator.INVALID_NAME)
+//	public void capitalizeName(final Issue issue, EmbeddedIssueResolutionAcceptor acceptor) {
+//		acceptor.accept(issue, "Capitalize name", "Capitalize the name.", "upcase.png", new IModification() {
+//			public void apply(IModificationContext context) throws BadLocationException {
+//				IXtextDocument xtextDocument = context.getXtextDocument();
+//				String firstLetter = xtextDocument.get(issue.getOffset(), 1);
+//				xtextDocument.replace(issue.getOffset(), 1, firstLetter.toUpperCase());
+//			}
+//		});
+//	}
+
+}
