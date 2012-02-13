8f89fc4658ff76f83729ab158f630441d793b437
cdemoulins <cdemoulins@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2012-02-13 15:26:19 +0000

    First version of Coloane as an eclipse rcp application.

---
+++ trunk/application/product/src/fr/lip6/move/coloane/rcp/ApplicationActionBarAdvisor.java
@@ -0,0 +1,56 @@
+package fr.lip6.move.coloane.rcp;
+
+import org.eclipse.jface.action.GroupMarker;
+import org.eclipse.jface.action.IContributionItem;
+import org.eclipse.jface.action.ICoolBarManager;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.action.Separator;
+import org.eclipse.ui.IWorkbenchActionConstants;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.actions.ActionFactory;
+import org.eclipse.ui.actions.ActionFactory.IWorkbenchAction;
+import org.eclipse.ui.application.ActionBarAdvisor;
+import org.eclipse.ui.application.IActionBarConfigurer;
+
+public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
+
+	private IContributionItem separator;
+
+	public ApplicationActionBarAdvisor(IActionBarConfigurer configurer) {
+        super(configurer);
+    }
+
+    protected void makeActions(IWorkbenchWindow window) {
+    	separator = new Separator();
+    	IWorkbenchAction newFile = ActionFactory.NEW_WIZARD_DROP_DOWN.create(window);
+    	newFile.setText("New");
+    	register(newFile);
+    	register(ActionFactory.IMPORT.create(window));
+    	register(ActionFactory.EXPORT.create(window));
+    	register(ActionFactory.PRINT.create(window));
+    	register(ActionFactory.QUIT.create(window));
+    }
+
+	/** {@inheritDoc} */
+	@Override
+	protected void fillCoolBar(ICoolBarManager coolBar) {
+	}
+
+	protected void fillMenuBar(IMenuManager menuBar) {
+    	IMenuManager fileMenu = new MenuManager("&File", "coloane.file");
+    	fileMenu.add(getAction(ActionFactory.NEW_WIZARD_DROP_DOWN.getId()));
+    	fileMenu.add(separator);
+    	fileMenu.add(getAction(ActionFactory.IMPORT.getId()));
+    	fileMenu.add(getAction(ActionFactory.EXPORT.getId()));
+    	fileMenu.add(separator);
+    	fileMenu.add(getAction(ActionFactory.PRINT.getId()));
+    	fileMenu.add(separator);
+    	fileMenu.add(getAction(ActionFactory.QUIT.getId()));
+    	
+    	menuBar.add(fileMenu);
+    	menuBar.add(new GroupMarker(IWorkbenchActionConstants.MB_ADDITIONS));
+    	
+    }
+    
+}
---
+++ trunk/application/product/src/fr/lip6/move/coloane/rcp/ApplicationWorkbenchWindowAdvisor.java
@@ -0,0 +1,25 @@
+package fr.lip6.move.coloane.rcp;
+
+import org.eclipse.ui.application.ActionBarAdvisor;
+import org.eclipse.ui.application.IActionBarConfigurer;
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
+
+    public ApplicationWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+        super(configurer);
+    }
+
+    public ActionBarAdvisor createActionBarAdvisor(IActionBarConfigurer configurer) {
+        return new ApplicationActionBarAdvisor(configurer);
+    }
+    
+    public void preWindowOpen() {
+        IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
+        configurer.setShowCoolBar(true);
+        configurer.setShowStatusLine(true);
+        configurer.setShowFastViewBars(true);
+        configurer.setShowMenuBar(true);
+    }
+}
