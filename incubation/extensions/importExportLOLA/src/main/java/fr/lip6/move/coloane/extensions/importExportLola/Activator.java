ec4444f940633bfa792956152534e5bd7a482ffb
ythierry <ythierry@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2012-06-11 09:49:50 +0000

    the project as built by Anas Aouad

---
+++ incubation/extensions/importExportLOLA/src/main/java/fr/lip6/move/coloane/extensions/importExportLola/Activator.java
@@ -0,0 +1,43 @@
+package main.java.fr.lip6.move.coloane.extensions.importExportLola;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class for the plug-in
+ *
+ */
+public class Activator extends AbstractUIPlugin {
+	// The plug-in ID
+	public static final String PLUGIN_ID = "fr.lip6.move.coloane.extensions.importExportLola";
+
+	// The shared instance
+	private static Activator plugin;
+
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+	}
+
+	/** {@inheritDoc} */
+	public final void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/** {@inheritDoc} */
+	public final void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+}
---
+++ incubation/extensions/importExportLOLA/src/main/java/fr/lip6/move/coloane/extensions/importExportLola/ImportFromLola.java
@@ -0,0 +1,64 @@
+package main.java.fr.lip6.move.coloane.extensions.importExportLola;
+
+
+import fr.lip6.move.coloane.interfaces.exceptions.ExtensionException;
+import fr.lip6.move.coloane.interfaces.extensions.IImportFrom;
+import fr.lip6.move.coloane.interfaces.formalism.IFormalism;
+import fr.lip6.move.coloane.interfaces.model.IGraph;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import main.antlr3.fr.lip6.move.coloane.extensions.importExportLola.parser.LoLALexer;
+import main.antlr3.fr.lip6.move.coloane.extensions.importExportLola.parser.LoLAParser;
+
+import org.antlr.runtime.ANTLRFileStream;
+import org.antlr.runtime.CommonTokenStream;
+import org.antlr.runtime.RecognitionException;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+
+/**
+ * Imports a model written in LoLA format
+ * 
+ * @author Anas Aouad
+ *
+ */
+public class ImportFromLola implements IImportFrom {
+	
+	private static final Logger LOGGER = Logger.getLogger("fr.lip6.move.coloane.core");
+
+	/**
+	 * Import a LoLA file into a Graph object
+	 * @param filePath The location of the file to be imported
+	 * @param formalism The formalism
+	 * @param monitor A monitor to follow the operation progress
+	 * @return The resulting model {@link IGraph}
+	 * @throws ExtensionException Something went wrong
+	 */
+	public final IGraph importFrom(String filePath, IFormalism formalism,
+			IProgressMonitor monitor) throws ExtensionException {
+
+		LOGGER.fine("Creating the workspace file...");
+
+		LoLALexer lexer;
+		try {
+			lexer = new LoLALexer(new ANTLRFileStream(filePath));
+		} catch (IOException e) {
+			throw new ExtensionException("Problem opening file " + e.getMessage());
+		}
+
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+		LoLAParser parser = new LoLAParser(tokens);
+		parser.setFormalism(formalism);
+		IGraph graph;
+		try {
+			graph = parser.lolaModel();
+		} catch (RecognitionException e) {
+			throw new ExtensionException("Error parsing prod file " + e.getMessage());
+		}
+		return graph;
+	}
+
+}
