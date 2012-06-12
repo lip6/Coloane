a95b55bcc794ba4976f4f3e9909335ab857c9762
ythierry <ythierry@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2012-06-12 15:07:20 +0000

    Now support of other export formats

--- trunk/apis/alligator/src/fr/lip6/move/coloane/api/alligator/wizard/ParametersWizard.java
+++ trunk/apis/alligator/src/fr/lip6/move/coloane/api/alligator/wizard/ParametersWizard.java
@@ -22,12 +22,18 @@ import fr.lip6.move.coloane.core.main.Coloane;
 import fr.lip6.move.coloane.core.ui.files.ModelLoader;
 import fr.lip6.move.coloane.extensions.exporttogml.ExportToGML;
 import fr.lip6.move.coloane.interfaces.exceptions.ExtensionException;
+import fr.lip6.move.coloane.interfaces.extensions.IExportTo;
 import fr.lip6.move.coloane.interfaces.model.IGraph;
 
-import java.io.StringWriter;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
+import main.java.fr.lip6.move.coloane.extensions.importExportLola.ExportToLola;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
@@ -46,6 +52,10 @@ public class ParametersWizard extends Wizard {
 	
 	private List<Item> parameters = new ArrayList<Item>();
 	
+	
+	private enum ModelFormat { GML, LOLA };
+	
+	private ModelFormat format = ModelFormat.GML;
 	/**
 	 * Constructor
 	 * @param params list of DescriptionItem provided by an Alligator Service
@@ -55,6 +65,9 @@ public class ParametersWizard extends Wizard {
 
 		for (DescriptionItem p : params) {
 			if (p.getType() == ItemType.MODEL) {
+				if (p.getDefaultValue().equals(ModelFormat.LOLA.toString())) {
+					format = ModelFormat.LOLA;
+				}
 				modelsPage = new ChooseModelsPage("Choose the models to send to the platform", null);
 				addPage(modelsPage);
 				break;
@@ -76,17 +89,37 @@ public class ParametersWizard extends Wizard {
 	@Override
 	public final boolean performFinish() {
 		if (modelsPage != null) {
-			ExportToGML exporter = new ExportToGML();
-
+			IExportTo exporter ;
+			switch (format) {
+				case LOLA :
+					exporter = new ExportToLola();
+					break;
+				case GML :
+				default:
+					exporter = new ExportToGML();
+					break;
+			}
+			
 			// convert the models to GML
 			for (IResource resource : modelsPage.getSelectedResources()) {
 				if (resource != null && resource instanceof IFile) {
 					IFile modelFile = (IFile) resource;
 					IGraph graph = ModelLoader.loadGraphFromXML(modelFile);
 					try {
-						StringWriter writer = new StringWriter();
-						exporter.export(graph, writer, new NullProgressMonitor());
-						parameters.add(new Item(ItemType.MODEL, modelFile.getName(), writer.toString()));
+						// Create temp file.
+					    File temp = File.createTempFile("pattern", ".suffix");
+
+					    // Delete temp file when program exits.
+					    temp.deleteOnExit();
+					    
+						exporter.export(graph, temp.getAbsolutePath(), new NullProgressMonitor());
+						
+						String output = slurp(temp.getAbsolutePath());
+						
+						parameters.add(new Item(ItemType.MODEL, modelFile.getName(), output));
+					} catch (IOException e) {
+						Coloane.showErrorMsg(e.getMessage());
+						return false;
 					} catch (ExtensionException e) {
 						Coloane.showErrorMsg(e.getMessage());
 						return false;
@@ -100,6 +133,28 @@ public class ParametersWizard extends Wizard {
 		return true;
 	}
 
+	
+	/**
+	 * Prints a file's content into a String
+	 * @param outputFile the file to read
+	 * @return the file's content
+	 * @throws IOException something is wrong
+	 */
+	private String slurp(String outputFile) throws IOException {
+		File file = new File(outputFile);
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		String line  = null;
+		StringBuilder stringBuilder = new StringBuilder();
+		String ls = System.getProperty("line.separator");
+		while ((line = reader.readLine()) != null) {
+			stringBuilder.append(line);
+			stringBuilder.append(ls);
+		}
+		reader.close();
+		return stringBuilder.toString();
+	}
+
+	
 	/**
 	 * @return unmodifiable list of the parameters
 	 */
