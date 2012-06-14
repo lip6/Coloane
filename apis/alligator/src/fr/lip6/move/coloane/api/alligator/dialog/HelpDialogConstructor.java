424f12d846daae8e28fc04a9d2e32622113131ee
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2012-06-14 00:54:20 +0000

    A new Item with a nested html browser for help text.

---
+++ apis/alligator/src/fr/lip6/move/coloane/api/alligator/dialog/HelpDialogConstructor.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2006-2010 MoVe - Laboratoire d'Informatique de Paris 6 (LIP6).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Jean-Baptiste VORON (LIP6) - Project Head / Initial contributor
+ *   Clément DÉMOULINS (LIP6) - Project Manager
+ *
+ * Official contacts:
+ *   coloane@lip6.fr
+ *   http://coloane.lip6.fr
+ */
+package fr.lip6.move.coloane.api.alligator.dialog;
+
+import fr.lip6.move.alligator.interfaces.DescriptionItem;
+import fr.lip6.move.alligator.interfaces.Item;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+
+/**
+ * @author Yann Thierry-Mieg, based on other Dialogs.
+ */
+public class HelpDialogConstructor implements ItemDialogConstructor {
+
+	private String description;
+	private Label label;
+	private Browser input;
+
+	/** {@inheritDoc}
+	 * @see fr.lip6.move.coloane.api.alligator.dialog.ItemDialogConstructor#create(org.eclipse.swt.widgets.Composite, fr.lip6.move.alligator.interfaces.DescriptionItem)
+	 */
+	public final void create(Composite parent, DescriptionItem descriptionI) {
+		this.description = descriptionI.getName();
+		
+		this.label = new Label(parent, SWT.WRAP);
+		this.label.setText("Description "+ ":");
+		this.label.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 2, 1));
+
+		this.input = new Browser(parent, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL);
+		this.input.setText(description);
+		GridData layoutData = new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1);
+		layoutData.heightHint = 50;
+		this.input.setLayoutData(layoutData);
+}
+
+	@Override
+	public void setParameterValues(List<Item> oldValues) {
+		return ;
+	}
+	
+	/** {@inheritDoc}
+	 * @see fr.lip6.move.coloane.api.alligator.dialog.ItemDialogConstructor#getParameters()
+	 */
+	public final List<Item> getParameters() {
+		return Collections.EMPTY_LIST;
+	}
+
+	/** {@inheritDoc}
+	 * @see fr.lip6.move.coloane.api.alligator.dialog.ItemDialogConstructor#dispose()
+	 */
+	public final void dispose() {
+		label.dispose();
+		input.dispose();
+	}
+
+}
