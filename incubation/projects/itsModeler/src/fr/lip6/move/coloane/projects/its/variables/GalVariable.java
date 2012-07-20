9ae22a3a183dfb6748dd991ad0feeac297f52116
ythierry <ythierry@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2012-07-20 15:40:10 +0000

    declare Galvariable

---
+++ incubation/projects/itsModeler/src/fr/lip6/move/coloane/projects/its/variables/GalVariable.java
@@ -0,0 +1,23 @@
+package fr.lip6.move.coloane.projects.its.variables;
+
+import fr.lip6.move.gal.Variable;
+
+
+
+public class GalVariable extends LeafModelVariable {
+	Variable variable;
+
+	public GalVariable(fr.lip6.move.gal.Variable variable) {
+		super(variable.getName());
+		setId("GV_"+variable.getName());
+		this.variable = variable;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public final String getDescription() {
+		return "An integer" ;// representing a simple variable of a GAL System";
+	}
+
+}
