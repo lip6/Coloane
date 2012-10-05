519af03a226b2b46b0767e451fbfd57ff631eb41
Clément Démoulins <clement@archivel.fr>
2012-10-05 14:48:26 +0000

    Rename all 'gml' references to 'grml'

---
+++ src/fr/lip6/move/coloane/extensions/importfromgrml/ImportFromGML.java
@@ -0,0 +1,45 @@
+package fr.lip6.move.coloane.extensions.importfromgrml;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+
+import fr.lip6.move.coloane.core.model.factory.GraphModelFactory;
+import fr.lip6.move.coloane.interfaces.exceptions.ExtensionException;
+import fr.lip6.move.coloane.interfaces.extensions.IImportFrom;
+import fr.lip6.move.coloane.interfaces.formalism.IFormalism;
+import fr.lip6.move.coloane.interfaces.model.IGraph;
+
+public class ImportFromGrML implements IImportFrom {
+
+	public ImportFromGrML() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public IGraph importFrom(String filePath, IFormalism formalism,
+			IProgressMonitor monitor) throws ExtensionException {
+		IGraph graph = new GraphModelFactory().createGraph(formalism);
+		File f = new File(filePath);
+		BufferedReader reader;
+		GrMLParser parser = new GrMLParser(graph);
+		
+		try {
+			reader = new BufferedReader( new FileReader(f) );
+			parser.parseXML(reader);
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return graph;
+	}
+
+}
