87b36a58da45f8ddacb6b34b15e66e240b6713f1
Clément Démoulins <clement@archivel.fr>
2012-10-16 11:31:44 +0000

    Add an 'import...' button for the item of type TEXT.

--- apis/alligator/src/fr/lip6/move/coloane/api/alligator/dialog/TextDialogConstructor.java
+++ apis/alligator/src/fr/lip6/move/coloane/api/alligator/dialog/TextDialogConstructor.java
@@ -17,13 +17,21 @@ package fr.lip6.move.coloane.api.alligator.dialog;
 
 import fr.lip6.move.alligator.interfaces.DescriptionItem;
 import fr.lip6.move.alligator.interfaces.Item;
+import fr.lip6.move.coloane.core.main.Coloane;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
@@ -35,23 +43,53 @@ public class TextDialogConstructor implements ItemDialogConstructor {
 	private DescriptionItem description;
 	private Label label;
 	private Text input;
+	private Button importButton;
 
 	/** {@inheritDoc}
 	 * @see fr.lip6.move.coloane.api.alligator.dialog.ItemDialogConstructor#create(org.eclipse.swt.widgets.Composite, fr.lip6.move.alligator.interfaces.DescriptionItem)
 	 */
-	public final void create(Composite parent, DescriptionItem description) {
+	public final void create(final Composite parent, DescriptionItem description) {
 		this.description = description;
 
 		this.label = new Label(parent, SWT.WRAP);
 		this.label.setText(description.getName() + ":");
 		this.label.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 2, 1));
 
-		this.input = new Text(parent, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL);
-		this.input.setText(description.getDefaultValue());
+		final Text inputText = new Text(parent, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL | SWT.H_SCROLL);
+		inputText.setText(description.getDefaultValue());
 		GridData layoutData = new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1);
 		layoutData.heightHint = 50;
-		this.input.setLayoutData(layoutData);
-}
+		inputText.setLayoutData(layoutData);
+		this.input = inputText;
+
+		this.importButton = new Button(parent, SWT.PUSH);
+		this.importButton.setText("Import…");
+		this.importButton.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 2, 1));
+		this.importButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				FileDialog dialog = new FileDialog(parent.getShell(), SWT.OPEN);
+				final String filePath = dialog.open();
+				if (filePath != null) {
+					parent.getDisplay().asyncExec(new Runnable() {
+						public void run() {
+							try {
+								BufferedReader reader = new BufferedReader(new FileReader(filePath));
+								StringBuilder sb = new StringBuilder();
+								int c;
+								while ((c = reader.read()) != -1) {
+									sb.append((char) c);
+								}
+								inputText.setText(sb.toString());
+							} catch (IOException e) {
+								Coloane.showErrorMsg(e.getLocalizedMessage());
+							}
+						}
+					});
+				}
+			}
+		});
+	}
 
 	/** {@inheritDoc}
 	 * @see fr.lip6.move.coloane.api.alligator.dialog.ItemDialogConstructor#setParameterValues(java.util.List)
