bde4bece3bce69e324b95c53a82ea2919d5fab50
cdemoulins <cdemoulins@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2012-11-09 16:59:16 +0000

    Fix all the checkstyle warnings

--- trunk/application/product/src/fr/lip6/move/coloane/rcp/ColoaneApplication.java
+++ trunk/application/product/src/fr/lip6/move/coloane/rcp/ColoaneApplication.java
@@ -23,38 +23,37 @@ import org.eclipse.ui.PlatformUI;
 
 /**
  * This class controls all aspects of the application's execution
+ * @author Clément Démoulins
  */
 public class ColoaneApplication implements IApplication {
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.equinox.app.IApplication#start(org.eclipse.equinox.app.IApplicationContext)
-	 */
-	public Object start(IApplicationContext context) throws Exception {
+	/** {@inheritDoc} */
+	public final Object start(IApplicationContext context) throws Exception {
 		Display display = PlatformUI.createDisplay();
 		try {
 			int returnCode = PlatformUI.createAndRunWorkbench(display, new ApplicationWorkbenchAdvisor());
-			if (returnCode == PlatformUI.RETURN_RESTART)
+			if (returnCode == PlatformUI.RETURN_RESTART) {
 				return IApplication.EXIT_RESTART;
-			else
+			} else {
 				return IApplication.EXIT_OK;
+			}
 		} finally {
 			display.dispose();
 		}
-		
 	}
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.equinox.app.IApplication#stop()
-	 */
-	public void stop() {
-		if (!PlatformUI.isWorkbenchRunning())
+	/** {@inheritDoc} */
+	public final void stop() {
+		if (!PlatformUI.isWorkbenchRunning()) {
 			return;
+		}
 		final IWorkbench workbench = PlatformUI.getWorkbench();
 		final Display display = workbench.getDisplay();
 		display.syncExec(new Runnable() {
 			public void run() {
-				if (!display.isDisposed())
+				if (!display.isDisposed()) {
 					workbench.close();
+				}
 			}
 		});
 	}
