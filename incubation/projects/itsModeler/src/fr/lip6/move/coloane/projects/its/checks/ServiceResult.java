478b82c5b11fe87f98dad7828deebcc5dd0e86d2
ythierry <ythierry@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2012-11-25 23:38:38 +0000

    parse of CTL results

--- incubation/projects/itsModeler/src/fr/lip6/move/coloane/projects/its/checks/ServiceResult.java
+++ incubation/projects/itsModeler/src/fr/lip6/move/coloane/projects/its/checks/ServiceResult.java
@@ -36,7 +36,7 @@ public class ServiceResult extends SimpleObservable {
 	private IServiceResultProvider cs;
 	private ParameterList parameters;
 	private ParameterList results;
-	
+
 	public ServiceResult(Status success, String report, AbstractCheckService cs) {
 		this.success = success;
 		this.report = report;
@@ -86,30 +86,37 @@ public class ServiceResult extends SimpleObservable {
 	private void parseParameters(ParameterList results2, String report2) {
 		String [] lines = report2.split("\n");
 		boolean isReachStatLine=false;
+		int formulaId = 0;
 		for (int i = 0; i < lines.length; i++) {
 			String line = lines[i];
 			if (line.contains("Model ,")) {
 				if (++i < lines.length) {
-					 line = lines[i];
-					 String[] stats = line.split(",");
-					 if (stats.length >= 5) {
-						 if (! isReachStatLine) {
-							 results2.addParameter("Nb. states", stats[1], "Number of reachable states");
-							 results2.addParameter("Time (s)", stats[2], "Time for computation, in seconds");
-							 results2.addParameter("Mem (kB)", stats[3], "Peak memory used, in kilobytes");
-							 isReachStatLine = true;
-						 } else {
-							 results2.addParameter("Time witness (s)", stats[2], "Time for computation, in seconds, including reachability and witness construction");
-							 results2.addParameter("Mem witness (kB)", stats[3], "Peak memory used, in kilobytes, for reachability test and witness construction.");							 
-						 }
-					 }
+					line = lines[i];
+					String[] stats = line.split(",");
+					if (stats.length >= 5) {
+						if (! isReachStatLine) {
+							results2.addParameter("Nb. states", stats[1], "Number of reachable states");
+							results2.addParameter("Time (s)", stats[2], "Time for computation, in seconds");
+							results2.addParameter("Mem (kB)", stats[3], "Peak memory used, in kilobytes");
+							isReachStatLine = true;
+						} else {
+							if (formulaId==0) {
+								results2.addParameter("Time witness (s)", stats[2], "Time for computation, in seconds, including reachability and witness construction");
+								results2.addParameter("Mem witness (kB)", stats[3], "Peak memory used, in kilobytes, for reachability test and witness construction.");
+							} else {
+								results2.addParameter("Formula "+formulaId+" verdict", stats[2].equals("0")?"false":"true", "Is the CTL formula true in the initial state.");
+								results2.addParameter("Time formula "+formulaId, stats[2], "Time for computation, in seconds, total after checking formula.");
+								results2.addParameter("Mem formula "+formulaId, stats[2], "Memory for computation, in kB to check formula.");
+							}
+						}
+					}
 				}
 			} else if (line.contains("reachable states that satisfy your predicate")) {
-				 String[] stats = line.split(" ");
-				 if (stats.length>=11) {
-					 results2.addParameter("Predicate", stats[10], "Reachability predicate");
-					 results2.addParameter("Nb. states verifying", stats[2], "Number of reachable states that validate the predicate");					 
-				 }
+				String[] stats = line.split(" ");
+				if (stats.length>=11) {
+					results2.addParameter("Predicate", stats[10], "Reachability predicate");
+					results2.addParameter("Nb. states verifying", stats[2], "Number of reachable states that validate the predicate");					 
+				}
 				if (++i < lines.length) {
 					line = lines[i];
 					if (line.contains("computing trace")) {
@@ -127,16 +134,35 @@ public class ServiceResult extends SimpleObservable {
 									if (m.matches()) {
 										trans = m.group(1);
 									}
-//									trans.replace("T_\\d+", "");
+									//									trans.replace("T_\\d+", "");
 									trace.append(trans + " ");
 								}
-								
+
 								results2.addParameter("Trace", trace.toString(), "Sequence of transitions leading to target states.");
 							}
 						}
 					}
 				}
+			} else if (line.contains("System contains")&& line.contains("deadlocks")) {
+				String[] stats = line.split(" ");
+				if (stats.length>=5) {
+					results2.addParameter("Deadlocks", stats[2], "System reachable deadlocks");
+				}
+			} else if (line.contains("original formula")) {
+				formulaId++;
+				String[] stats = line.split(":");
+				if (stats.length>=2) {
+					results2.addParameter("Formula "+formulaId, stats[1], "Normalized CTL formula (with ITS Ids)");
+				}
+			} else if (line.contains("equivalent forward existential")) {
+				String[] stats = line.split(":");
+				if (stats.length>=2) {
+					results2.addParameter("Forward formula "+formulaId, stats[1], "Normalized Forward-CTL formula (with ITS Ids)");
+				}
+			} else if (line.contains("Formula is")) {
+				results2.addParameter("Formula "+formulaId+" verdict", line.replace("Formula ", ""), "Is the CTL formula true in the initial state.");				
 			}
+
 		}
 	}
 }
