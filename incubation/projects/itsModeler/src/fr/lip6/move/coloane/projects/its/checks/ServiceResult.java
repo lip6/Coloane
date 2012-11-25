51c6240904a567edfdc6c8e34fe599c0229967b5
ythierry <ythierry@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2012-11-25 13:43:02 +0000

    ServiceResult now carries a parsed version of results, packaged as a Parameter list

--- incubation/projects/itsModeler/src/fr/lip6/move/coloane/projects/its/checks/ServiceResult.java
+++ incubation/projects/itsModeler/src/fr/lip6/move/coloane/projects/its/checks/ServiceResult.java
@@ -21,6 +21,8 @@ import fr.lip6.move.coloane.projects.its.obs.SimpleObservable;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ServiceResult extends SimpleObservable {
 
@@ -33,13 +35,15 @@ public class ServiceResult extends SimpleObservable {
 	private Calendar date;
 	private IServiceResultProvider cs;
 	private ParameterList parameters;
-
+	private ParameterList results;
+	
 	public ServiceResult(Status success, String report, AbstractCheckService cs) {
 		this.success = success;
 		this.report = report;
 		this.date = new GregorianCalendar();
 		this.cs = cs;
 		this.parameters = new ParameterList(cs.getParameters());
+		results = null;
 	}
 
 	public ParameterList getParameters() {
@@ -71,4 +75,68 @@ public class ServiceResult extends SimpleObservable {
 		return ret;
 	}
 
+	public ParameterList getResults() {
+		if (results==null) {
+			results = new ParameterList();
+			parseParameters(results,report);
+		}
+		return results;
+	}
+
+	private void parseParameters(ParameterList results2, String report2) {
+		String [] lines = report2.split("\n");
+		boolean isReachStatLine=false;
+		for (int i = 0; i < lines.length; i++) {
+			String line = lines[i];
+			if (line.contains("Model ,")) {
+				if (++i < lines.length) {
+					 line = lines[i];
+					 String[] stats = line.split(",");
+					 if (stats.length >= 5) {
+						 if (! isReachStatLine) {
+							 results2.addParameter("Nb. states", stats[1], "Number of reachable states");
+							 results2.addParameter("Time (s)", stats[2], "Time for computation, in seconds");
+							 results2.addParameter("Mem (kB)", stats[3], "Peak memory used, in kilobytes");
+							 isReachStatLine = true;
+						 } else {
+							 results2.addParameter("Time witness (s)", stats[2], "Time for computation, in seconds, including reachability and witness construction");
+							 results2.addParameter("Mem witness (kB)", stats[3], "Peak memory used, in kilobytes, for reachability test and witness construction.");							 
+						 }
+					 }
+				}
+			} else if (line.contains("reachable states that satisfy your predicate")) {
+				 String[] stats = line.split(" ");
+				 if (stats.length>=11) {
+					 results2.addParameter("Predicate", stats[10], "Reachability predicate");
+					 results2.addParameter("Nb. states verifying", stats[2], "Number of reachable states that validate the predicate");					 
+				 }
+				if (++i < lines.length) {
+					line = lines[i];
+					if (line.contains("computing trace")) {
+						if (++i < lines.length) {
+							line = lines[i];
+							// diagnostic line about whether reverse trans rel is exact
+							assert(line.contains("Reverse"));
+							if (++i < lines.length) {
+								line = lines[i];
+								StringBuilder trace = new StringBuilder();
+								String[] transitions = line.split(" ");
+								Pattern p = Pattern.compile("T_\\d+(.+)"); 
+								for (String trans : transitions) {
+									Matcher m = p.matcher(trans);
+									if (m.matches()) {
+										trans = m.group(1);
+									}
+//									trans.replace("T_\\d+", "");
+									trace.append(trans + " ");
+								}
+								
+								results2.addParameter("Trace", trace.toString(), "Sequence of transitions leading to target states.");
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 }
