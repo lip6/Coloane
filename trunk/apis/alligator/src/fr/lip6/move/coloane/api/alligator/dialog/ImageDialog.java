4d7a73d43f3d902ca410800ab2c98315bda69d6a
alinard <alinard@29d87963-57d0-4673-b437-6d7abd9aa7fe>
2013-06-17 14:33:51 +0000

    Update dialogs to add a "save" button for image output parameters.

--- trunk/apis/alligator/src/fr/lip6/move/coloane/api/alligator/dialog/ImageDialog.java
+++ trunk/apis/alligator/src/fr/lip6/move/coloane/api/alligator/dialog/ImageDialog.java
@@ -9,16 +9,26 @@ package fr.lip6.move.coloane.api.alligator.dialog;
 
 import fr.lip6.move.coloane.api.alligator.Utility;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
 import java.util.logging.Logger;
 
 import org.cosyverif.alligator.service.Parameter;
 import org.cosyverif.alligator.service.parameter.FileParameter;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
@@ -31,7 +41,8 @@ public final class ImageDialog
     private Label image;
     private Label label;
     private Text help;
-
+    private Button saveButton;
+    private File file = null;
     private Composite container;
 
     public ImageDialog(FileParameter parameter) {
@@ -42,7 +53,8 @@ public final class ImageDialog
 
     @Override
     public
-        void updateDialog() {
+        void
+        updateDialog() {
         if (parameter.isActualParameter()) {
             LOGGER.info("Setting image...");
             try {
@@ -58,19 +70,22 @@ public final class ImageDialog
 
     @Override
     public
-        void updateParameter() {
+        void
+        updateParameter() {
         throw new AssertionError();
     }
 
     @Override
     public
-        int size() {
+        int
+        size() {
         return 6;
     }
 
     @Override
     public
-        void create(Composite parent) {
+        void
+        create(final Composite parent) {
         // Label:
         label = new Label(parent, SWT.WRAP);
         label.setText(parameter.getName() + ":");
@@ -88,10 +103,37 @@ public final class ImageDialog
         image = new Label(container, SWT.CENTER);
         image.setText("");
         image.setSize(width, height);
+        // Button:
+        saveButton = new Button(parent, SWT.PUSH);
+        saveButton.setText("Save as…");
+        saveButton.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 3, 1));
+        saveButton.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public
+                void
+                widgetSelected(SelectionEvent e) {
+                FileDialog dialog = new FileDialog(parent.getShell(), SWT.SAVE);
+                dialog.setFilterPath(ResourcesPlugin.getWorkspace()
+                                                    .getRoot()
+                                                    .getLocation()
+                                                    .toString());
+                String[] parts = parameter.getContentType()
+                                          .split("/");
+                dialog.setFilterExtensions(new String[] {
+                    "*." + parts[parts.length - 1]
+                });
+                final String filePath = dialog.open();
+                if (filePath != null) {
+                    file = new File(filePath);
+                    copyFile();
+                }
+            }
+        });
     }
 
     private
-        Image resize(Image i) {
+        Image
+        resize(Image i) {
         // Keep aspect ratio:
         float ratio = (float) i.getBounds().width / (float) i.getBounds().height;
         int newWidth;
@@ -115,16 +157,53 @@ public final class ImageDialog
 
     @Override
     public
-        String errorMessage() {
+        String
+        errorMessage() {
         return null;
     }
 
     @Override
     public
-        void update(Parameter<?> that) {
+        void
+        update(Parameter<?> that) {
         if (!this.equals(that)) {
             parameter.populateFrom(that);
             updateDialog();
+        }
+    }
+
+    // http://stackoverflow.com/questions/300559/move-copy-file-operations-in-java
+    public
+        void
+        copyFile() {
+        String[] parts = parameter.getContentType()
+                                  .split("/");
+        file.delete();
+        try {
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileChannel source = null;
+            FileChannel destination = null;
+            try {
+                source = new FileInputStream(parameter.getFile()).getChannel();
+                destination = new FileOutputStream(file).getChannel();
+                // previous code: destination.transferFrom(source, 0, source.size());
+                // to avoid infinite loops, should be:
+                long count = 0;
+                long size = source.size();
+                while ((count += destination.transferFrom(source, count, size - count)) < size)
+                    ;
+            } finally {
+                if (source != null) {
+                    source.close();
+                }
+                if (destination != null) {
+                    destination.close();
+                }
+            }
+        } catch (IOException e1) {
+            e1.printStackTrace();
         }
     }
 
