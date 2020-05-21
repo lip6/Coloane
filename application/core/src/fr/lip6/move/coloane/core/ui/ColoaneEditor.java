7fa4e39e67b15b6add1f4573b687e77f71ea45f5
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2020-05-21 13:05:16 +0200

    patch potential crash

--- application/core/src/fr/lip6/move/coloane/core/ui/ColoaneEditor.java
+++ application/core/src/fr/lip6/move/coloane/core/ui/ColoaneEditor.java
@@ -491,7 +491,7 @@ public class ColoaneEditor extends GraphicalEditorWithFlyoutPalette implements I
 					if (event.getDelta() != null && event.getDelta().getKind() == IResourceDelta.CHANGED) {
 						for (IResourceDelta c: Arrays.asList(event.getDelta().getAffectedChildren())) {
 							for (IResourceDelta d: Arrays.asList(c.getAffectedChildren())) {
-								if ((d.getKind() & IResourceDelta.ADDED) == IResourceDelta.ADDED) {
+								if ((d.getKind() & IResourceDelta.ADDED) == IResourceDelta.ADDED && d.getResource().getType() == IResource.FILE) {
 									final IFile file = (IFile) d.getResource();
 									Display.getDefault().asyncExec(new Runnable() {
 										@Override
